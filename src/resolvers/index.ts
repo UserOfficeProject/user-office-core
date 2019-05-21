d78059953e8859fa7f88691567ae0657586c6940
Hannes Petri <hannes.petri@maxiv.lu.se>
2019-05-21 20:32:03 +0200

    Removed unnecessary instances of "function"

--- src/resolvers/index.ts
+++ src/resolvers/index.ts
@@ -28,15 +28,15 @@ interface CreateUserArgs {
 }
 
 export default {
-  proposal: function(args: ProposalArgs, context: ResolverContext) {
+  proposal(args: ProposalArgs, context: ResolverContext) {
     return context.queries.proposal.get(args.id);
   },
 
-  proposals: function(_args: ProposalsArgs, context: ResolverContext) {
+  proposals(_args: ProposalsArgs, context: ResolverContext) {
     return context.queries.proposal.getAll();
   },
 
-  createProposal: function(args: CreateProposalArgs, context: ResolverContext) {
+  createProposal(args: CreateProposalArgs, context: ResolverContext) {
     const { abstract, status, users } = args;
     return context.mutations.proposal.create(
       context.user,
@@ -46,22 +46,19 @@ export default {
     );
   },
 
-  approveProposal: function(
-    args: ApproveProposalArgs,
-    context: ResolverContext
-  ) {
+  approveProposal(args: ApproveProposalArgs, context: ResolverContext) {
     return context.mutations.proposal.accept(context.user, args.id);
   },
 
-  user: function(args: UserArgs, context: ResolverContext) {
+  user(args: UserArgs, context: ResolverContext) {
     return context.queries.user.get(args.id);
   },
 
-  users: function(_args: UsersArgs, context: ResolverContext) {
+  users(_args: UsersArgs, context: ResolverContext) {
     return context.queries.user.getAll();
   },
 
-  createUser: function(args: CreateUserArgs, context: ResolverContext) {
+  createUser(args: CreateUserArgs, context: ResolverContext) {
     return context.mutations.user.create(args.firstname, args.lastname);
   }
 };
