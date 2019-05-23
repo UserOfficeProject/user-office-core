ca2070b2c8035e8ac23280a98f0eba1509999645
Fredrik Bolmsten <fredrik.bolmsten@esss.se>
2019-05-23 09:32:02 +0200

    remove duplicate

--- src/datasources/mockups/ProposalDataSource.ts
+++ src/datasources/mockups/ProposalDataSource.ts
@@ -4,25 +4,23 @@ import Proposal from "../../models/Proposal";
 export const dummyProposal = new Proposal(1, "asd", 1);
 
 export class proposalDataSource implements ProposalDataSource {
-  public dummyProposal = new Proposal(1, "asd", 1);
-
   async acceptProposal(id: number): Promise<Proposal | null> {
-    return this.dummyProposal;
+    return dummyProposal;
   }
 
   async get(id: number) {
-    return this.dummyProposal;
+    return dummyProposal;
   }
 
   async create(abstract: string, status: number, users: number[]) {
-    return this.dummyProposal;
+    return dummyProposal;
   }
 
   async getProposals() {
-    return this.dummyProposal;
+    return dummyProposal;
   }
 
   async getUserProposals(id: number) {
-    return this.dummyProposal;
+    return dummyProposal;
   }
 }
