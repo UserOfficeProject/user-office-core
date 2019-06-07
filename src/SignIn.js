5a5fe21f40b0c869e4661f0efa75250f30c1d775
Fredrik Bolmsten <fredrik.bolmsten@esss.se>
2019-06-07 17:31:08 +0200

    Fixed link

--- src/SignIn.js
+++ src/SignIn.js
@@ -5,13 +5,12 @@ import CssBaseline from '@material-ui/core/CssBaseline';
 import TextField from '@material-ui/core/TextField';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import Checkbox from '@material-ui/core/Checkbox';
-import Link from '@material-ui/core/Link';
 import Grid from '@material-ui/core/Grid';
 import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
-
+import { Link } from "react-router-dom";
 
 const useStyles = makeStyles(theme => ({
   '@global': {
@@ -94,7 +93,7 @@ export default function SignIn() {
               </Link>
             </Grid>
             <Grid item>
-              <Link href="/SignUp/" variant="body2">
+              <Link to="/SignUp/">
                 {"Don't have an account? Sign Up"}
               </Link>
             </Grid>
--- src/SignUp.js
+++ src/SignUp.js
@@ -3,14 +3,14 @@ import Avatar from '@material-ui/core/Avatar';
 import Button from '@material-ui/core/Button';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import TextField from '@material-ui/core/TextField';
-import Link from '@material-ui/core/Link';
 import Grid from '@material-ui/core/Grid';
 import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
 import { request } from 'graphql-request'
- 
+import { Link } from "react-router-dom";
+
 
 const useStyles = makeStyles(theme => ({
   '@global': {
@@ -146,7 +146,7 @@ export default function SignUp() {
           </Button>
           <Grid container justify="flex-end">
             <Grid item>
-              <Link href="/SignIn/" variant="body2">
+              <Link to="/SignIn/">
                 Already have an account? Sign in
               </Link>
             </Grid>
