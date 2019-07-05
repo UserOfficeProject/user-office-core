96fa629d4da4740faa72bf641742e1234012a68d
Hannes Petri <hannes.petri@maxiv.lu.se>
2019-07-05 09:41:00 +0200

    Replaced email property with dummy email

--- src/eventHandlers/email.ts
+++ src/eventHandlers/email.ts
@@ -3,7 +3,7 @@ import { UserDataSource } from "../datasources/UserDataSource";
 
 export default function createHandler(userDataSource: UserDataSource) {
   // Handler to send email to proposers in accepted proposal
-  
+
   return async function emailHandler(event: ApplicationEvent) {
     function sendEmail(_address: string, _topic: string, _message: string) {}
 
@@ -12,7 +12,8 @@ export default function createHandler(userDataSource: UserDataSource) {
         const proposal = event.proposal;
         const participants = await userDataSource.getProposalUsers(proposal.id);
 
-        for (const { firstname, lastname, email } of participants) {
+        for (const { firstname, lastname } of participants) {
+          const email = "dummy@dummy.com";
           const topic = "Congrats!";
           const message = `Dear ${firstname} ${lastname}, your proposal has been accepted.`;
           sendEmail(email, topic, message);
@@ -25,7 +26,8 @@ export default function createHandler(userDataSource: UserDataSource) {
         const proposal = event.proposal;
         const participants = await userDataSource.getProposalUsers(proposal.id);
 
-        for (const { firstname, lastname, email } of participants) {
+        for (const { firstname, lastname } of participants) {
+          const email = "dummy@dummy.com";
           const topic = "Tough luck!";
           const message = `Sorry ${firstname} ${lastname}, your proposal was rejected because: ${
             event.reason
