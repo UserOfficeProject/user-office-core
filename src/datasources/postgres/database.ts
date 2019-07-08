b7a27501f3b0ff37833c68b29527bea13cdd03a6
Fredrik Bolmsten <fredrik.bolmsten@esss.se>
2019-07-08 16:14:36 +0200

    make connection to psql dynamic

--- src/datasources/postgres/database.ts
+++ src/datasources/postgres/database.ts
@@ -2,10 +2,5 @@ var Knex = require("knex");
 
 export default new Knex({
   client: "postgresql",
-  connection: {
-    host: "127.0.0.1",
-    user: "duouser",
-    password: "duopassword",
-    database: "duo"
-  }
+  connection: process.env.DATABASE_URL
 });
