cbd0e1ba92203df5df7b9fcb1b311e91f7edf339
Fredrik Bolmsten <fredrik.bolmsten@esss.se>
2019-07-09 12:22:37 +0200

    reverse proxy fixes

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const proxy = require("express-http-proxy");
 const favicon = require("express-favicon");
 const path = require("path");
 const port = process.env.PORT || 8000;
@@ -7,10 +8,20 @@ app.use(favicon(__dirname + "/build/favicon.ico"));
 // the __dirname is the current directory from where the script is running
 app.use(express.static(__dirname));
 app.use(express.static(path.join(__dirname, "build")));
-app.get("/ping", function(req, res) {
-  return res.send("pong");
-});
+
+app.use(
+  "/graphql",
+  proxy("https://duo2-backend-staging.herokuapp.com", {
+    proxyReqPathResolver: function(req) {
+      return "/graphql";
+    }
+  })
+);
+
 app.get("/*", function(req, res) {
   res.sendFile(path.join(__dirname, "build", "index.html"));
 });
+
+app.use("/", proxy("https://duo2-backend-staging.herokuapp.com"));
+
 app.listen(port);
