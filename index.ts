ee5c4af2e2943ee722595fdd6e339b48c2c6e612
Hannes Petri <hannes.petri@maxiv.lu.se>
2019-07-15 14:20:38 +0200

    Added type declaration for request object with user information

--- index.ts
+++ index.ts
@@ -8,6 +8,7 @@ import schema from "./src/schema";
 import root from "./src/resolvers";
 import baseContext from "./src/buildContext";
 import { ResolverContext } from "./src/context";
+import { User } from "./src/models/User";
 
 var app = express();
 
@@ -19,15 +20,23 @@ const authMiddleware = jwt({
 
 app.use(authMiddleware);
 
+declare global {
+  namespace Express {
+    interface Request {
+      user: { id: number };
+    }
+  }
+}
+
 app.use(
   "/graphql",
   graphqlHTTP(async (req: Request) => {
     // Adds the currently logged-in user to the context object, which makes it available to the resolvers
     // The user sends a JWT token that is decrypted, this JWT token contains information about roles and ID
-    // let user = null;
-    // if (req.user) {
-    //   user = await baseContext.queries.user.getAgent(req.user.id);
-    // }
+    let user = null;
+    if (req.user) {
+      user = await baseContext.queries.user.getAgent(req.user.id);
+    }
 
     // For development purpose you can bypass JWT tokens and activate a user directly here
 
@@ -40,14 +49,14 @@ app.use(
     //   proposals: () => []
     // };
 
-    const user = {
-      id: 2,
-      firstname: "Anders",
-      lastname: "Andersson",
-      username: "testofficer",
-      roles: () => [],
-      proposals: () => []
-    };
+    // const user = {
+    //   id: 2,
+    //   firstname: "Anders",
+    //   lastname: "Andersson",
+    //   username: "testofficer",
+    //   roles: () => [],
+    //   proposals: () => []
+    // };
 
     const context: ResolverContext = { ...baseContext, user };
 
