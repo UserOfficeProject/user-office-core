4d865f80e06809ef5ad68bbee8f8c545206debdd
Fredrik Bolmsten <fredrik.bolmsten@esss.se>
2019-08-02 09:41:36 +0200

    Create a useProposalData hook for fetching data

--- src/components/ProposalEdit.js
+++ src/components/ProposalEdit.js
@@ -1,54 +1,9 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import ProposalContainer from "./ProposalContainer";
-import { useDataAPI } from "../hooks/useDataAPI";
+import { useProposalData } from "../hooks/useProposalData";
 
 export default function ProposalEdit({ match }) {
-  const [proposalData, setProposalData] = useState({});
-  const [loading, setLoading] = useState(true);
-  const sendRequest = useDataAPI();
-
-  useEffect(() => {
-    const getProposalInformation = id => {
-      const query = `
-      query($id: ID!) {
-        proposal(id: $id) {
-          id
-          title
-          abstract
-          status
-          users{
-            firstname
-            lastname
-            username
-            id
-          }
-        }
-      }
-      `;
-
-      const variables = {
-        id
-      };
-      sendRequest(query, variables).then(data => {
-        setProposalData({
-          title: data.proposal.title,
-          abstract: data.proposal.abstract,
-          id: data.proposal.id,
-          status: data.proposal.status,
-          users: data.proposal.users.map(user => {
-            return {
-              name: user.firstname,
-              surname: user.lastname,
-              username: user.username,
-              id: user.id
-            };
-          })
-        });
-        setLoading(false);
-      });
-    };
-    getProposalInformation(match.params.proposalID);
-  }, [match.params.proposalID, sendRequest]);
+  const { loading, proposalData } = useProposalData(match.params.proposalID);
 
   if (loading) {
     return <p>Loading</p>;
