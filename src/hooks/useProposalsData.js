c5f9dd178c76dc8c87d62a18e8ed7c52de77a2f9
Fredrik Bolmsten <fredrik.bolmsten@esss.se>
2019-08-02 10:24:36 +0200

    created hook for fetching all proposals, it takes filter as a parameter

---
+++ src/hooks/useProposalsData.js
@@ -0,0 +1,42 @@
+import { useEffect, useState } from "react";
+import { useDataAPI } from "../hooks/useDataAPI";
+
+export function useProposalsData(filter) {
+  const sendRequest = useDataAPI();
+  const [proposalsData, setProposalsData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    const sendAllProposalRequest = filter => {
+      const query = `
+        query($filter: String!) {
+          proposals(filter: $filter) {
+            proposals{
+              id
+              title
+              status
+              }
+            totalCount
+            }
+          }`;
+
+      const variables = {
+        filter: filter
+      };
+      sendRequest(query, variables).then(data => {
+        setProposalsData(
+          data.proposals.proposals.map(proposal => {
+            return {
+              id: proposal.id,
+              title: proposal.title,
+              status: proposal.status
+            };
+          })
+        );
+        setLoading(false);
+      });
+    };
+    sendAllProposalRequest(filter);
+  }, [filter, sendRequest]);
+
+  return { loading, proposalsData };
+}
