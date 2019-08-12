aaf53b30725ce1f009c161a1cd280f71646e5748
Fredrik Bolmsten <fredrik.bolmsten@esss.se>
2019-08-12 08:45:16 +0200

    clean up of css for proposal table

--- src/components/ProposalTable.js
+++ src/components/ProposalTable.js
@@ -1,45 +1,10 @@
 import React, { useState } from "react";
 import { Redirect } from "react-router";
 import MaterialTable from "material-table";
-import {
-  AddBox,
-  Check,
-  Clear,
-  DeleteOutline,
-  Edit,
-  FilterList,
-  ViewColumn,
-  ArrowUpward,
-  Search,
-  FirstPage,
-  LastPage,
-  ChevronRight,
-  ChevronLeft,
-  Remove,
-  SaveAlt
-} from "@material-ui/icons";
+import { tableIcons } from "../utils/tableIcons";
+import { Edit } from "@material-ui/icons";
 
 export default function ProposalTable(props) {
-  const tableIcons = {
-    Add: AddBox,
-    Check: Check,
-    Clear: Clear,
-    Delete: DeleteOutline,
-    DetailPanel: ChevronRight,
-    Edit: Edit,
-    Export: SaveAlt,
-    Filter: FilterList,
-    FirstPage: FirstPage,
-    LastPage: LastPage,
-    NextPage: ChevronRight,
-    PreviousPage: ChevronLeft,
-    ResetSearch: Clear,
-    Search: Search,
-    SortArrow: ArrowUpward,
-    ThirdStateCheck: Remove,
-    ViewColumn: ViewColumn
-  };
-
   const columns = [
     { title: "ID", field: "id" },
     { title: "Title", field: "title" },
