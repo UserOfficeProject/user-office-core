008cdd77812003d5352df5de6027fbfb768f097c
Jekabs Karklins <jekabs.karklins@esss.se>
2019-08-21 16:14:03 +0200

    SWAP-150 Adding ProposalTestBed for common test setup

--- src/model/ConditionEvaluator.spec.ts
+++ src/model/ConditionEvaluator.spec.ts
@@ -1,44 +1,14 @@
-import { ProposalTemplate, ProposalTemplateField } from "./ProposalModel";
-import { ConditionEvaluator } from "./ConditionEvaluator";
+import {createTemplate} from "../test/ProposalTestBed"
 
-var template:ProposalTemplate;
-var conditionEvaluator:ConditionEvaluator
+test("Is dependency checking working", () => {
+  let template = createTemplate();
+  expect(template.areDependenciesSatisfied("has_links_with_industry")).toBe(true);
+  expect(template.areDependenciesSatisfied("links_with_industry")).toBe(false);
+});
 
-beforeEach(() => {
-    conditionEvaluator = new ConditionEvaluator();
-    template = new ProposalTemplate({
-      "fields": [
-          {
-            "proposal_question_id": "has_links_with_industry",
-            "dependencies": [],
-            "data_type": "SELECTION_FROM_OPTIONS",
-            "question": "Links with industry?",
-            "config": "{\"variant\":\"radio\", \"options\":[\"yes\", \"no\"], \"topic\":\"general_info\"}"
-          },
-          {
-            "proposal_question_id": "links_with_industry",
-            "dependencies": [
-              {
-                "proposal_question_dependency": "has_links_with_industry",
-                "condition": "{ \"condition\": \"equals\", \"params\":\"yes\" }",
-                "proposal_question_id": "links_with_industry"
-              }
-            ],
-            "data_type": "SMALL_TEXT",
-            "question": "Please specify",
-            "config": "{\"max\":300, \"min\":2,\"topic\":\"general_info\"}"
-          }
-        ]
-    });
-  });
-
-  test("Is dependency checking working", () => {
-    expect(template.areDependenciesSatisfied("has_links_with_industry")).toBe(true);
+test("Updating value changes dependency sattisfaction", () => {
+    let template = createTemplate();
     expect(template.areDependenciesSatisfied("links_with_industry")).toBe(false);
-  });
-  
-  test("Updating value changes dependency sattisfaction", () => {
-      expect(template.areDependenciesSatisfied("links_with_industry")).toBe(false);
-      template.getFieldById("has_links_with_industry").value = "yes";
-      expect(template.areDependenciesSatisfied("links_with_industry")).toBe(true);
-  })
+    template.getFieldById("has_links_with_industry").value = "yes";
+    expect(template.areDependenciesSatisfied("links_with_industry")).toBe(true);
+})
--- src/model/ProposalModel.spec.ts
+++ src/model/ProposalModel.spec.ts
@@ -1,50 +1,7 @@
-import { ProposalTemplate } from "./ProposalModel";
-
-var template: ProposalTemplate;
-
-beforeEach(() => {
-  template = new ProposalTemplate({
-    "fields": [
-        {
-          "proposal_question_id": "has_links_with_industry",
-          "dependencies": [],
-          "data_type": "SELECTION_FROM_OPTIONS",
-          "question": "Links with industry?",
-          "config": "{\"variant\":\"radio\", \"options\":[\"yes\", \"no\"], \"topic\":\"general_info\"}"
-        },
-        {
-          "proposal_question_id": "links_with_industry",
-          "dependencies": [
-            {
-              "proposal_question_dependency": "has_links_with_industry",
-              "condition": "{ \"condition\": \"equals\", \"params\":\"yes\" }",
-              "proposal_question_id": "links_with_industry"
-            }
-          ],
-          "data_type": "SMALL_TEXT",
-          "question": "Please specify",
-          "config": "{\"max\":300, \"min\":2,\"topic\":\"general_info\"}"
-        },
-        {
-          "proposal_question_id": "final_delivery_date",
-          "dependencies": [],
-          "data_type": "DATE",
-          "question": "Final delivery date",
-          "config": "{\"topic\":\"general_info\"}"
-        },
-        {
-          "proposal_question_id": "final_delivery_date_motivation",
-          "dependencies": [],
-          "data_type": "LARGE_TEXT",
-          "question": "Please motivate the chosen date",
-          "config": "{\"small_label\":\"(e.g. based on awarded beamtime, or described intention to apply)\",\"topic\":\"general_info\"}"
-        }
-      ]
-  });
-});
-
+import {createTemplate} from "../test/ProposalTestBed"
 
 test("Can parse object", () => {
+  var template = createTemplate();
   expect(template.fields.length).toBe(4);
   expect(template.getFieldById("links_with_industry").dependencies).not.toBe(null);
   expect(template.getFieldById("links_with_industry").dependencies!.length).toBe(1);
