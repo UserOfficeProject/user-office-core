7503db5c796275618e6d4666bfa6acf6828ebb91
Jekabs Karklins <jekabs.karklins@esss.se>
2019-08-26 09:10:57 +0200

    SWAP-150 refactoring to functional component

--- src/components/ProposalComponents.tsx
+++ src/components/ProposalComponents.tsx
@@ -1,12 +1,18 @@
 import React, { ChangeEvent } from "react";
-import { ProposalTemplateField } from "../model/ProposalModel";
-import { TextField } from "@material-ui/core";
+import { ProposalTemplateField, DataType } from "../model/ProposalModel";
+import {
+  TextField,
+  FormControl,
+  FormLabel,
+  RadioGroup,
+  FormControlLabel,
+  Radio,
+  makeStyles
+} from "@material-ui/core";
 import JSDict from "../utils/Dictionary";
 import * as Yup from "yup";
 
-export class ProposalComponentTextInput extends React.Component<IBasicComponentProps> 
-{
-
+export class ProposalComponentTextInput extends React.Component<IBasicComponentProps> {
   onChange(e: ChangeEvent<HTMLInputElement>) {
     this.props.templateField.value = e.target.value;
     this.props.handleChange(e); // letting Formik know that there was a change
@@ -16,76 +22,146 @@ export class ProposalComponentTextInput extends React.Component<IBasicComponentP
     if (this.props.templateField === undefined) {
       return <div>loading...</div>;
     }
-    let {templateField, onComplete, touched, errors} = this.props;
+    let { templateField, onComplete, touched, errors } = this.props;
     return (
       <div className="baseComponent">
-        
-        <TextField id={templateField.proposal_question_id} 
-              name={templateField.proposal_question_id}
-              fullWidth 
-              label={templateField.question} 
-              value={templateField.value} 
-              onChange={this.onChange.bind(this)} 
-              onBlur={() => onComplete()} 
-              // @ts-ignore: Formik does not know that this is available
-              error={touched[templateField.proposal_question_id] && errors[templateField.proposal_question_id]} 
-              // @ts-ignore: Formik does not know that this is available
-              helperText={touched[templateField.proposal_question_id] && errors[templateField.proposal_question_id] && errors[templateField.proposal_question_id]}
-              margin="normal" />
+        <TextField
+          id={templateField.proposal_question_id}
+          name={templateField.proposal_question_id}
+          fullWidth
+          label={templateField.question}
+          value={templateField.value}
+          onChange={this.onChange.bind(this)}
+          onBlur={() => onComplete()}
+          error={
+            touched[templateField.proposal_question_id] &&
+            errors[templateField.proposal_question_id]
+          }
+          helperText={
+            touched[templateField.proposal_question_id] &&
+            errors[templateField.proposal_question_id] &&
+            errors[templateField.proposal_question_id]
+          }
+          margin="normal"
+        />
         <span>{templateField.config.small_label}</span>
       </div>
     );
   }
 }
 
-interface IBasicComponentProps {
-  templateField: ProposalTemplateField; 
-  onComplete: Function;
-  touched:any;
-  errors:any;
-  handleChange:any;
+export function ProposalComponentMultipleChoice(props: IBasicComponentProps) {
+  const classes = makeStyles({
+    horizontal: {
+      flexDirection: "row"
+    }
+  })();
+
+  if (props.templateField === undefined) {
+    return <div>loading...</div>;
+  }
+  let { templateField, onComplete } = props;
+
+  return (
+    <div className="baseComponent">
+      <FormControl component="fieldset">
+        <FormLabel component="legend">{templateField.question}</FormLabel>
+        <span>{templateField.config.small_label}</span>
+        <RadioGroup
+          id={templateField.proposal_question_id}
+          name={templateField.proposal_question_id}
+          onChange={evt => {
+            props.templateField.value = (evt.target as HTMLInputElement).value;
+            props.handleChange(evt); // letting Formik know that there was a change
+            onComplete();
+          }}
+          value={templateField.value}
+          className={templateField.config.options.length < 3 ? classes.horizontal : undefined}
+        >
+          {(templateField.config.options as string[]).map(option => {
+            return (
+              <FormControlLabel
+                value={option}
+                control={<Radio />}
+                label={option}
+              />
+            );
+          })}
+        </RadioGroup>
+      </FormControl>
+    </div>
+  );
 }
 
+interface IBasicComponentProps {
+  templateField: ProposalTemplateField;
+  onComplete: Function;
+  touched: any;
+  errors: any;
+  handleChange: any;
+}
 
 export class ComponentFactory {
   private componentMap = JSDict.Create<string, any>();
 
   constructor() {
-    this.componentMap.put("SMALL_TEXT", ProposalComponentTextInput);
+    this.componentMap.put(DataType.SMALL_TEXT, ProposalComponentTextInput);
+    this.componentMap.put(
+      DataType.SELECTION_FROM_OPTIONS,
+      ProposalComponentMultipleChoice
+    );
   }
 
-  createComponent( field: ProposalTemplateField, props: any ): React.ComponentElement<IBasicComponentProps, any> {
+  createComponent(
+    field: ProposalTemplateField,
+    props: any
+  ): React.ComponentElement<IBasicComponentProps, any> {
     props.templateField = field;
     props.key = field.proposal_question_id;
 
-    // return React.createElement(this.componentMap.get(node.data_type.toString()), props);
-    return React.createElement(this.componentMap.get("SMALL_TEXT"), props); // WIP returing TextInputComponent until other components are made
+    let component = this.componentMap.get(field.data_type);
+
+    return component
+      ? React.createElement(component, props)
+      : React.createElement(this.componentMap.get(DataType.SMALL_TEXT), props); // TMP
   }
 }
 
+export const toYupValidationSchema = (
+  field: ProposalTemplateField
+): Yup.Schema<any> => {
+  switch (field.data_type) {
+    case DataType.SMALL_TEXT:
+      return Yup.string()
+        .min(
+          field.config.min,
+          `Value must be at least ${field.config.min} characters`
+        )
+        .max(
+          field.config.max,
+          `Value must be at most ${field.config.max} characters`
+        );
+    case DataType.SELECTION_FROM_OPTIONS:
+      return Yup.string();
+  }
 
-export const toYupValidationSchema = (field:ProposalTemplateField):Yup.Schema<any> => {
-  //switch(field.data_type) {
-    //case DataType.SMALL_TEXT:
-        return Yup.string()
-          .min(field.config.min, `Value must be at least ${field.config.min} characters`)
-          .max(field.config.max, `Value must be at most ${field.config.max} characters`)
-  //}
-}
+  return Yup.string();
+};
 
+export const toYupInitialValues = (field: ProposalTemplateField): any => {
+  return field.value;
+};
 
-export const toYupInitialValues = (field:ProposalTemplateField):any => {
-        return field.value;
-}
-
-export const createFormikCofigObjects = (fields:ProposalTemplateField[]):{validationSchema:any, initialValues:any} =>  {
-  let validationSchema:any = {};
-  let initialValues:any = {};
+export const createFormikCofigObjects = (
+  fields: ProposalTemplateField[]
+): { validationSchema: any; initialValues: any } => {
+  let validationSchema: any = {};
+  let initialValues: any = {};
 
   fields.forEach(field => {
     validationSchema[field.proposal_question_id] = toYupValidationSchema(field);
     initialValues[field.proposal_question_id] = toYupInitialValues(field);
   });
-          
+
   return { initialValues, validationSchema };
-}
+};
--- src/components/ProposalQuestionareStep.tsx
+++ src/components/ProposalQuestionareStep.tsx
@@ -1,66 +1,77 @@
 import React from "react";
-import * as Yup from "yup"
+import * as Yup from "yup";
 import { Formik, Form } from "formik";
 import { ProposalTemplate, Proposal } from "../model/ProposalModel";
-import { Button } from "@material-ui/core";
-import { ComponentFactory, createFormikCofigObjects } from "./ProposalComponents";
+import { Button, makeStyles } from "@material-ui/core";
+import {
+  ComponentFactory,
+  createFormikCofigObjects
+} from "./ProposalComponents";
 
-export default class ProposalQuestionareStep extends React.Component<{ model: ProposalTemplate; next: Function; back: Function; data: Proposal; topic: string;}> 
-{
-  private componentFactory: ComponentFactory = new ComponentFactory();
+export  default function ProposalQuestionareStep(props: {
+  model: ProposalTemplate;
+  next: Function;
+  back: Function;
+  data: Proposal;
+  topic: string;
+}) {
 
-  onInputUpdated() {
-    this.setState({}); // re-render
-  }
+  const [, updateState] = React.useState();
+  const forceUpdate = React.useCallback(() => updateState({}), []);
+  const componentFactory: ComponentFactory = new ComponentFactory();
 
-  render() {
-    const { back, next, model, topic } = this.props;
-
-    if (model == null) {
-      return <div>loading...</div>;
+  const classes = makeStyles({
+    inputComponentWrapper: {
+      margin: "10px 0"
     }
+  })();
 
-    let activeFields = (model! as ProposalTemplate).fields.filter(field => {
-      return (
-        field.config.topic === topic &&
-        model.areDependenciesSatisfied(field.proposal_question_id)
-      );
-    });
+  const { back, next, model, topic } = props;
 
-    let backbutton = back ? <Button onClick={() => back()}>Back</Button> : null;
-    let nextButton = next ? <Button type="submit" variant="contained" color="primary" onClick={() => { next(); }} > Next </Button> : null;
-
-    let { initialValues, validationSchema } = createFormikCofigObjects(
-      activeFields
-    );
-
-    return (
-      <Formik
-        initialValues={initialValues}
-        onSubmit={() => {}}
-        validationSchema={Yup.object().shape(validationSchema)}
-      >
-        {({ errors, touched, handleChange, isSubmitting }) => (
-          <Form>
-            <div>
-              {activeFields.map(field => {
-                return this.componentFactory.createComponent(field, {
-                  onComplete: this.onInputUpdated.bind(this), // for re-rendering when input changes
-                  touched: touched, // for formik
-                  errors: errors,  // for formik
-                  handleChange: handleChange // for formik
-                });
-              })}
-              <div>
-                {backbutton}
-                {nextButton}
-              </div>
-            </div>
-          </Form>
-        )}
-      </Formik>
-    );
+  if (model == null) {
+    return <div>loading...</div>;
   }
+
+  let activeFields = (model! as ProposalTemplate).fields.filter(field => {
+    return (
+      field.config.topic === topic &&
+      model.areDependenciesSatisfied(field.proposal_question_id)
+    );
+  });
+
+  let backbutton = back ? <Button onClick={() => back()}>Back</Button> : null;
+  let nextButton = next ? <Button type="submit" variant="contained" color="primary">Next</Button> : null;
+
+  let { initialValues, validationSchema } = createFormikCofigObjects(
+    activeFields
+  );
+
+  return (
+    <Formik
+      initialValues={initialValues}
+      onSubmit={() => {}}
+      validationSchema={Yup.object().shape(validationSchema)}
+    >
+      {({ errors, touched, handleChange, isSubmitting }) => (
+        <Form>
+          {activeFields.map(field => {
+            return (
+              <div className={classes.inputComponentWrapper}>
+                {componentFactory.createComponent(field, {
+                  onComplete: forceUpdate, // for re-rendering when input changes
+                  touched: touched, // for formik
+                  errors: errors, // for formik
+                  handleChange: handleChange // for formik
+                })}
+              </div>
+            );
+          })}
+          <div>
+            {backbutton}
+            {nextButton}
+          </div>
+        </Form>
+      )}
+    </Formik>
+  );
 }
-
-
