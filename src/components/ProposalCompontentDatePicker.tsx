ad61e5706335f6a12f84cc39405cf0bb70663f37
Jekabs Karklins <jekabs.karklins@esss.se>
2019-09-18 11:18:59 +0200

    Fixing bug where date from datepicker was not saved

--- src/components/ProposalCompontentDatePicker.tsx
+++ src/components/ProposalCompontentDatePicker.tsx
@@ -4,39 +4,48 @@ import DateFnsUtils from '@date-io/date-fns';
 import { MuiPickersUtilsProvider, KeyboardDatePicker } from '@material-ui/pickers';
 import { IBasicComponentProps } from "./IBasicComponentProps";
 import { ProposalErrorLabel } from "./ProposalErrorLabel";
+import { Field } from "formik";
 export function ProposalCompontentDatePicker(props: IBasicComponentProps) {
-  let { templateField, onComplete, touched, errors, handleChange } = props;
+  let { templateField, onComplete, touched, errors, handleChange, setFieldValue } = props;
   let { proposal_question_id, config, question } = templateField;
   let isError = (touched[proposal_question_id] && errors[proposal_question_id]) ? true : false;
   if (!templateField.value) {
     templateField.value = new Date();
   }
+
+
   return (
     <FormControl error={isError}>
       <MuiPickersUtilsProvider utils={DateFnsUtils}>
-        <KeyboardDatePicker
-          error={isError}
-          disableToolbar
-          variant="inline"
-          format="dd/MMM/yyyy"
-          id={proposal_question_id}
+        <Field
           name={proposal_question_id}
           label={question}
-          value={templateField.value}
-          onChange={(date, val) => {
-            templateField.value = val;
-            handleChange(val); // letting Formik know that there was a change;
-            onComplete();
-          }}
-          KeyboardButtonProps={{
-            "aria-label": "change date"
-          }}
+          component={
+            ({ field, form, ...other }:{field:any, form:any}) => {
+              const currentError = form.errors[field.name];
+              return (
+                <KeyboardDatePicker
+                  error={isError}
+                  name={field.name}
+                  value={field.value}
+                  format="dd/MMM/yyyy"
+                  helperText={currentError}
+                  label={question}
+                  onChange={
+                    date => {
+                      templateField.value = date;
+                      form.setFieldValue(field.name, date, false);
+                      onComplete();
+                    }
+                  }
+                  {...other}
+                />
+              );
+            }
+          }
         />
       </MuiPickersUtilsProvider>
       <span>{config.small_label}</span>
-      {isError && (
-        <ProposalErrorLabel>{errors[proposal_question_id]}</ProposalErrorLabel>
-      )}
     </FormControl>
   );
 }
