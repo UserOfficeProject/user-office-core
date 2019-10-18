a1f57a39330b23142a49446cd52e2dfa98bca95b
Jekabs Karklins <jekabs.karklins@esss.se>
2019-10-18 11:10:19 +0200

    SWAP-274 Navigating though steps. Adding DB init script

--- db_patches/0001_CreateTopicReadinessState.sql
+++ db_patches/0001_CreateTopicReadinessState.sql
@@ -16,9 +16,9 @@ CREATE TABLE IF NOT EXISTS proposal_topic_completenesses(
 , topic_id INT NOT NULL REFERENCES proposal_topics(topic_id)
 , is_complete BOOLEAN NOT NULL DEFAULT FALSE
 , PRIMARY KEY (proposal_id, topic_id)
-)
+);
 
 CREATE 
     INDEX UX_proposal_topic_completenesses_proposal_id  
 ON 
-    proposal_topic_completenesses(proposal_id) 
+    proposal_topic_completenesses(proposal_id);
