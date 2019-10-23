f801a2329ce025d97ce33b9813373a6eb32e61b5
Fredrik Bolmsten <fredrik.bolmsten@esss.se>
2019-10-23 14:27:33 +0200

    remove console log

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -156,7 +156,6 @@ export default function SignUp(props) {
   if (authCodeOrcID && loading) {
     return <p>loading</p>;
   }
-  console.log(orcData);
   return (
     <Container component="main" maxWidth="xs">
       <Formik
