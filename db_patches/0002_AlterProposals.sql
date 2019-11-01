b67f9497288fe42b7a6447841c68ea0465d653d9
Jekabs Karklins <jekabs.karklins@esss.se>
2019-11-01 11:13:41 +0100

    Using 6 digit integer as code

--- db_patches/0002_AlterProposals.sql
+++ db_patches/0002_AlterProposals.sql
@@ -1,3 +1,6 @@
+/*
+-- Using AirLine sytax e.g. GQX349
+
 CREATE OR REPLACE FUNCTION consistent_shuffle(alphabet TEXT, salt TEXT) RETURNS TEXT AS $$
 DECLARE
     SALT_LENGTH INT := length(salt);
@@ -76,7 +79,28 @@ ALTER TABLE proposals
 ADD short_code VARCHAR(50) NOT NULL UNIQUE default generate_airline_code(CAST (nextval('proposals_short_code_seq') AS INTEGER));
 
 CREATE UNIQUE INDEX proposals_short_code_idx ON proposals(short_code);
+*/
+
+
+-- Usuing pseudorandom 6 digit integer
+
+CREATE SEQUENCE proposals_short_code_seq MAXVALUE 999999 MINVALUE 1 START 1;
+
+CREATE OR REPLACE FUNCTION generate_proposal_shortcode(id BIGINT) RETURNS TEXT AS $$
+DECLARE
+	max_shortcode INT := 999999;
+	coprime INT := 567122; -- this will give good random feel
+	new_shortcode TEXT;
+BEGIN
+	new_shortcode := CAST(coprime * id % max_shortcode as TEXT); 
+    new_shortcode := LPAD(new_shortcode, 6, '0');
+    RETURN new_shortcode;
+END;
+$$ LANGUAGE plpgsql VOLATILE;
 
 
 
+ALTER TABLE proposals
+ADD short_code VARCHAR(8) NOT NULL UNIQUE default generate_proposal_shortcode(nextval('proposals_short_code_seq'));
 
+CREATE UNIQUE INDEX proposals_short_code_idx ON proposals(short_code);
