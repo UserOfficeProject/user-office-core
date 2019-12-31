8ae22e7ece7b19d475ff9e20a429e5f7d37bdc42
Jekabs Karklins <jekabs.karklins@esss.se>
2019-12-31 15:02:28 +0100

    Updating topic order

---
+++ src/resolvers/mutations/UpdateTopicOrderMutation.ts
@@ -0,0 +1,35 @@
+import {
+  Arg,
+  Ctx,
+  Field,
+  Int,
+  Mutation,
+  ObjectType,
+  Resolver
+} from "type-graphql";
+import { ResolverContext } from "../../context";
+import { AbstractResponseWrap, wrapResponse } from "../Utils";
+
+@ObjectType()
+class UpdateTopicOrderResponseWrap extends AbstractResponseWrap<number[]> {
+  @Field(() => [Int], { nullable: true })
+  public topicOrder: number[];
+
+  setValue(value: number[]): void {
+    this.topicOrder = value;
+  }
+}
+const wrap = wrapResponse<number[]>(new UpdateTopicOrderResponseWrap());
+
+@Resolver()
+export class UpdateTopicOrderMutation {
+  @Mutation(() => UpdateTopicOrderResponseWrap, { nullable: true })
+  UpdateTopicOrder(
+    @Arg("topicOrder", () => [Int]) topicOrder: number[],
+    @Ctx() context: ResolverContext
+  ) {
+    return wrap(
+      context.mutations.template.updateTopicOrder(context.user, topicOrder)
+    );
+  }
+}
