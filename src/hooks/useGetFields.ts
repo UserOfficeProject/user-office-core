d1bdc2b0a1be597a54ca74572cf04ee47a029c65
Jekabs Karklins <jekabs.karklins@esss.se>
2020-01-13 16:04:36 +0100

    SWAP-502 Get fields

--- src/hooks/useGetFields.ts
+++ src/hooks/useGetFields.ts
@@ -1,30 +1,19 @@
 import { useEffect, useState } from "react";
-import { request } from "graphql-request";
+import { GetFieldsQuery } from "../generated/sdk";
+import { useDataApi2 } from "./useDataApi2";
 
 export function useGetFields() {
-  const [fieldContent, setFieldContent] = useState<{
-    institutions: { id: number; value: string }[];
-    nationalities: { id: number; value: string }[];
-  } | null>(null);
+  const sendRequest = useDataApi2();
+  const [fieldContent, setFieldContent] = useState<
+    GetFieldsQuery["getFields"] | null
+  >(null);
   useEffect(() => {
-    const query = `
-    query {
-      getFields{
-        institutions{
-            id
-            value
-          }
-          nationalities{
-            id
-            value
-          }
-      }
-    }`;
-
-    request("/graphql", query).then(data => {
-      setFieldContent(data.getFields);
-    });
-  }, []);
+    sendRequest()
+      .getFields()
+      .then(data => {
+        setFieldContent(data.getFields);
+      });
+  }, [sendRequest]);
 
   return fieldContent;
 }
