dd2e26e4702a002645f0693113571c2255c95f65
Jekabs Karklins <jekabs.karklins@esss.se>
2020-01-13 16:23:44 +0100

    SWAP-502 Get Orcid information

---
+++ src/hooks/useOrcIDInformation.ts
@@ -0,0 +1,24 @@
+import { useEffect, useState } from "react";
+import { useDataApi2 } from "./useDataApi2";
+import { GetOrcIdInformationQuery } from "../generated/sdk";
+
+export function useOrcIDInformation(authorizationCode: string) {
+  const sendRequest = useDataApi2();
+  const [orcData, setOrcData] = useState<
+    GetOrcIdInformationQuery["getOrcIDInformation"]
+  >(null);
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    if (authorizationCode === undefined) {
+      return;
+    }
+    sendRequest()
+      .getOrcIDInformation({ authorizationCode })
+      .then(data => {
+        setOrcData(data.getOrcIDInformation);
+        setLoading(false);
+      });
+  }, [authorizationCode, sendRequest]);
+
+  return { loading, orcData };
+}
