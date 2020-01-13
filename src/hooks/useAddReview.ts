9962fa9db3c8f84eff3cb9470c7e9ebcaca5a614
Jekabs Karklins <jekabs.karklins@esss.se>
2020-01-13 11:14:11 +0100

    Add review

--- src/hooks/useAddReview.ts
+++ src/hooks/useAddReview.ts
@@ -1,32 +1,18 @@
 import { useCallback } from "react";
-import { useDataAPI } from "./useDataAPI";
-import { Review } from "../models/Review";
+import { useDataApi2 } from "./useDataApi2";
 
 export function useAddReview() {
-  const sendRequest = useDataAPI<{ review: Review; error: string }>();
+  const sendRequest = useDataApi2();
 
   const sendAddReview = useCallback(
     async (reviewID, grade, comment) => {
-      const query = `
-    mutation($reviewID: Int!, $grade: Int!, $comment: String!) {
-      addReview(reviewID: $reviewID, grade: $grade, comment: $comment){
-        error
-        review {
-          id
-          status
-          comment
-          grade
-        }
-      }
-    }
-    `;
       const variables = {
         reviewID,
         grade,
         comment
       };
 
-      return await sendRequest(query, variables).then(resp => resp);
+      return await sendRequest.addReview(variables).then(resp => resp);
     },
     [sendRequest]
   );
