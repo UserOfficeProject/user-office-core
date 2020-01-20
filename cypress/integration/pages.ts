de2c5feeeaaf766b4566bb7d3d058963e4fbac25
Jekabs Karklins <jekabs.karklins@esss.se>
2020-01-20 15:22:33 +0100

    SWAP-513 E2E test for pages

---
+++ cypress/integration/pages.ts
@@ -0,0 +1,50 @@
+/// <reference types="Cypress" />
+var faker = require("faker");
+
+context("Page tests", () => {
+  beforeEach(() => {
+    cy.visit("localhost:3000");
+    cy.viewport(1100, 1000);
+  });
+
+  afterEach(() => {
+    cy.wait(2000);
+  });
+
+  const faqContents = faker.random.words(50);
+  const ADMIN_EMAIL = "Aaron_Harris49@gmail.com";
+  const ADMIN_PASS = "Test1234!";
+
+  it("Should be able update FAQ", () => {
+    cy.get("[data-cy=input-email] input")
+      .type(ADMIN_EMAIL)
+      .should("have.value", ADMIN_EMAIL);
+
+    cy.get("[data-cy=input-password] input")
+      .type(ADMIN_PASS)
+      .should("have.value", ADMIN_PASS);
+
+    cy.get("[data-cy=submit]").click();
+
+    cy.contains("Edit Pages").click();
+
+    cy.contains("Set user homepage");
+    cy.contains("Set help page");
+    cy.contains("Set privacy agreement");
+    cy.contains("Set cookie policy");
+
+    cy.window().then(win => {
+      cy.wait(1500).then(() => {
+        win.tinyMCE.editors[0].setContent(faqContents); // faq page editor
+        cy.get("#help-update-btn").click({ force: true });
+
+        cy.reload();
+        cy.contains("View Proposals").click();
+        cy.contains("FAQ").click();
+        cy.contains(faqContents);
+        cy.contains("Close").click();
+        cy.contains("Logout").click();
+      });
+    });
+  });
+});
