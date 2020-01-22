627822a24c4a14e94d6014a3b069a75ab7c2bec6
Jekabs Karklins <jekabs.karklins@esss.se>
2020-01-22 13:19:57 +0100

    SWAP-500 Adjusting people table for API2

--- src/components/PeopleTable.js
+++ src/components/PeopleTable.js
@@ -1,31 +1,18 @@
-import React, { useState } from "react";
-import MaterialTable from "material-table";
-import { tableIcons } from "../utils/tableIcons";
-import { useDataAPI } from "../hooks/useDataAPI";
-import { makeStyles } from "@material-ui/styles";
 import { Email } from "@material-ui/icons";
+import { makeStyles } from "@material-ui/styles";
+import MaterialTable from "material-table";
+import React, { useState } from "react";
+import { useDataApi2 } from "../hooks/useDataApi2";
+import { tableIcons } from "../utils/tableIcons";
 import { InviteUserForm } from "./InviteUserForm";
 
 function sendUserRequest(
   searchQuery,
-  apiCall,
+  api,
   setLoading,
   selectedUsers,
   usersOnly
 ) {
-  const query = `
-  query($filter: String!, $first: Int!, $offset: Int!, $usersOnly: Boolean, $subtractUsers: [Int!]) {
-    users(filter: $filter, first: $first, offset: $offset, usersOnly: $usersOnly, subtractUsers: $subtractUsers){
-      users{
-      firstname
-      lastname
-      organisation
-      id
-      }
-      totalCount
-    }
-  }`;
-
   const variables = {
     filter: searchQuery.search,
     offset: searchQuery.pageSize * searchQuery.page,
@@ -34,25 +21,27 @@ function sendUserRequest(
     subtractUsers: selectedUsers ? selectedUsers.map(user => user.id) : []
   };
   setLoading(true);
-  return apiCall(query, variables).then(data => {
-    setLoading(false);
-    return {
-      page: searchQuery.page,
-      totalCount: data.users.totalCount,
-      data: data.users.users.map(user => {
-        return {
-          firstname: user.firstname,
-          lastname: user.lastname,
-          organisation: user.organisation,
-          id: user.id
-        };
-      })
-    };
-  });
+  return api()
+    .getUsers(variables)
+    .then(data => {
+      setLoading(false);
+      return {
+        page: searchQuery.page,
+        totalCount: data.users.totalCount,
+        data: data.users.users.map(user => {
+          return {
+            firstname: user.firstname,
+            lastname: user.lastname,
+            organisation: user.organisation,
+            id: user.id
+          };
+        })
+      };
+    });
 }
 
 function PeopleTable(props) {
-  const sendRequest = useDataAPI();
+  const sendRequest = useDataApi2();
   const [loading, setLoading] = useState(false);
   const [sendUserEmail, setSendUserEmail] = useState(false);
   const columns = [
