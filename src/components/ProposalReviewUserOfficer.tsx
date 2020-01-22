4c83e0668cbe4683e3b7ca538a19e43519f00737
Fredrik Bolmsten <fredrik.bolmsten@esss.se>
2020-01-22 13:05:21 +0100

    Add tabs component

--- src/components/ProposalReviewUserOfficer.tsx
+++ src/components/ProposalReviewUserOfficer.tsx
@@ -1,40 +1,18 @@
 import React, { useState, useEffect } from "react";
-import { makeStyles } from "@material-ui/core/styles";
 import ReviewTable from "./ReviewTable";
 import { useProposalData } from "../hooks/useProposalData";
 import { useAddUserForReview } from "../hooks/useAddUserForReview";
 import { useRemoveUserForReview } from "../hooks/useRemoveUserForReview";
-
+import SimpleTabs from "./TabPanel";
 import { Add } from "@material-ui/icons";
 import ParticipantModal from "./ParticipantModal";
 import Container from "@material-ui/core/Container";
-import Paper from "@material-ui/core/Paper";
 import PeopleTable from "./PeopleTable";
 import ProposaQuestionaryReview from "./ProposalQuestionaryReview";
+import ProposalScore from "./ProposalScore";
 
-const useStyles = makeStyles(theme => ({
-  paper: {
-    marginTop: theme.spacing(3),
-    marginBottom: theme.spacing(3),
-    padding: theme.spacing(2),
-    [theme.breakpoints.up(600 + theme.spacing(3) * 2)]: {
-      marginTop: theme.spacing(6),
-      marginBottom: theme.spacing(6),
-      padding: theme.spacing(3)
-    }
-  },
-  buttons: {
-    display: "flex",
-    justifyContent: "flex-end"
-  },
-  button: {
-    marginTop: "25px",
-    marginLeft: "10px"
-  }
-}));
 
 export default function ProposalReview({ match }: { match: any }) {
-  const classes = useStyles();
   const [modalOpen, setOpen] = useState(false);
   const [reviewers, setReviewers] = useState<any>([]);
   const { proposalData } = useProposalData(parseInt(match.params.id));
@@ -74,21 +52,19 @@ export default function ProposalReview({ match }: { match: any }) {
   if (!proposalData) {
     return <p>Loading</p>;
   }
-
   return (
     <Container maxWidth="lg">
-      <Paper className={classes.paper}>
+      <SimpleTabs tabNames={["Information", "Reviews/Reviewers", "Technical", "Excellence", "Safety"]}>
         <ProposaQuestionaryReview data={proposalData} />
-      </Paper>
-      <Paper className={classes.paper}>
+        <>
         <ReviewTable reviews={proposalData.reviews} />
-      </Paper>
-      <Paper className={classes.paper}>
         <ParticipantModal
           show={modalOpen}
           close={setOpen}
           addParticipant={addUser}
+          selectedUsers={reviewers}
           title={"Add Reviewer"}
+          userRole={"REVIEWER"}
         />
         <PeopleTable
           title="Reviewers"
@@ -99,8 +75,9 @@ export default function ProposalReview({ match }: { match: any }) {
           search={false}
           onRemove={removeUser}
           disabled={true}
-        />
-      </Paper>
+        /></>
+        <ProposalScore technicalScore={proposalData.technicalScore} safetyScore={proposalData.safetyScore} excellenceScore={proposalData.excellenceScore} proposalId={proposalData.id} />
+      </SimpleTabs>
     </Container>
   );
 }
