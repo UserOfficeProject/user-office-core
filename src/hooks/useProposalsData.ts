255bc462eb19b67e56983e8fdbb0256feecb7a5f
Jekabs Karklins <jekabs.karklins@esss.se>
2020-02-03 15:08:33 +0100

    SWAP-558 Simplify

--- src/hooks/useProposalsData.ts
+++ src/hooks/useProposalsData.ts
@@ -9,29 +9,26 @@ export function useProposalsData(filter: string) {
   );
   const [loading, setLoading] = useState(true);
   useEffect(() => {
-    const sendAllProposalRequest = (filter: string) => {
-      api()
-        .getProposals({
-          filter: filter
-        })
-        .then(data => {
-          if (data.proposals) {
-            setProposalsData(
-              data.proposals.proposals.map(proposal => {
-                return {
-                  ...proposal,
-                  status:
-                    proposal.status === ProposalStatus.DRAFT
-                      ? "Open"
-                      : "Submitted"
-                };
-              })
-            );
-          }
-          setLoading(false);
-        });
-    };
-    sendAllProposalRequest(filter);
+    api()
+      .getProposals({
+        filter: filter
+      })
+      .then(data => {
+        if (data.proposals) {
+          setProposalsData(
+            data.proposals.proposals.map(proposal => {
+              return {
+                ...proposal,
+                status:
+                  proposal.status === ProposalStatus.DRAFT
+                    ? "Open"
+                    : "Submitted"
+              };
+            })
+          );
+        }
+        setLoading(false);
+      });
   }, [filter, api]);
 
   return { loading, proposalsData, setProposalsData };
