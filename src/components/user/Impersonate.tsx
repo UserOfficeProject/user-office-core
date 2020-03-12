7c839bea14f417773c3beb280e9cf68e0c686fa2
Martin Trajanovski <martin.trajanovski@sigmatechnology.se>
2020-03-12 11:49:48 +0100

    divide content in user settings

--- src/components/user/Impersonate.tsx
+++ src/components/user/Impersonate.tsx
@@ -1,41 +1,50 @@
-import { Button, Typography } from '@material-ui/core';
+import { Button, Typography, makeStyles } from '@material-ui/core';
 import { useSnackbar } from 'notistack';
 import React, { useContext } from 'react';
 import { useHistory } from 'react-router';
 import { UserContext } from '../../context/UserContextProvider';
 import { useDataApi } from '../../hooks/useDataApi';
 
+const useStyles = makeStyles({
+  buttons: {
+    display: 'flex',
+    justifyContent: 'flex-end',
+  },
+});
+
 export function Impersonate(props: { id: number }) {
   const api = useDataApi();
   const { handleLogin } = useContext(UserContext);
   const { enqueueSnackbar } = useSnackbar();
   const history = useHistory();
+  const classes = useStyles();
 
   return (
     <React.Fragment>
       <Typography variant="h6" gutterBottom>
         Impersonate
       </Typography>
-      <Button
-        variant="contained"
-        color="primary"
-        style={{ display: 'block', margin: '0 auto' }}
-        onClick={() =>
-          api()
-            .getTokenForUser({ userId: props.id })
-            .then(data => {
-              const { token, error } = data.getTokenForUser;
-              if (error) {
-                enqueueSnackbar(error, { variant: 'error' });
-              } else {
-                handleLogin(token);
-                history.push('/home');
-              }
-            })
-        }
-      >
-        Connect as this user...
-      </Button>
+      <div className={classes.buttons}>
+        <Button
+          variant="contained"
+          color="primary"
+          onClick={() =>
+            api()
+              .getTokenForUser({ userId: props.id })
+              .then(data => {
+                const { token, error } = data.getTokenForUser;
+                if (error) {
+                  enqueueSnackbar(error, { variant: 'error' });
+                } else {
+                  handleLogin(token);
+                  history.push('/home');
+                }
+              })
+          }
+        >
+          Connect as this user...
+        </Button>
+      </div>
     </React.Fragment>
   );
 }
--- src/components/user/UserPage.tsx
+++ src/components/user/UserPage.tsx
@@ -6,10 +6,24 @@ import UpdateUserRoles from './UpdateUserRoles';
 import { Impersonate } from './Impersonate';
 import SimpleTabs from '../common/TabPanel';
 import EventLogList from '../eventLog/EventLogList';
-import { Container } from '@material-ui/core';
+import { Container, Divider, makeStyles } from '@material-ui/core';
+import { getTheme } from '../../theme';
+
+const useStyles = makeStyles({
+  divider: {
+    border: 'none',
+    height: '25px',
+    borderBottom: `1px solid ${getTheme().palette.primary.main}`,
+    boxShadow: `0px 5px 10px -10px #000`,
+    marginBottom: '16px',
+    background: 'none',
+  },
+});
 
 function UserPage(props: { match: { params: { id: string } } }) {
   const userId = parseInt(props.match.params.id);
+
+  const classes = useStyles();
 
   return (
     <Container maxWidth="lg">
@@ -17,7 +31,9 @@ function UserPage(props: { match: { params: { id: string } } }) {
         <UpdateUserInformation id={userId} />
         <React.Fragment>
           <UpdatePassword id={userId} />
+          <Divider className={classes.divider} />
           <UpdateUserRoles id={userId} />
+          <Divider className={classes.divider} />
           <Impersonate id={userId} />
         </React.Fragment>
         <EventLogList eventType="USER" changedObjectId={userId} />
