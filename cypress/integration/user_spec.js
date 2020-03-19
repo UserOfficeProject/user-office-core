46d68337faa2a531bab29493f9bc5de2f1b39659
Martin Trajanovski <martin.trajanovski@sigmatechnology.se>
2020-03-19 12:49:35 +0100

    fix some cypress type definitions and do not use @ts-ignore anymore

--- cypress/integration/user_spec.js
+++ cypress/integration/user_spec.js
@@ -1,13 +1,13 @@
 /// <reference types="Cypress" />
-const faker = require("faker");
+const faker = require('faker');
 
-context("User tests", () => {
+context('User tests', () => {
   beforeEach(() => {
-    cy.visit("/SignUp?code=WRMVXa");
+    cy.visit('/SignUp?code=WRMVXa');
   });
 
   // Login details
-  const password = "aslaksjdajsl9#ASdADSlk!";
+  const password = 'aslaksjdajsl9#ASdADSlk!';
 
   // Personal details
   const firstName = faker.name.firstName();
@@ -28,102 +28,102 @@ context("User tests", () => {
   const telephone = faker.phone.phoneNumber();
   const telephoneAlt = faker.phone.phoneNumber();
 
-  it("A user should be able to create a new account", () => {
-    cy.get("[data-cy=email] input")
+  it('A user should be able to create a new account', () => {
+    cy.get('[data-cy=email] input')
       .type(email)
-      .should("have.value", email);
+      .should('have.value', email);
 
-    cy.get("[data-cy=password] input")
+    cy.get('[data-cy=password] input')
       .type(password)
-      .should("have.value", password);
+      .should('have.value', password);
 
-    cy.get("[data-cy=confirmPassword] input")
+    cy.get('[data-cy=confirmPassword] input')
       .type(password)
-      .should("have.value", password);
+      .should('have.value', password);
 
     // Personal details
-    cy.get("#mui-component-select-user_title").click();
-    cy.contains("Prof.").click();
-    cy.get("[data-cy=firstname] input")
+    cy.get('#mui-component-select-user_title').click();
+    cy.contains('Prof.').click();
+    cy.get('[data-cy=firstname] input')
       .clear()
       .type(firstName)
-      .should("have.value", firstName);
+      .should('have.value', firstName);
 
-    cy.get("[data-cy=middlename] input")
+    cy.get('[data-cy=middlename] input')
       .type(middleName)
-      .should("have.value", middleName);
-    cy.get("[data-cy=lastname] input")
+      .should('have.value', middleName);
+    cy.get('[data-cy=lastname] input')
       .clear()
       .type(lastName)
-      .should("have.value", lastName);
-    cy.get("[data-cy=preferredname] input")
+      .should('have.value', lastName);
+    cy.get('[data-cy=preferredname] input')
       .type(firstName)
-      .should("have.value", firstName);
+      .should('have.value', firstName);
 
-    cy.get("#mui-component-select-gender").click();
+    cy.get('#mui-component-select-gender').click();
 
-    cy.contains("Male").click();
+    cy.contains('Male').click();
 
-    cy.get("#mui-component-select-nationality").click();
+    cy.get('#mui-component-select-nationality').click();
 
-    cy.contains("Swedish").click();
+    cy.contains('Swedish').click();
 
-    cy.get("[data-cy=birthdate] input")
+    cy.get('[data-cy=birthdate] input')
       .type(birthDate)
-      .should("have.value", birthDate);
+      .should('have.value', birthDate);
 
     //Organization details
-    cy.get("#mui-component-select-organisation").click();
+    cy.get('#mui-component-select-organisation').click();
 
-    cy.contains("Lund University").click();
+    cy.contains('Lund University').click();
 
-    cy.get("[data-cy=department] input")
+    cy.get('[data-cy=department] input')
       .type(department)
-      .should("have.value", department);
+      .should('have.value', department);
 
-    cy.get("[data-cy=position] input")
+    cy.get('[data-cy=position] input')
       .type(position)
-      .should("have.value", position);
+      .should('have.value', position);
 
     //Contact details
 
-    cy.get("[data-cy=telephone] input")
+    cy.get('[data-cy=telephone] input')
       .type(telephone)
-      .should("have.value", telephone);
+      .should('have.value', telephone);
 
-    cy.get("[data-cy=telephone-alt] input")
+    cy.get('[data-cy=telephone-alt] input')
       .type(telephoneAlt)
-      .should("have.value", telephoneAlt);
+      .should('have.value', telephoneAlt);
 
-    cy.get("[data-cy=privacy-agreement] input").click();
+    cy.get('[data-cy=privacy-agreement] input').click();
 
-    cy.get("[data-cy=cookie-policy] input").click();
+    cy.get('[data-cy=cookie-policy] input').click();
 
     //Submit
-    cy.get("[data-cy=submit]").click();
+    cy.get('[data-cy=submit]').click();
 
-    cy.contains("Click here for sign in").click();
+    cy.contains('Click here for sign in').click();
     //Check redirect to Sign in page
-    cy.contains("Sign in");
+    cy.contains('Sign in');
   });
 
-  it("A user should be able to login and out", () => {
-    cy.contains("Have an account? Sign In").click();
+  it('A user should be able to login and out', () => {
+    cy.contains('Have an account? Sign In').click();
 
-    cy.get("[data-cy=input-email] input")
+    cy.get('[data-cy=input-email] input')
       .type(email)
-      .should("have.value", email);
+      .should('have.value', email);
 
-    cy.get("[data-cy=input-password] input")
+    cy.get('[data-cy=input-password] input')
       .type(password)
-      .should("have.value", password);
+      .should('have.value', password);
 
-    cy.get("[data-cy=submit]").click();
+    cy.get('[data-cy=submit]').click();
 
-    cy.contains("Your proposals");
+    cy.contains('Your proposals');
 
-    cy.contains("Logout").click();
+    cy.contains('Logout').click();
 
-    cy.contains("Sign in");
+    cy.contains('Sign in');
   });
 });
