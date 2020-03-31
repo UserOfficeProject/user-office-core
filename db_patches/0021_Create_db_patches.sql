b6095e2239381ffea4ea00157c4c673fa01fc158
Jekabs Karklins <jekabs.karklins@esss.se>
2020-03-31 09:10:45 +0200

    Adding unregister patch

--- db_patches/0021_Create_db_patches.sql
+++ db_patches/0021_Create_db_patches.sql
@@ -9,17 +9,28 @@ CREATE TABLE db_patches (
 CREATE OR REPLACE FUNCTION register_patch(new_patch_id TEXT, author TEXT, purpose TEXT, created_at TIMESTAMPTZ) RETURNS boolean AS $$
     BEGIN
     IF EXISTS (SELECT 1 FROM db_patches WHERE patch_id=new_patch_id) THEN
-	RAISE NOTICE 'Refusing to apply patch. Patch id % already present', new_patch_id;
+	RAISE WARNING 'Refusing to apply patch. Patch id % already present', new_patch_id;
         return false;
     ELSE 
-	RAISE NOTICE 'Applying %', v_job_id;
         INSERT INTO db_patches VALUES(new_patch_id, author, purpose, created_at);
-        RAISE NOTICE 'Patch % successfully applied', new_patch_id;
         return true;
     END IF;
     END
 $$ LANGUAGE plpgsql;
 
+
+CREATE OR REPLACE FUNCTION unregister_patch(new_patch_id TEXT) RETURNS boolean AS $$
+    BEGIN
+    IF EXISTS (SELECT 1 FROM db_patches WHERE patch_id=new_patch_id) THEN
+	RAISE NOTICE 'Unergistering pathc %', new_patch_id;
+	DELETE FROM db_patches WHERE patch_id=new_patch_id;
+        return true;
+    ELSE 
+        RAISE NOTICE 'Cant unregister. Patch % does not exist', new_patch_id;
+        return false;
+    END IF;
+    END
+$$ LANGUAGE plpgsql;
 
 DO
 $$
