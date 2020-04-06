f11b7059ca9fd021932dd6fe9e15062bea3d77ab
Martin Trajanovski <martin.trajanovski@sigmatechnology.se>
2020-04-06 15:30:09 +0200

    fix linting errors

--- src/components/review/TechnicalReviewInformation.tsx
+++ src/components/review/TechnicalReviewInformation.tsx
@@ -1,54 +1,54 @@
+import { getTranslation } from '@esss-swap/duo-localisation';
 import {
-    Table,
-    TableRow,
-    TableCell,
-    TableBody,
-    Typography,
-    makeStyles,
-  } from '@material-ui/core';
-  import { getTranslation } from '@esss-swap/duo-localisation';
-  import React, { Fragment, HTMLAttributes} from 'react';
-  
-  import { TechnicalReview } from '../../generated/sdk';
+  Table,
+  TableRow,
+  TableCell,
+  TableBody,
+  Typography,
+  makeStyles,
+} from '@material-ui/core';
+import React, { Fragment, HTMLAttributes } from 'react';
 
-  
-  export default function TechnicalReviewInformation(
-    props: HTMLAttributes<any> & {
-      data: TechnicalReview | null | undefined;
-    }
-  ) {
-    const classes = makeStyles(theme => ({
-        heading: {
-          marginTop: theme.spacing(2),
-        },
-      }))();
+import { TechnicalReview } from '../../generated/sdk';
 
-    if(!props.data){
-        return <p>Can't find technical review</p>
-    }
-
-    return (
-      <Fragment>
-        <Typography variant="h6" className={classes.heading} gutterBottom>
-          Tehnical Reivew
-        </Typography>
-        <Table>
-          <TableBody>
-            <TableRow key="status">
-              <TableCell>Status</TableCell>
-              <TableCell>{props.data.status && getTranslation(props.data.status)}</TableCell>
-            </TableRow>
-            <TableRow key="comment">
-              <TableCell>Comment</TableCell>
-              <TableCell>{props.data.publicComment}</TableCell>
-            </TableRow>
-            <TableRow key="timeAllocation">
-              <TableCell>Time Allocation</TableCell>
-              <TableCell>{props.data.timeAllocation}</TableCell>
-            </TableRow>
-          </TableBody>
-        </Table>
-      </Fragment>
-    );
+export default function TechnicalReviewInformation(
+  props: HTMLAttributes<any> & {
+    data: TechnicalReview | null | undefined;
   }
-  
+) {
+  const classes = makeStyles(theme => ({
+    heading: {
+      marginTop: theme.spacing(2),
+    },
+  }))();
+
+  if (!props.data) {
+    return <p>Can&apos;t find technical review</p>;
+  }
+
+  return (
+    <Fragment>
+      <Typography variant="h6" className={classes.heading} gutterBottom>
+        Tehnical Reivew
+      </Typography>
+      <Table>
+        <TableBody>
+          <TableRow key="status">
+            <TableCell>Status</TableCell>
+            <TableCell>
+              {props.data.status && getTranslation(props.data.status)}
+            </TableCell>
+          </TableRow>
+          <TableRow key="comment">
+            <TableCell>Comment</TableCell>
+            <TableCell>{props.data.publicComment}</TableCell>
+          </TableRow>
+          <TableRow key="timeAllocation">
+            <TableCell>Time Allocation</TableCell>
+            <TableCell>{props.data.timeAllocation}</TableCell>
+          </TableRow>
+        </TableBody>
+      </Table>
+    </Fragment>
+  );
+}
