35e92997f62e44d45516228ad49e8c27a6e2d4d2
Fredrik Bolmsten <fredrik.bolmsten@esss.se>
2020-05-14 08:30:20 +0200

    Add new view for proposal administration

--- src/components/proposal/ProposalAdmin.tsx
+++ src/components/proposal/ProposalAdmin.tsx
@@ -1,43 +1,57 @@
 import Button from '@material-ui/core/Button';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
-import { Formik, Form } from 'formik';
+import { Formik, Form, Field } from 'formik';
+import { TextField } from 'formik-material-ui';
 import { useSnackbar } from 'notistack';
 import React, { Fragment } from 'react';
 import * as Yup from 'yup';
 
-import { ProposalEndStatus } from '../../generated/sdk';
+import { Proposal } from '../../generated/sdk';
+import { ProposalEndStatus, ProposalStatus } from '../../generated/sdk';
 import { useDataApi } from '../../hooks/useDataApi';
 import { ButtonContainer } from '../../styles/StyledComponents';
 import FormikDropdown from '../common/FormikDropdown';
 
-export default function ProposalAdmin(props: { id: number }) {
+export default function ProposalAdmin(props: { data: Proposal }) {
   const api = useDataApi();
   const { enqueueSnackbar } = useSnackbar();
+
+  const initialValues = {
+    finalStatus: props.data.finalStatus || ProposalEndStatus.UNSET,
+    proposalStatus: props.data.status,
+    commentForUser: props.data.commentForUser,
+    commentForManagement: props.data.commentForManagement,
+  };
 
   return (
     <Fragment>
       <Typography variant="h6" gutterBottom>
-        Admin
+        Administration
       </Typography>
       <Formik
-        initialValues={{
-          finalStatus: ProposalEndStatus.ACCEPTED,
-        }}
+        initialValues={initialValues}
         validationSchema={Yup.object().shape({
-          finalStatus: Yup.string().required('status is required'),
+          status: Yup.string().nullable(),
+          timeAllocation: Yup.number().nullable(),
+          comment: Yup.string().nullable(),
+          publicComment: Yup.string().nullable(),
         })}
         onSubmit={async (values, actions) => {
-          console.log(values);
-          api()
-            .updateProposal({
-              id: props.id,
+          await api()
+            .administrationProposal({
+              id: props.data.id,
               finalStatus:
                 ProposalEndStatus[values.finalStatus as ProposalEndStatus],
+              status: ProposalStatus[values.proposalStatus as ProposalStatus],
+              commentForUser: values.commentForUser,
+              commentForManagement: values.commentForManagement,
             })
             .then(data =>
               enqueueSnackbar('Updated', {
-                variant: data.updateProposal.error ? 'error' : 'success',
+                variant: data.administrationProposal.error
+                  ? 'error'
+                  : 'success',
               })
             );
           actions.setSubmitting(false);
@@ -51,11 +65,54 @@ export default function ProposalAdmin(props: { id: number }) {
                   name="finalStatus"
                   label="Final status"
                   items={[
+                    { text: 'Unset', value: ProposalEndStatus.UNSET },
                     { text: 'Accepted', value: ProposalEndStatus.ACCEPTED },
                     { text: 'Reserved', value: ProposalEndStatus.RESERVED },
                     { text: 'Rejected', value: ProposalEndStatus.REJECTED },
                   ]}
                   required
+                />
+              </Grid>
+              <Grid item xs={6}>
+                <FormikDropdown
+                  name="proposalStatus"
+                  label="Proposal status"
+                  items={[
+                    { text: '', value: ProposalStatus.BLANK },
+                    { text: 'Draft', value: ProposalStatus.DRAFT },
+                    { text: 'Submitted', value: ProposalStatus.SUBMITTED },
+                  ]}
+                  required
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <Field
+                  name="commentForUser"
+                  label="Comment for user"
+                  type="text"
+                  component={TextField}
+                  margin="normal"
+                  fullWidth
+                  autoComplete="off"
+                  data-cy="comment"
+                  multiline
+                  rowsMax="16"
+                  rows="4"
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <Field
+                  name="commentForManagement"
+                  label="Comment for management"
+                  type="text"
+                  component={TextField}
+                  margin="normal"
+                  fullWidth
+                  autoComplete="off"
+                  data-cy="publicComment"
+                  multiline
+                  rowsMax="16"
+                  rows="4"
                 />
               </Grid>
             </Grid>
