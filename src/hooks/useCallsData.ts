7e2f6400a6835f12a080cfa513e0647996f74495
Jekabs Karklins <jekabskarklins@esss.se>
2020-05-18 13:45:29 +0200

    Regression bugfix, user can create proposal for inactive call

--- src/hooks/useCallsData.ts
+++ src/hooks/useCallsData.ts
@@ -1,36 +1,21 @@
 import { useEffect, useState } from 'react';
-
 import { GetCallsQuery, GetCallsQueryVariables } from '../generated/sdk';
 import { useDataApi } from './useDataApi';
 
-export function useCallsData(show: boolean, filter: GetCallsQueryVariables) {
+export function useCallsData(show: boolean, filter?: GetCallsQueryVariables) {
   const [callsData, setCallsData] = useState<GetCallsQuery['calls'] | null>();
   const [loading, setLoading] = useState(true);
 
   const api = useDataApi();
 
-  /**
-   * NOTE: Fix api calls loop!
-   * When we put filter as an object in the dependency array it is never equal to previous object.
-   * This is how object equality works.
-   * Comparing objects is all about the place in the computer memory where they are stored, not about the value they represent.
-   */
-  const templateIds = filter.filter?.templateIds;
-  const isActive = filter.filter?.isActive;
-
   useEffect(() => {
     api()
-      .getCalls({
-        filter: {
-          templateIds,
-          isActive,
-        },
-      })
+      .getCalls(filter)
       .then(data => {
         setCallsData(data.calls);
         setLoading(false);
       });
-  }, [api, show, templateIds, isActive]);
+  }, [api, show, filter]);
 
-  return { loading, callsData };
+  return { loading, callsData, filter };
 }
