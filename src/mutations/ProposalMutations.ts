8f95a4f0338b60f097676c8ee124c46f34bb0ab5
Fredrik Bolmsten <fredrik.bolmsten@esss.se>
2020-05-26 10:38:06 +0200

    check if finalStatus is set

--- src/mutations/ProposalMutations.ts
+++ src/mutations/ProposalMutations.ts
@@ -268,7 +268,7 @@ export default class ProposalMutations {
   async notify(user: User | null, args: NotifyProposalArgs): Promise<unknown> {
     const proposal = await this.proposalDataSource.get(args.id);
 
-    if (!proposal || proposal.notified) {
+    if (!proposal || proposal.notified || !proposal.finalStatus) {
       return rejection('INTERNAL_ERROR');
     }
     proposal.notified = true;
