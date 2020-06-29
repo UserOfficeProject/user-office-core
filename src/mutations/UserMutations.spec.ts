a0f871dfe4ea5737750283a7a247f52b5f1f73c8
Jekabs Karklins <jekabskarklins@esss.se>
2020-06-29 13:11:03 +0200

    test

--- src/mutations/UserMutations.spec.ts
+++ src/mutations/UserMutations.spec.ts
@@ -11,11 +11,12 @@ import {
   dummyUserNotOnProposalWithRole,
   dummyUserOfficerWithRole,
 } from '../datasources/mockups/UserDataSource';
+
+import UserMutations from './UserMutations';
 import { EmailInviteResponse } from '../models/EmailInviteResponse';
 import { BasicUserDetails, UserRole } from '../models/User';
 import { isRejection } from '../rejection';
 import { UserAuthorization } from '../utils/UserAuthorization';
-import UserMutations from './UserMutations';
 
 const secret = process.env.secret as string;
 
@@ -28,7 +29,7 @@ const goodToken = jsonwebtoken.sign(
   secret,
   { expiresIn: '24h' }
 );
-
+asd;
 const badToken = jsonwebtoken.sign(
   {
     id: dummyUser.id,
