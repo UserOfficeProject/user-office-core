fed48913b586a315b8a2fd97f33e504a43e4bff5
Jekabs Karklins <jekabskarklins@esss.se>
2020-07-17 09:25:07 +0200

    Reorder fields in DB table

--- db_patches/0050_CreateSamplesTable.sql
+++ db_patches/0050_CreateSamplesTable.sql
@@ -6,8 +6,8 @@ BEGIN
 
       CREATE TABLE samples (
             sample_id serial PRIMARY KEY
-          , creator_id int REFERENCES users (user_id)
           , title varchar(100)
+          , creator_id int REFERENCES users (user_id)
           , questionary_id int REFERENCES questionaries (questionary_id) ON DELETE CASCADE
           , status int NOT NULL DEFAULT 0
           , created_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
