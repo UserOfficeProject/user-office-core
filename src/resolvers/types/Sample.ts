b9cd15719df81f89c40daf125f27e406ecf9846a
jekabskarklins <lucie@fbeaveraqb.com>
2020-08-11 14:12:58 +0200

    allowing to get questionary for sample within same service call

--- src/resolvers/types/Sample.ts
+++ src/resolvers/types/Sample.ts
@@ -1,6 +1,16 @@
-import { ObjectType, Field, Int } from 'type-graphql';
+import {
+  ObjectType,
+  Field,
+  Int,
+  Resolver,
+  FieldResolver,
+  Root,
+  Ctx,
+} from 'type-graphql';
 
 import { Sample as SampleOrigin, SampleStatus } from '../../models/Sample';
+import { Questionary } from './Questionary';
+import { ResolverContext } from '../../context';
 @ObjectType()
 export class Sample implements Partial<SampleOrigin> {
   @Field(() => Int)
@@ -20,4 +30,18 @@ export class Sample implements Partial<SampleOrigin> {
 
   @Field(() => Date)
   public created: Date;
+}
+
+@Resolver(() => Sample)
+export class SampleResolver {
+  @FieldResolver(() => Questionary)
+  async questionary(
+    @Root() sample: Sample,
+    @Ctx() context: ResolverContext
+  ): Promise<Questionary | null> {
+    return context.queries.questionary.getQuestionary(
+      context.user,
+      sample.questionaryId
+    );
+  }
 }
