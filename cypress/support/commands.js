c97f4097a90ce962a15688d12e2a11c0ee885196
Martin Trajanovski <martin.trajanovski@ess.eu>
2020-10-14 08:39:12 +0200

    check for notification variant class

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -81,21 +81,28 @@ const logout = () => {
 const notification = ({ variant, text }) => {
   let notificationQuerySelector = '[role="alert"]';
   let bgColor = '';
+  let classVariantRegEx = '';
 
   switch (variant) {
     case 'error':
-      // notificationQuerySelector = '[class*=" SnackbarItem-variantError-"]';
+      classVariantRegEx = /variantError/;
       bgColor = 'rgb(211, 47, 47)';
       break;
 
     default:
-      // notificationQuerySelector = '[class*=" SnackbarItem-variantSuccess-"]';
+      classVariantRegEx = /variantSuccess/;
       bgColor = 'rgb(67, 160, 71)';
       break;
   }
   let notification = cy
     .get(notificationQuerySelector)
-    .should('exist')
+    .should(div => {
+      expect(div).to.have.length(1);
+
+      const className = div[0].className;
+
+      expect(className).to.match(classVariantRegEx);
+    })
     .and('have.css', 'background-color', bgColor);
 
   if (text) {
