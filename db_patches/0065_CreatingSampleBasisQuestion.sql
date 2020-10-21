539bff42c3ba353d8d977743c56125222b4dd275
jekabskarklins <jekabs.karklins@ess.eu>
2020-10-21 13:25:14 +0200

    Fix DB upgrade patch

--- db_patches/0065_CreatingSampleBasisQuestion.sql
+++ db_patches/0065_CreatingSampleBasisQuestion.sql
@@ -20,7 +20,6 @@ BEGIN
             'sample_basis',
             2
         );
-
     INSERT INTO questions(
             question_id,
             data_type,
@@ -38,16 +37,36 @@ BEGIN
             1
         );
 
-    Update topics set sort_order=sort_order+1;
+    UPDATE topics
+    SET sort_order = sort_order + 1
+    WHERE template_id IN
+        (SELECT template_id
+        FROM templates
+        WHERE category_id = 1 ); /* Make space for new topic at position 0 */
+
 
     INSERT INTO topics(topic_title, is_enabled, sort_order, template_id)
-    SELECT 'New proposal', true, 0, template_id FROM templates;
+    SELECT 'New proposal',
+        TRUE,
+        0,
+        template_id
+    FROM templates
+    WHERE category_id = 1; /* Insert new topic for every proposal template at position 0 */
+ 
 
     INSERT INTO templates_has_questions(question_id, template_id, topic_id, sort_order, config)
-    SELECT 'proposal_basis', template_id, topic_id, 0, '{"required":false,"small_label":"","tooltip":""}'  from topics WHERE sort_order=0;
-
+    SELECT 'proposal_basis',
+        template_id,
+        topic_id,
+        0,
+        '{"required":false,"small_label":"","tooltip":""}'
+    FROM topics
+    WHERE sort_order = 0
+    AND template_id IN
+        (SELECT template_id
+        FROM templates
+        WHERE category_id = 1 ); /* Add one question with ID proposal_basis to newly created topic */
     END;
 	END IF;
 END;
-$$
-LANGUAGE plpgsql;
+$$ LANGUAGE plpgsql;
