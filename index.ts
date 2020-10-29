c023524e48e27f5e832d63d194a90d1542e5e361
Peter Asztalos <peter.asztalos@sigmatechnology.se>
2020-10-29 15:49:37 +0100

    PDF factory added

--- index.ts
+++ index.ts
@@ -1,4 +1,6 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
+import 'dotenv/config';
+
 import cookieParser from 'cookie-parser';
 import express from 'express';
 
@@ -7,6 +9,7 @@ import authorization from './src/middlewares/authorization';
 import exceptionHandler from './src/middlewares/exceptionHandler';
 import files from './src/middlewares/files';
 import apolloServer from './src/middlewares/graphql';
+import pdfFactory from './src/middlewares/pdfFactory';
 import proposalDownload from './src/middlewares/proposalDownload';
 import { logger } from './src/utils/Logger';
 
@@ -18,7 +21,11 @@ async function bootstrap() {
     .use(cookieParser())
     .use(authorization())
     .use(files())
-    .use(proposalDownload())
+    .use(
+      process.env.UO_FEATURE_ENABLE_UO_FACTORY === '1'
+        ? pdfFactory()
+        : proposalDownload()
+    )
     .use(exceptionHandler());
 
   await apolloServer(app);
