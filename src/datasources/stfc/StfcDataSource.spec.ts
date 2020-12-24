bd60ebc06a79711ae02117b526fd215964ed6821
Simon Fernandes <simon.fernandes@stfc.ac.uk>
2020-12-24 14:31:03 +0000

    UserOfficeProject/stfc-user-office-project#45: Add unit tests for STFC data source

---
+++ src/datasources/stfc/StfcDataSource.spec.ts
@@ -0,0 +1,33 @@
+import { StfcDataSource } from './StfcDataSource';
+import { Role } from '../../models/Role';
+import { Roles } from '../../models/Role';
+
+jest.mock('./UOWSSoapInterface');
+
+const dataSource = new StfcDataSource();
+const dummyUserNumber = 12345;
+const userRoleDbId = 0;
+const userOfficerRoleDbId = 2;
+const instrumentScientistRoleDbId = 7;
+
+test('When getting roles for a user, the User role is the first role in the list', async () => {
+    const roles = await dataSource.getUserRoles(dummyUserNumber);
+
+    return expect(
+        roles[0]
+    ).toEqual(
+        expect.objectContaining(
+            new Role(userRoleDbId, Roles.USER, "User")
+        )
+    );
+});
+
+test('When getting roles for a user, STFC roles are translated into ESS roles', async () => {
+    return expect(
+        dataSource.getUserRoles(dummyUserNumber)
+    ).resolves.toEqual([
+        new Role(userRoleDbId, Roles.USER, "User"),
+        new Role(instrumentScientistRoleDbId, Roles.INSTRUMENT_SCIENTIST, "Instrument Scientist"),
+        new Role(userOfficerRoleDbId, Roles.USER_OFFICER, "User Officer")
+    ]);
+});
--- src/datasources/stfc/__mocks__/UOWSSoapInterface.ts
+++ src/datasources/stfc/__mocks__/UOWSSoapInterface.ts
@@ -11,4 +11,37 @@ export default class UOWSSoapClient {
       },
     };
   }
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  public getRolesForUser(token: any, userNumber: any): any {
+    return {
+      return: [    
+        {
+          "name": "ISIS Instrument Scientist",
+        },
+        {
+          "name": "ISIS Administrator",
+        },
+        {
+          "name": "Developer",
+        },
+        {
+          "name": "Admin",
+        },
+        {
+          "name": "ISIS Instrument Scientist",
+        },
+        {
+          "name": "User Officer",
+        },
+        {
+          "name": "User Officer",
+        },
+        {
+          "name": "User",
+        }
+      ]
+    };
+  }
+  
 }
