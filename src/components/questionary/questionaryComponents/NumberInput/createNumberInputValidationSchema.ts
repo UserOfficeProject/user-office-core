6bd443801af3e8e45f439f3a05d29ff1b6f3de5e
Peter Asztalos <169059+peter-asztalos@users.noreply.github.com>
2021-02-05 16:28:13 +0100

    Missing validations added (#225)
    * variable name fixed
    
    * fix variable name

--- src/components/questionary/questionaryComponents/NumberInput/createNumberInputValidationSchema.ts
+++ src/components/questionary/questionaryComponents/NumberInput/createNumberInputValidationSchema.ts
@@ -8,24 +8,24 @@ import { IntervalPropertyId } from '../Interval/intervalUnits';
 export const createNumberInputValidationSchema: QuestionaryComponentDefinition['createYupValidationSchema'] = answer => {
   const config = answer.config as NumberInputConfig;
 
-  let value = Yup.number().transform(value =>
+  let valueScheme = Yup.number().transform(value =>
     isNaN(value) ? undefined : value
   );
 
   if (config.required) {
-    value = value.required('Please fill in');
+    valueScheme = valueScheme.required('Please fill in');
   }
 
   if (config.numberValueConstraint === NumberValueConstraint.ONLY_NEGATIVE) {
-    value = value.negative('Value must be a negative number');
+    valueScheme = valueScheme.negative('Value must be a negative number');
   }
 
   if (config.numberValueConstraint === NumberValueConstraint.ONLY_POSITIVE) {
-    value = value.positive('Value must be a positive number');
+    valueScheme = valueScheme.positive('Value must be a positive number');
   }
 
   return Yup.object().shape({
-    value,
+    value: valueScheme,
     unit:
       config.property !== IntervalPropertyId.UNITLESS
         ? Yup.string().required('Please specify unit')
