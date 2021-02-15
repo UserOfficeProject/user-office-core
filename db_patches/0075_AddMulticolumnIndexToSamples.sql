d21ff3b6992126bad5ece958f066b662fd2fbf67
Martin <martin.trajanovski@ess.eu>
2021-02-15 14:29:30 +0100

    Swap 1362 image captions (#169)
    * data migration for files to support captions
    
    * fix dates on last patches and migrate file upload answers correctly
    
    * remove dev-commands from build
    
    * small type update for attachments
    
    Co-authored-by: Peter Asztalos <peter.asztalos@sigmatechnology.se>
    Co-authored-by: Peter Asztalos <169059+peter-asztalos@users.noreply.github.com>

--- db_patches/0075_AddMulticolumnIndexToSamples.sql
+++ db_patches/0075_AddMulticolumnIndexToSamples.sql
@@ -1,7 +1,7 @@
 DO
 $$
 BEGIN
-    IF register_patch('AddMulticolumnIndexToSamples.sql', 'Peter Asztalos', 'Add Multicolumn Index To Samples', '2020-12-18') THEN
+    IF register_patch('AddMulticolumnIndexToSamples.sql', 'Peter Asztalos', 'Add Multicolumn Index To Samples', '2021-01-11') THEN
 
         CREATE INDEX samples_proposal_id_question_id_mm_idx ON "samples" ("proposal_id", "question_id");
 
