f568d06923d052020fa26400480eb05099cc5f9d
David Symons <david.symons@stfc.ac.uk>
2021-03-30 15:21:59 +0100

    UserOfficeProject/stfc-user-office-project#80: Rename Status

--- db_patches/0085_Insert_Into_Proposal_Statuses_Table.sql
+++ db_patches/0085_Insert_Into_Proposal_Statuses_Table.sql
@@ -6,7 +6,7 @@ BEGIN
 	IF register_patch('Insert_Into_Proposal_Statuses_Table.sql', 'David Symons', 'Add additional Status into Proposal Statuses table', '2021-03-19') THEN
     BEGIN
         
-        INSERT INTO proposal_statuses (name, description) VALUES ('EDITABLE_SUBMISSION', 'Proposal is editable after submission.');
+        INSERT INTO proposal_statuses (name, description) VALUES ('EDITABLE_SUBMITTED', 'Proposal is editable after submission.');
     END;
 	END IF;
 END;
