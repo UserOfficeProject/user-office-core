749b2fb507a32220bf06ab5ecb34e21c794194b4
TCMeldrum <thomas.cottee-meldrum@stfc.ac.uk>
2021-04-19 13:34:15 +0100

    Added statement to check if we are in development environment of so to send any emails to busappssink email

--- src/eventHandlers/MailService/SMTPMailService.ts
+++ src/eventHandlers/MailService/SMTPMailService.ts
@@ -58,12 +58,20 @@ export class SMTPMailService extends MailService {
             ...(typeof participant.address !== 'string'
               ? {
                   to: {
-                    address: participant.address.email,
+                    address:
+                      process.env.NODE_ENV === 'development'
+                        ? <string>process.env.SINK_EMAIL
+                        : participant.address.email,
+                    // : 'BISAPPSSINK@stfc.ac.uk',
                     name: participant.address.header_to,
                   },
                 }
               : {
-                  to: participant.address,
+                  to:
+                    process.env.NODE_ENV === 'development'
+                      ? <string>process.env.SINK_EMAIL
+                      : participant.address,
+                  // : 'BISAPPSSINK@stfc.ac.uk',
                 }),
           },
           locals: options.substitution_data,
