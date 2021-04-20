3852254200474ff5c563135f8bec6b67b6645f3c
Alberto <alberto.popescu@stfc.ac.uk>
2021-04-20 19:51:04 +0100

    fix error in usedataapi

--- src/hooks/common/useDataApi.ts
+++ src/hooks/common/useDataApi.ts
@@ -56,7 +56,7 @@ class UnauthorizedGraphQLClient extends GraphQLClient {
         notificationWithClientLog(
           this.enqueueSnackbar,
           'Something went wrong!',
-          error.response.errors[0].message
+          error.response.error
         );
       }
 
