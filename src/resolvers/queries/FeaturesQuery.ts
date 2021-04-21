d6c504dd4a30957cd67a5f3e4dbc3b5ccefe2cb7
Alberto <alberto.popescu@stfc.ac.uk>
2021-04-21 14:15:28 +0100

    make separate query file for settings

--- src/resolvers/queries/FeaturesQuery.ts
+++ src/resolvers/queries/FeaturesQuery.ts
@@ -2,20 +2,11 @@ import { Ctx, Query, Resolver } from 'type-graphql';
 
 import { ResolverContext } from '../../context';
 import { Feature } from '../types/Feature';
-import { Settings } from '../types/Settings';
 
 @Resolver()
 export class FeaturesQuery {
   @Query(() => [Feature])
   features(@Ctx() context: ResolverContext) {
     return context.queries.admin.getFeatures();
-  }
-}
-
-@Resolver()
-export class SettingsQuery {
-  @Query(() => [Settings])
-  settings(@Ctx() context: ResolverContext) {
-    return context.queries.admin.getSettings();
   }
 }
