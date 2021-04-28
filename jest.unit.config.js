e24638919d9ef50d397de026e2883fcf9456987d
Jekabs Karklins <58165815+jekabs-karklins@users.noreply.github.com>
2021-04-28 14:42:29 +0200

    add back env config for unit tests (#272)
    Co-authored-by: Martin <martin.trajanovski@ess.eu>

--- jest.unit.config.js
+++ jest.unit.config.js
@@ -7,4 +7,5 @@ module.exports = {
   testRegex: '(/__tests__/.*|(\\.|/)(spec))\\.[jt]sx?$',
   collectCoverage: true,
   setupFilesAfterEnv: ['<rootDir>/src/config/dependencyConfigTest.ts'],
+  setupFiles: ['dotenv/config'],
 };
