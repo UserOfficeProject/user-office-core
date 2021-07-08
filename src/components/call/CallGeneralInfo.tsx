717a1040bd14c74267bd13dd506a9cf93a4292ab
Rasmia Kulan <rasmia.kulan@stfc.ac.uk>
2021-07-08 08:45:40 +0100

    UserOfficeProject/stfc-user-office-project#110:Add validation to check for invalid format and display information on how to correct it

--- src/components/call/CallGeneralInfo.tsx
+++ src/components/call/CallGeneralInfo.tsx
@@ -1,4 +1,25 @@
 import DateFnsUtils from '@date-io/date-fns';
+import {
+  Button,
+  createStyles,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  IconButton,
+  InputAdornment,
+  Paper,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Theme,
+  Typography,
+  withStyles,
+  WithStyles,
+} from '@material-ui/core';
+import HelpIcon from '@material-ui/icons/Help';
 import { MuiPickersUtilsProvider } from '@material-ui/pickers';
 import { Field, useFormikContext } from 'formik';
 import { TextField } from 'formik-material-ui';
@@ -53,6 +74,62 @@ const CallGeneralInfo: React.FC<{
     }
   }, [startCall, endCall, formik]);
 
+  function validateRefnumber(input: string) {
+    let error;
+    const regExp = /((([a-z|\d]+)){)({digits:[1-9]+})/;
+    const prefix = /((([a-z|\d]+)){)/;
+    const suffix = /({digits:[1-9]+})/;
+    if (input) {
+      if (input.match(regExp)) {
+      } else if (!input.match(suffix)) {
+        error = 'Invalid parameter.';
+      } else if (!input.match(prefix)) {
+        error = 'Invalid parameter.';
+      }
+    }
+
+    return error;
+  }
+
+  const [open, setOpen] = React.useState(false);
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const StyledTableCell = withStyles((theme: Theme) =>
+    createStyles({
+      head: {
+        backgroundColor: theme.palette.common.black,
+        color: theme.palette.common.white,
+      },
+      body: {
+        fontSize: 14,
+      },
+    })
+  )(TableCell);
+
+  const StyledTableRow = withStyles((theme: Theme) =>
+    createStyles({
+      root: {
+        '&:nth-of-type(odd)': {
+          backgroundColor: theme.palette.action.hover,
+        },
+      },
+    })
+  )(TableRow);
+
+  function populateTable(format: string, refNumber: string) {
+    return { format, refNumber };
+  }
+
+  const rows = [
+    populateTable('abc{digits:3}', 'abc001, abc002, abc003'),
+    populateTable('211{digits:5}', '21100001, 21100002, 21100003'),
+  ];
+
   return (
     <>
       <Field
@@ -90,9 +167,77 @@ const CallGeneralInfo: React.FC<{
         />
         <Field
           name="referenceNumberFormat"
+          validate={validateRefnumber}
           label="Reference number format"
           type="text"
           component={TextField}
+          InputProps={{
+            endAdornment: (
+              <InputAdornment position="end">
+                <IconButton onClick={handleClickOpen}>
+                  <HelpIcon />
+                </IconButton>
+                <Dialog
+                  onClose={handleClose}
+                  aria-labelledby="customized-dialog-title"
+                  open={open}
+                  color="primary"
+                >
+                  <DialogContent dividers>
+                    <Typography gutterBottom color="inherit" variant="body1">
+                      A reference number format determines how reference numbers
+                      are generated. It consists of a <strong>prefix </strong>
+                      and <strong> digits parameter</strong>.<br></br>
+                      <br></br>
+                      The<strong> prefix</strong> can contain alphanumeric
+                      characters and is what all generated reference numbers
+                      will begin with. For example, <code>21a</code>.<br></br>
+                      <br></br>
+                      The <strong> digits parameter</strong> is a numerical
+                      value that determines how many digits a proposal&apos;s
+                      sequence number is, including padding.It is written as
+                      <code>{'{digits:x}'}</code>, where x is a the value of the
+                      number. For example, if parameter is 6 (
+                      <code>{'{digits:6}'}</code>),the first proposal will be
+                      numbered 000001, the second 000002, and so on.
+                      <h3>Valid examples</h3>
+                      <TableContainer component={Paper}>
+                        <Table aria-label="customized table">
+                          <TableHead>
+                            <StyledTableRow>
+                              <StyledTableCell>
+                                <strong>Format</strong>
+                              </StyledTableCell>
+                              <StyledTableCell align="right">
+                                <strong>Generated reference numbers</strong>
+                              </StyledTableCell>
+                            </StyledTableRow>
+                          </TableHead>
+                          <TableBody>
+                            {rows.map((row) => (
+                              <StyledTableRow key={row.format}>
+                                <StyledTableCell component="th" scope="row">
+                                  {row.format}
+                                </StyledTableCell>
+                                <StyledTableCell align="right">
+                                  {row.refNumber}
+                                </StyledTableCell>
+                              </StyledTableRow>
+                            ))}
+                          </TableBody>
+                        </Table>
+                      </TableContainer>
+                    </Typography>
+                  </DialogContent>
+                  <DialogActions>
+                    <Button autoFocus onClick={handleClose} color="primary">
+                      Close
+                    </Button>
+                  </DialogActions>
+                </Dialog>
+              </InputAdornment>
+            ),
+          }}
           margin="normal"
           fullWidth
           data-cy="reference-number-format"
@@ -138,3 +283,8 @@ CallGeneralInfo.propTypes = {
 };
 
 export default CallGeneralInfo;
+export interface DialogTitleProps extends WithStyles {
+  id: string;
+  children: React.ReactNode;
+  onClose: () => void;
+}
