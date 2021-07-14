9a51301ffeb30a719bc211dd731b5caee772088d
Martin Trajanovski <martin.trajanovski@ess.eu>
2021-07-14 11:57:16 +0200

    fix(styles): Fix some styling issues introduced after merging the STFC theme PR

--- src/styles/StyledComponents.tsx
+++ src/styles/StyledComponents.tsx
@@ -6,24 +6,53 @@ import styled from '@material-ui/core/styles/styled';
 import { CSSProperties } from '@material-ui/styles';
 import React from 'react';
 
+const getValueFromArrayProperty = (prop: number[], theme: Theme) =>
+  prop.map((item) => `${theme.spacing(item)}px`).join(' ');
+
 export const StyledPaper = styled(({ ...other }) => <Paper {...other} />)(
-  ({ theme, ...props }: { theme: Theme } & CSSProperties) => ({
-    margin: props.margin || theme.spacing(3, 0),
-    padding: props.padding || theme.spacing(2),
-    [theme.breakpoints.up(600 + theme.spacing(3) * 2)]: {
-      margin: props.margin || theme.spacing(6, 0),
-      padding: props.padding || theme.spacing(3),
-    },
-  })
+  ({
+    theme,
+    ...props
+  }: { theme: Theme } & CSSProperties & {
+      margin?: number[];
+      padding?: number[];
+    }) => {
+    const margin: string | number | undefined = Array.isArray(props.margin)
+      ? getValueFromArrayProperty(props.margin, theme)
+      : props.margin;
+    const padding: string | number | undefined = Array.isArray(props.padding)
+      ? getValueFromArrayProperty(props.padding, theme)
+      : props.padding;
+
+    return {
+      margin: margin || theme.spacing(3, 0),
+      padding: padding || theme.spacing(2),
+      [theme.breakpoints.up(600 + theme.spacing(3) * 2)]: {
+        margin: margin || theme.spacing(6, 0),
+        padding: padding || theme.spacing(3),
+      },
+    };
+  }
 );
 export const FormWrapper = styled(({ ...other }) => <Box {...other} />)(
-  ({ theme, ...props }: { theme: Theme } & CSSProperties) => ({
-    margin: props.margin || theme.spacing(8),
-    display: props.display || 'flex',
-    flexDirection: props.flexDirection || 'column',
-    alignItems: props.alignItems || 'center',
-    overflow: props.overflow || 'auto',
-  })
+  ({
+    theme,
+    ...props
+  }: { theme: Theme } & CSSProperties & {
+      margin?: number[];
+    }) => {
+    const margin: string | number | undefined = Array.isArray(props.margin)
+      ? getValueFromArrayProperty(props.margin, theme)
+      : props.margin;
+
+    return {
+      margin: margin || theme.spacing(8),
+      display: props.display || 'flex',
+      flexDirection: props.flexDirection || 'column',
+      alignItems: props.alignItems || 'center',
+      overflow: props.overflow || 'auto',
+    };
+  }
 );
 export const ContentContainer = styled(({ maxWidth = false, ...other }) => (
   <Container maxWidth={maxWidth} {...other} />
