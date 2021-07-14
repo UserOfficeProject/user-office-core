a2a37027a41b5c838a7f6cbac51d12e81423cf9c
Martin Trajanovski <martin.trajanovski@ess.eu>
2021-07-14 12:00:26 +0200

    final fix for styled components

--- src/styles/StyledComponents.tsx
+++ src/styles/StyledComponents.tsx
@@ -10,13 +10,7 @@ const getValueFromArrayProperty = (prop: number[], theme: Theme) =>
   prop.map((item) => `${theme.spacing(item)}px`).join(' ');
 
 export const StyledPaper = styled(({ ...other }) => <Paper {...other} />)(
-  ({
-    theme,
-    ...props
-  }: { theme: Theme } & CSSProperties & {
-      margin?: number[];
-      padding?: number[];
-    }) => {
+  ({ theme, ...props }: { theme: Theme } & CSSProperties) => {
     const margin: string | number | undefined = Array.isArray(props.margin)
       ? getValueFromArrayProperty(props.margin, theme)
       : props.margin;
@@ -35,12 +29,7 @@ export const StyledPaper = styled(({ ...other }) => <Paper {...other} />)(
   }
 );
 export const FormWrapper = styled(({ ...other }) => <Box {...other} />)(
-  ({
-    theme,
-    ...props
-  }: { theme: Theme } & CSSProperties & {
-      margin?: number[];
-    }) => {
+  ({ theme, ...props }: { theme: Theme } & CSSProperties) => {
     const margin: string | number | undefined = Array.isArray(props.margin)
       ? getValueFromArrayProperty(props.margin, theme)
       : props.margin;
@@ -56,9 +45,15 @@ export const FormWrapper = styled(({ ...other }) => <Box {...other} />)(
 );
 export const ContentContainer = styled(({ maxWidth = false, ...other }) => (
   <Container maxWidth={maxWidth} {...other} />
-))(({ theme, ...props }: { theme: Theme } & CSSProperties) => ({
-  padding: props.padding || theme.spacing(2, 2),
-}));
+))(({ theme, ...props }: { theme: Theme } & CSSProperties) => {
+  const padding: string | number | undefined = Array.isArray(props.padding)
+    ? getValueFromArrayProperty(props.padding, theme)
+    : props.padding;
+
+  return {
+    padding: padding || theme.spacing(2, 2),
+  };
+});
 export const ButtonContainer = styled(({ ...other }) => <div {...other} />)({
   display: 'flex',
   justifyContent: 'flex-end',
