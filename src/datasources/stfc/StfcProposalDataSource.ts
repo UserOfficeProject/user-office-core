6c0558f955de89affc86d20a3b5963a1d298263f
Vlad Ionica <vlad.ionica@stfc.ac.uk>
2021-10-26 10:12:29 +0100

    Removed an external method.

--- src/datasources/stfc/StfcProposalDataSource.ts
+++ src/datasources/stfc/StfcProposalDataSource.ts
@@ -9,39 +9,6 @@ import {
   ProposalRecord,
 } from '../postgres/records';
 
-export async function calculateReferenceNumber(
-  format: string,
-  sequence: number | null
-): Promise<string> {
-  const prefix: string = format.slice(0, format.indexOf('{'));
-  const formatParameters: { [param: string]: string } = {};
-  format.match(/(\w+:\w+)/g)?.forEach((el: string) => {
-    const [k, v] = el.split(':');
-    formatParameters[k] = v;
-  });
-
-  if (!prefix || !('digits' in formatParameters)) {
-    return Promise.reject(
-      new Error(`The reference number format ('${format}') is invalid`)
-    );
-  }
-
-  sequence = sequence ?? 0;
-  if (String(sequence).length > Number(formatParameters['digits'])) {
-    return Promise.reject(
-      new Error(
-        `The sequence number provided ('${formatParameters['digits']}') exceeds the format's digits parameter`
-      )
-    );
-  }
-  const paddedSequence: string = String(sequence).padStart(
-    Number(formatParameters['digits']),
-    '0'
-  );
-
-  return prefix + paddedSequence;
-}
-
 @injectable()
 export default class StfcProposalDataSource extends PostgresProposalDataSource {
   async getInstrumentScientistProposals(
