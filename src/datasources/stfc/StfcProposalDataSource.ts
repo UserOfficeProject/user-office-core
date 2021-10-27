be96f6678c52bd480174e9a644baa1f19560efe3
Vlad Ionica <vlad.ionica@stfc.ac.uk>
2021-10-27 10:47:53 +0100

    Fixed a bug with filters not doing anything.

--- src/datasources/stfc/StfcProposalDataSource.ts
+++ src/datasources/stfc/StfcProposalDataSource.ts
@@ -23,6 +23,63 @@ export default class StfcProposalDataSource extends PostgresProposalDataSource {
         database.raw('count(*) OVER() AS full_count')
       ])
       .from('proposals') 
+      .modify((query) => {
+        if (filter?.text) {
+          query
+            .where('title', 'ilike', `%${filter.text}%`)
+            .orWhere('abstract', 'ilike', `%${filter.text}%`);
+        }
+        if (filter?.callId) {
+          query.where('proposals.call_id', filter.callId);
+        }
+        if (filter?.instrumentId) {
+          query.where(
+            'instrument_has_proposals.instrument_id',
+            filter.instrumentId
+          );
+        }
+
+        if (filter?.proposalStatusId) {
+          query.where('proposals.status_id', filter?.proposalStatusId);
+        }
+
+        if (filter?.shortCodes) {
+          const filteredAndPreparedShortCodes = filter?.shortCodes
+            .filter((shortCode) => shortCode)
+            .join('|');
+
+          query.whereRaw(
+            `proposals.proposal_id similar to '%(${filteredAndPreparedShortCodes})%'`
+          );
+        }
+
+        if (filter?.questionFilter) {
+          const questionFilter = filter.questionFilter;
+          const questionFilterQuery = getQuestionDefinition(
+            questionFilter.dataType
+          ).filterQuery;
+          if (!questionFilterQuery) {
+            throw new Error(
+              `Filter query not implemented for ${filter.questionFilter.dataType}`
+            );
+          }
+          query
+            .leftJoin(
+              'answers',
+              'answers.questionary_id',
+              'proposals.questionary_id'
+            )
+            .andWhere('answers.question_id', questionFilter.questionId)
+            .modify(questionFilterQuery, questionFilter);
+        }
+
+        if (first) {
+          query.limit(first);
+        }
+        if (offset) {
+          query.offset(offset);
+        }
+      })
       .then((proposals: ProposalRecord[]) => {
         const props = proposals.map((proposal) =>
           createProposalObject(proposal)
