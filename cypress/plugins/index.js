ab2653167eb2f0fe0a5be135139a4209894e4117
Jekabs Karklins <58165815+jekabs-karklins@users.noreply.github.com>
2021-11-02 10:45:16 +0100

    fix: Ordering screenshots by failing datetime (#621)
    * ordering screenshots by failure date
    
    * new sdk
    
    * fix(cypress): fix flaky test
    
    * docs(readme): Adding exclamation mark
    
    * Revert

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -11,7 +11,42 @@
 // This function is called when a project is opened or re-opened (e.g. due to
 // the project's config changing)
 
+const fs = require('fs')
+
+function replaceLastOccurrenceInString(string, find, replace) {
+  var lastIndex = string.lastIndexOf(find);
+  
+  if (lastIndex === -1) {
+      return string;
+  }
+  
+  var beginString = string.substring(0, lastIndex);
+  var endString = string.substring(lastIndex + find.length);
+  
+  return beginString + replace + endString;
+}
+
+
+function replaceInvalidFileNameCharacters(filename) {
+ return filename.replaceAll(":",".");
+}
+
 module.exports = (on, config) => {
   // `on` is used to hook into various events Cypress emits
   // `config` is the resolved Cypress config
+  on('after:screenshot', (details) => {
+
+    const fileNamePrefix = replaceInvalidFileNameCharacters(details.takenAt);
+    const newPath = replaceLastOccurrenceInString(details.path, '/', `/${fileNamePrefix} -- `);
+
+    return new Promise((resolve, reject) => {
+      fs.rename(details.path, newPath, (err) => {
+        if (err) return reject(err)
+
+        // because we renamed and moved the image, resolve with the new path
+        // so it is accurate in the test results
+        resolve({ path: newPath })
+      })
+    })
+  })
 }
