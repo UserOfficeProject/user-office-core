2379c838c0762a974eec585fea03d445033d9e16
Thomas Cottee Meldrum <thomas.cottee-meldrum@stfc.ac.uk>
2021-11-30 13:40:59 +0000

    update SDK

--- src/generated/sdk.ts
+++ src/generated/sdk.ts
@@ -801,6 +801,7 @@ export type Mutation = {
   finalizeProposalBooking: ProposalBookingResponseWrap;
   finalizeScheduledEvent: ScheduledEventResponseWrap;
   getTokenForUser: TokenResponseWrap;
+  importProposal: ProposalResponseWrap;
   login: TokenResponseWrap;
   logout: LogoutTokenWrap;
   moveProposalWorkflowStatus: ProposalWorkflowConnectionResponseWrap;
@@ -1351,6 +1352,17 @@ export type MutationFinalizeScheduledEventArgs = {
 
 export type MutationGetTokenForUserArgs = {
   userId: Scalars['Int'];
+};
+
+
+export type MutationImportProposalArgs = {
+  abstract?: Maybe<Scalars['String']>;
+  callId: Scalars['Int'];
+  proposerId?: Maybe<Scalars['Int']>;
+  referenceNumber: Scalars['Int'];
+  submitterId: Scalars['Int'];
+  title?: Maybe<Scalars['String']>;
+  users?: Maybe<Array<Scalars['Int']>>;
 };
 
 
