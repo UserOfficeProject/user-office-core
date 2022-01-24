4a01993b28c7cb4b046ad58caaf9b99389d91a74
Vlad Ionica <vlad.ionica@stfc.ac.uk>
2022-01-24 15:44:39 +0000

    Extracted the email search bar component into its own file.

---
+++ src/components/common/EmailSearchbar.tsx
@@ -0,0 +1,55 @@
+import { MTableToolbar } from "@material-table/core";
+import { Button, makeStyles } from "@material-ui/core";
+import { Form, Field } from "formik";
+import { TextField } from "formik-material-ui";
+
+const useStyles = makeStyles({
+    titleStyle: {
+      display: 'inline',
+    },
+    inviteButton: {
+      marginLeft: '10px',
+      whiteSpace: 'nowrap',
+    },
+    email: {
+      display: 'flex',
+      justifyContent: 'flex-end',
+      alignItems: 'baseline',
+    }
+  });
+
+const StylisedToolbar: React.FC = (props) => {
+    const classes = useStyles();
+  
+    return (
+      <>
+        <div className={classes.titleStyle}>
+          <MTableToolbar {...props} />
+        </div>
+        <div>
+          <Form className={useStyles().email}>
+            <Field
+              name="email"
+              label="E-mail"
+              id="Email-input"
+              type="email"
+              component={TextField}
+              margin="normal"
+              fullWidth
+              flex="1"
+              data-cy="email"
+            />
+            <Button
+              data-cy="findUser"
+              variant="contained"
+              color="primary"
+              type="submit"
+              className={useStyles().inviteButton}
+            >
+              Find User
+            </Button>
+          </Form>
+        </div>
+      </>
+    );
+  };
