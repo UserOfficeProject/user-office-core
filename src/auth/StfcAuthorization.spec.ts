04a135671a5d69650a5485c30b6c93cb19e0ffb0
AlexShepherd <Alexander.Shepherd@stfc.ac.uk>
2022-03-08 13:58:47 +0000

    Changes to the tests for this

--- src/auth/StfcAuthorization.spec.ts
+++ src/auth/StfcAuthorization.spec.ts
@@ -1,15 +1,23 @@
-import { dummyUser } from '../datasources/mockups/UserDataSource';
+import { container } from 'tsyringe';
 
-const UserAuthorization = newStfcUserAuthorization;
+import { dummyUser } from '../datasources/mockups/UserDataSource';
+import { UserAuthorization } from './UserAuthorization';
+
+jest.mock('../datasources/stfc/UOWSSoapInterface.ts');
+
+const userAuthorization = container.resolve(UserAuthorization);
+
 test('When an invalid external token is supplied, no user is found', async () => {
-  return expect(userdataSource.externalTokenLogin('invalid')).rejects.toMatch(
+  return expect(
+    userAuthorization.externalTokenLogin('invalid')
+  ).rejects.toMatch(
     'Failed to fetch user details for STFC external authentication'
   );
 });
 
 test('When a valid external token is supplied, valid user is returned', async () => {
-  const result = await userdataSource.externalTokenLogin('valid');
+  const result = await userAuthorization.externalTokenLogin('valid');
 
-  expect(result.id).toBe(1);
-  expect(result.email).toBe(dummyUser.email);
+  expect(result?.id).toBe(1);
+  expect(result?.email).toBe(dummyUser.email);
 });
