1b3f119ef89fe4293f8a5ef46751a5fb6a48a523
Simon Fernandes <simon.fernandes@stfc.ac.uk>
2022-03-08 11:30:16 +0000

    Add validation to file question
    Adds file upload validation. The validation looks ateach file's signature to check that the file uploaded is one of the accepted file types.
    
    Validation for an individual file passes if the file can be read and determined to be one of the accepted types, or if there is a problem retrieving the file to validate it.
    
    Validation for an individual file fails if the file can be read and is not one of the accepted types, or if no types can be analysed from the file.
    
    For the entire question to pass validation, all files within the question must pass the invididual file validation.
    
    Part of UserOfficeProject/stfc-user-office-project#226

--- src/models/questionTypes/FileUpload.ts
+++ src/models/questionTypes/FileUpload.ts
@@ -1,20 +1,95 @@
-/* eslint-disable quotes */
-import { fileUploadQuestionValidationSchema } from '@user-office-software/duo-validation';
+import fs from 'fs/promises';
 
+import { logger } from '@user-office-software/duo-logger';
+import { fileUploadQuestionValidationSchema } from '@user-office-software/duo-validation';
+import fileTypeInfo from 'magic-bytes.js';
+import { GuessedFile } from 'magic-bytes.js/dist/model/tree';
+import { container } from 'tsyringe';
+
+import FileMutations from '../../mutations/FileMutations';
 import { FileUploadConfig } from '../../resolvers/types/FieldConfig';
 import { QuestionTemplateRelation } from '../../resolvers/types/QuestionTemplateRelation';
 import { QuestionFilterCompareOperator } from '../Questionary';
+import { isRejection } from '../Rejection';
 import { DataType } from '../Template';
 import { Question } from './QuestionRegistry';
 
 export const fileUploadDefinition: Question = {
   dataType: DataType.FILE_UPLOAD,
-  validate: (field: QuestionTemplateRelation, value: any) => {
-    if (field.question.dataType !== DataType.FILE_UPLOAD) {
-      throw new Error('DataType should be FILE_UPLOAD');
+  validate: async (field: QuestionTemplateRelation, value: any) => {
+    const fileMutations = container.resolve(FileMutations);
+
+    if (!(await fileUploadQuestionValidationSchema(field).isValid(value))) {
+      return false;
     }
 
-    return fileUploadQuestionValidationSchema(field).isValid(value);
+    const isValidType = async (fileId: string): Promise<boolean> => {
+      const errorContext = {
+        question: field,
+        answer: value,
+        fileId: fileId,
+      };
+
+      const path = await fileMutations.prepare(fileId);
+
+      if (isRejection(path)) {
+        logger.logInfo(
+          'Could not retrieve file to validate file upload question',
+          errorContext
+        );
+
+        return true; // Allow the file
+      }
+
+      let data: Buffer;
+
+      try {
+        data = await fs.readFile(path);
+      } catch (error) {
+        logger.logInfo('Could not read file to validate file upload question', {
+          error: error,
+          ...errorContext,
+        });
+
+        return true; // Allow the file
+      }
+
+      const possibleTypes: string[] = [];
+
+      const getFileInfo = async (data: Buffer) => {
+        fileTypeInfo(data).forEach((type: GuessedFile) => {
+          if (type.extension) {
+            possibleTypes.push('.'.concat(type.extension));
+          }
+          if (type.mime) {
+            possibleTypes.push(type.mime);
+          }
+        });
+      };
+
+      await getFileInfo(data);
+
+      fs.unlink(path);
+
+      const config = field.config as FileUploadConfig;
+
+      return possibleTypes.some((type) => {
+        const anySubtype = type.split('/')[0]?.concat('/*'); // e.g. 'image/*'
+
+        return (
+          config.file_type.includes(type) ||
+          config.file_type.includes(anySubtype)
+        );
+      });
+    };
+
+    const results: boolean[] = value.map(async (file: { id: string }) => {
+      return await isValidType(file.id);
+    });
+
+    return (await Promise.all(results)).every((result) => {
+      return result;
+    });
   },
   createBlankConfig: (): FileUploadConfig => {
     const config = new FileUploadConfig();
