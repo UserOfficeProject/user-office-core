62b35ce81719d4ccea67a6408db98211b334c9dc
Simon Fernandes <simon.fernandes@stfc.ac.uk>
2022-03-11 09:18:52 +0000

    Move file check into separate function
    Part of UserOfficeProject/user-office-project-issue-tracker#226

--- src/models/questionTypes/FileUpload.ts
+++ src/models/questionTypes/FileUpload.ts
@@ -17,78 +17,18 @@ import { Question } from './QuestionRegistry';
 export const fileUploadDefinition: Question = {
   dataType: DataType.FILE_UPLOAD,
   validate: async (field: QuestionTemplateRelation, value: any) => {
-    const fileMutations = container.resolve(FileMutations);
-
     if (!(await fileUploadQuestionValidationSchema(field).isValid(value))) {
       return false;
     }
 
-    const isValidType = async (fileId: string): Promise<boolean> => {
-      const errorContext = {
-        question: field,
-        answer: value,
-        fileId: fileId,
-      };
-
-      const path = await fileMutations.prepare(fileId);
-
-      if (isRejection(path)) {
-        logger.logInfo(
-          'Could not retrieve file to validate file upload question',
-          errorContext
-        );
-
-        return true; // Allow the file
+    const typeCheckResults: boolean[] = value.map(
+      async (file: { id: string }) => {
+        return await isValidFileType(file.id, field, value);
       }
+    );
 
-      let data: Buffer;
-
-      try {
-        data = await fs.readFile(path);
-      } catch (error) {
-        logger.logInfo('Could not read file to validate file upload question', {
-          error: error,
-          ...errorContext,
-        });
-
-        return true; // Allow the file
-      }
-
-      const possibleTypes: string[] = [];
-
-      const getFileInfo = async (data: Buffer) => {
-        fileTypeInfo(data).forEach((type: GuessedFile) => {
-          if (type.extension) {
-            possibleTypes.push('.'.concat(type.extension));
-          }
-          if (type.mime) {
-            possibleTypes.push(type.mime);
-          }
-        });
-      };
-
-      await getFileInfo(data);
-
-      fs.unlink(path);
-
-      const config = field.config as FileUploadConfig;
-
-      return possibleTypes.some((type) => {
-        const anySubtype = type.split('/')[0]?.concat('/*'); // e.g. 'image/*'
-
-        return (
-          config.file_type.includes(type) ||
-          config.file_type.includes(anySubtype)
-        );
-      });
-    };
-
-    const results: boolean[] = value.map(async (file: { id: string }) => {
-      return await isValidType(file.id);
-    });
-
-    return (await Promise.all(results)).every((result) => {
-      return result;
+    return (await Promise.all(typeCheckResults)).every((check) => {
+      return check;
     });
   },
   createBlankConfig: (): FileUploadConfig => {
@@ -122,4 +62,80 @@ export const fileUploadDefinition: Question = {
         );
     }
   },
+};
+
+/**
+ * Checks the file signature of a file and returns whether the file
+ * is one of the accepted types specified in the question config.
+ *
+ * @param fileId The ID of the file to check
+ * @param field The question data containing the config
+ * @param value All files in the question
+ * @returns True if the file is identified as one of the accepted types.
+ * False if the file cannot be retrieved or identified, or is not one of
+ * the accepted types
+ */
+const isValidFileType = async (
+  fileId: string,
+  field: QuestionTemplateRelation,
+  value: any
+): Promise<boolean> => {
+  const fileMutations = container.resolve(FileMutations);
+
+  const errorContext = {
+    question: field,
+    answer: value,
+    fileId: fileId,
+  };
+
+  const path = await fileMutations.prepare(fileId);
+
+  if (isRejection(path)) {
+    logger.logInfo(
+      'Could not retrieve file to validate file upload question',
+      errorContext
+    );
+
+    return true; // Allow the file
+  }
+
+  let data: Buffer;
+
+  try {
+    data = await fs.readFile(path);
+  } catch (error) {
+    logger.logInfo('Could not read file to validate file upload question', {
+      error: error,
+      ...errorContext,
+    });
+
+    return true; // Allow the file
+  }
+
+  const possibleTypes: string[] = [];
+
+  const getFileInfo = async (data: Buffer) => {
+    fileTypeInfo(data).forEach((type: GuessedFile) => {
+      if (type.extension) {
+        possibleTypes.push('.'.concat(type.extension));
+      }
+      if (type.mime) {
+        possibleTypes.push(type.mime);
+      }
+    });
+  };
+
+  await getFileInfo(data);
+
+  fs.unlink(path);
+
+  const config = field.config as FileUploadConfig;
+
+  return possibleTypes.some((type) => {
+    const anySubtype = type.split('/')[0]?.concat('/*'); // e.g. 'image/*'
+
+    return (
+      config.file_type.includes(type) || config.file_type.includes(anySubtype)
+    );
+  });
 };
