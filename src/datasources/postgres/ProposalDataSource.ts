ce1e70d521e51dc8074b518290304fa69e785edb
Cosimo Campo <cosimo.campo@stfc.ac.uk>
2022-05-16 17:41:15 +0100

    feat: Show proposals for assigned calls in instrument scientist table
    The instrument scientist proposal table now shows all proposals for all calls
    assigned to the scientist's instruments. This is in addition to the proposals
    assigned directly to the scietist's instruments, which is what the table showed
    before this change.
    
    The main reason for this is to automatically show all proposals received
    for a given instrument without requiring a user officer to assign each proposal
    to the instrument.
    
    Part of https://github.com/UserOfficeProject/user-office-project-issue-tracker/issues/1

--- src/datasources/postgres/ProposalDataSource.ts
+++ src/datasources/postgres/ProposalDataSource.ts
@@ -501,12 +501,22 @@ export default class PostgresProposalDataSource implements ProposalDataSource {
         database.raw('count(*) OVER() AS full_count'),
       ])
       .from('proposal_table_view')
-      .join('instruments', {
-        'instruments.instrument_id': 'proposal_table_view.instrument_id',
+      .join('instruments', function () {
+        this.on({
+          'instruments.instrument_id':
+            'proposal_table_view.proposal_instrument_id',
+        }).orOn({
+          'instruments.instrument_id': 'proposal_table_view.call_instrument_id',
+        });
       })
-      .leftJoin('instrument_has_scientists', {
-        'instrument_has_scientists.instrument_id':
-          'proposal_table_view.instrument_id',
+      .leftJoin('instrument_has_scientists', function () {
+        this.on({
+          'instrument_has_scientists.instrument_id':
+            'proposal_table_view.proposal_instrument_id',
+        }).orOn({
+          'instrument_has_scientists.instrument_id':
+            'proposal_table_view.call_instrument_id',
+        });
       })
       .where(function () {
         this.where('instrument_has_scientists.user_id', scientistId).orWhere(
