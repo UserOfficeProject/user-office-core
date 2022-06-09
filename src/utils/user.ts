cea5989b0ea6fb69fb1954609f4781bc3ce445e7
Fredrik Bolmsten <fredrik.bolmsten@ess.eu>
2022-06-09 15:14:18 +0200

    Add email when showing PI and Co-proposers
    As a instrument scientist I want to be able to easily contact people on the experiment

--- src/utils/user.ts
+++ src/utils/user.ts
@@ -3,3 +3,8 @@ import { User } from 'generated/sdk';
 export const getFullUserName = (
   user?: Pick<User, 'firstname' | 'lastname'> | null
 ): string => (user ? `${user.firstname} ${user.lastname}` : 'None');
+
+export const getFullUserNameWithEmail = (
+  user?: Pick<User, 'firstname' | 'lastname' | 'email'> | null
+): string =>
+  user ? `${user.firstname} ${user.lastname}(${user.email})` : 'None';
