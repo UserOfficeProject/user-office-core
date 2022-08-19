fd2c5f1a7e22d3cb675e96053df617f2be36e3ad
Martin Trajanovski <martin.trajanovski@gmail.com>
2022-08-19 13:39:56 +0200

    Add a bit of an explanation for the predefined message and its usage

--- src/resolvers/types/PredefinedMessage.ts
+++ src/resolvers/types/PredefinedMessage.ts
@@ -12,6 +12,13 @@ import { ResolverContext } from '../../context';
 import { PredefinedMessage as PredefinedMessageOrigin } from '../../models/PredefinedMessage';
 import { BasicUserDetails } from './BasicUserDetails';
 
+/**
+ * NOTE: Predefined messages are used as a way to reuse some messages that are repeatable throughout the app.
+ * For example feedback inputs, comments and other messages that are quite generic can be just selected from a list of predefined messages.
+ * In the frontend we are going to have one component called FormikUIPredefinedMessagesTextField.
+ * This is Textarea which is loading all predefined messages from the database filtered by some specific key.
+ * It is easy to just search and select the message you want to use for that specific form input.
+ */
 @ObjectType()
 export class PredefinedMessage implements Partial<PredefinedMessageOrigin> {
   @Field(() => Int)
