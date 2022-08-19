5517bcb14b1121469152506e1999e0292f4d9da7
Martin Trajanovski <martin.trajanovski@gmail.com>
2022-08-19 15:03:47 +0200

    add some explanation for the predefined message component

--- src/components/common/predefinedMessages/FormikUIPredefinedMessagesTextField.tsx
+++ src/components/common/predefinedMessages/FormikUIPredefinedMessagesTextField.tsx
@@ -13,6 +13,12 @@ export enum PredefinedMessageKey {
   GENERAL = 'general',
 }
 
+/**
+ * NOTE: Predefined messages are used as a way to reuse some messages that are repeatable throughout the app.
+ * For example feedback inputs, comments and other messages that are quite generic can be just selected from a list of predefined messages.
+ * This is Textarea which is loading all predefined messages from the database filtered by some specific key.
+ * It is easy to just search and select the message you want to use for that specific form input.
+ */
 const FormikUIPredefinedMessagesTextField: React.FC<
   FieldAttributes<TextFieldProps> & { 'message-key'?: PredefinedMessageKey }
 > = (props) => {
