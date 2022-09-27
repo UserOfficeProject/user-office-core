67c113173e01f6ad7203b201bd29cc7e26721c9a
Farai Mutambara <farai.mutambara@stfc.ac.uk>
2022-09-27 10:29:41 +0100

    112,backend update asyn jobs for all calls ended  to run in one job

--- apps/user-office-backend/src/asyncJobs/allAsyncJobs.ts
+++ apps/user-office-backend/src/asyncJobs/allAsyncJobs.ts
@@ -1,14 +1,12 @@
-import checkCallsEndedJob from './jobs/checkCallsEnded';
-import checkCallsEndedInternalJob from './jobs/checkCallsEndedInternal';
+import checkAllCallsEndedJob from './jobs/checkAllCallsEnded';
 import checkCallsReviewEndedJob from './jobs/checkCallsReviewEnded';
 import checkCallsSEPReviewEndedJob from './jobs/checkCallsSEPReviewEnded';
 import { UserOfficeAsyncJob } from './startAsyncJobs';
 
 const ALL_ASYNC_JOBS: UserOfficeAsyncJob[] = [
-  checkCallsEndedJob,
+  checkAllCallsEndedJob,
   checkCallsReviewEndedJob,
   checkCallsSEPReviewEndedJob,
-  checkCallsEndedInternalJob,
 ];
 
 export default ALL_ASYNC_JOBS;
