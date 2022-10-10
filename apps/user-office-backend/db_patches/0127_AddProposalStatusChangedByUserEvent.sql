ac651dba54739fb63ff7175aa2971c64a13b3125
Martin Trajanovski <martin.trajanovski@gmail.com>
2022-10-10 11:26:42 +0200

    add missing proposal_updated event

--- apps/user-office-backend/db_patches/0127_AddProposalStatusChangedByUserEvent.sql
+++ apps/user-office-backend/db_patches/0127_AddProposalStatusChangedByUserEvent.sql
@@ -5,7 +5,8 @@ BEGIN
         BEGIN
             ALTER TABLE proposal_events
             ADD COLUMN proposal_status_changed_by_user BOOLEAN DEFAULT FALSE,
-            ADD COLUMN proposal_status_changed_by_workflow BOOLEAN DEFAULT FALSE;
+            ADD COLUMN proposal_status_changed_by_workflow BOOLEAN DEFAULT FALSE,
+            ADD COLUMN proposal_updated BOOLEAN DEFAULT FALSE;
         END;
 	END IF;
 END;
