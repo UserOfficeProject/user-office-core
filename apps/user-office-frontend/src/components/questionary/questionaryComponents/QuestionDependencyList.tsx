879f145668f2f21963d0b62bbd2b5d8c25756a6b
Keiran Price <keiran.price@stfc.ac.uk>
2022-10-24 14:44:54 +0100

    feat: update question dependency list

--- apps/user-office-frontend/src/components/questionary/questionaryComponents/QuestionDependencyList.tsx
+++ apps/user-office-frontend/src/components/questionary/questionaryComponents/QuestionDependencyList.tsx
@@ -58,9 +58,12 @@ const QuestionDependencyList: React.FC<QuestionDependencyListProps> = ({
     field.dependencies.length >=
     getAllFields(template.steps).filter(
       (option) =>
-        [DataType.BOOLEAN, DataType.SELECTION_FROM_OPTIONS].includes(
-          option.question.dataType
-        ) && currentQuestionId !== option.question.id
+        [
+          DataType.BOOLEAN,
+          DataType.SELECTION_FROM_OPTIONS,
+          DataType.MULTI_PART_SELECTION,
+        ].includes(option.question.dataType) &&
+        currentQuestionId !== option.question.id
     ).length;
 
   return (
