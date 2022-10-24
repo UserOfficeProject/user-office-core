a4396571b55e741156d0d252bbfbe1747f40bb55
Keiran Price <keiran.price@stfc.ac.uk>
2022-10-24 14:31:19 +0100

    Feat: Add selection form

---
+++ apps/user-office-frontend/src/components/questionary/questionaryComponents/MultiPartSelection/QuestionMultiPartSelectionForm.tsx
@@ -0,0 +1,157 @@
+import { FormControlLabel } from '@mui/material';
+import Button from '@mui/material/Button';
+import Checkbox from '@mui/material/Checkbox';
+import { Field, FieldArray } from 'formik';
+import { TextField } from 'formik-mui';
+import React, { FC } from 'react';
+import * as Yup from 'yup';
+
+import { MultiPartSelectionConfig } from '../../../../generated/sdk';
+import { useNaturalKeySchema } from '../../../../utils/userFieldValidationSchema';
+import TitledContainer from '../../../common/TitledContainer';
+import { QuestionFormProps } from '../../QuestionaryComponentRegistry';
+import { QuestionFormShell } from '../QuestionFormShell';
+
+export const QuestionMultiPartSelectionForm: FC<QuestionFormProps> = (
+  props
+) => {
+  const field = props.question;
+  const naturalKeySchema = useNaturalKeySchema(field.naturalKey);
+
+  return (
+    <QuestionFormShell
+      {...props}
+      validationSchema={Yup.object().shape({
+        naturalKey: naturalKeySchema,
+        question: Yup.string().required('Question is required'),
+        config: Yup.object({
+          required: Yup.bool(),
+          selectionPairs: Yup.array().of(
+            Yup.object().shape({
+              key: Yup.string(),
+              value: Yup.array().of(Yup.string()),
+            })
+          ),
+        }),
+      })}
+    >
+      {(formikProps) => (
+        <>
+          <Field
+            name="naturalKey"
+            label="Key"
+            id="Key-input"
+            type="text"
+            component={TextField}
+            fullWidth
+            inputProps={{ 'data-cy': 'natural_key' }}
+          />
+          <Field
+            name={'question'}
+            id={'Question-inpu'}
+            label="Question"
+            type={'text'}
+            component={TextField}
+            fullWidth
+            inputProps={{ 'data-cy': 'question' }}
+          />
+          <TitledContainer label={'Constraints'}>
+            <FormControlLabel
+              control={
+                <Field
+                  name={'config.required'}
+                  component={Checkbox}
+                  type={'checkbox'}
+                  inputProps={{ 'data-cy': 'required' }}
+                />
+              }
+              label={'Is required'}
+            />
+          </TitledContainer>
+          <TitledContainer label={'Question Configuration'}>
+            <Field
+              name={'config.partOneQuestion'}
+              component={TextField}
+              type={'text'}
+              label={'Question Part One'}
+              fullWidth
+            />
+            <Field
+              name={'config.partTwoQuestion'}
+              component={TextField}
+              type={'text'}
+              label={'Question Part Two'}
+              fullWidth
+            />
+          </TitledContainer>
+          <TitledContainer label={'Answer Configuration'}>
+            <FieldArray
+              name={'config.selectionPairs'}
+              render={(arrayHelpers) => (
+                <div>
+                  {(
+                    formikProps.values.config as MultiPartSelectionConfig
+                  ).selectionPairs.map((pair, index) => (
+                    <TitledContainer
+                      label={`Answer Pair ${index + 1}`}
+                      key={`${index}`}
+                    >
+                      <Field name={`config.selectionPairs[${index}].key`} />
+
+                      <FieldArray
+                        name={`config.selectionPairs[${index}].value`}
+                        render={(arrayHelpers) => (
+                          <div>
+                            {(
+                              formikProps.values
+                                .config as MultiPartSelectionConfig
+                            ).selectionPairs[index].value.map((v, index_) => (
+                              <div key={index_}>
+                                <Field
+                                  name={`config.selectionPairs[${index}].value[${index_}]`}
+                                />
+                                <Button
+                                  onClick={() => arrayHelpers.remove(index)}
+                                >
+                                  Remove Part 2 Answer Option
+                                </Button>
+                              </div>
+                            ))}
+                            <Button
+                              onClick={() =>
+                                arrayHelpers.push({
+                                  key: '',
+                                  value: [],
+                                })
+                              }
+                            >
+                              Add Part 2 Answer Option
+                            </Button>
+                          </div>
+                        )}
+                      />
+
+                      <Button onClick={() => arrayHelpers.remove(index)}>
+                        Remove
+                      </Button>
+                    </TitledContainer>
+                  ))}
+                  <Button
+                    onClick={() =>
+                      arrayHelpers.push({
+                        key: '',
+                        value: [],
+                      })
+                    }
+                  >
+                    Add Answer Pair
+                  </Button>
+                </div>
+              )}
+            />
+          </TitledContainer>
+        </>
+      )}
+    </QuestionFormShell>
+  );
+};
