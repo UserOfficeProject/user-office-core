15370ea496655f6ccb5791c1523e22f1331d0bd1
Jay Quan <b331998513@gmail.com>
2022-12-15 18:37:38 +0100

    Fix: Some modals missing close modal button (#116)
    * Added navigation link
    
    * removed comments
    
    * Put css in one place
    
    * Fix: Some modals missing close modal button
    
    * fix: follow the same modal design pattern

--- apps/user-office-frontend/src/components/template/QuestionEditor.tsx
+++ apps/user-office-frontend/src/components/template/QuestionEditor.tsx
@@ -1,3 +1,6 @@
+import CloseIcon from '@mui/icons-material/Close';
+import IconButton from '@mui/material/IconButton';
+import makeStyles from '@mui/styles/makeStyles';
 import React from 'react';
 
 import StyledModal from 'components/common/StyledModal';
@@ -5,34 +8,56 @@ import { createQuestionForm } from 'components/questionary/QuestionaryComponentR
 import { Question, Template } from 'generated/sdk';
 import { Event, EventType } from 'models/questionary/QuestionaryEditorModel';
 
+const useStyles = makeStyles((theme) => ({
+  closeButton: {
+    position: 'absolute',
+    right: theme.spacing(1),
+    top: theme.spacing(1),
+    '& > svg': {
+      color: theme.palette.grey[600],
+    },
+  },
+}));
+
 export default function QuestionEditor(props: {
   field: Question | null;
   dispatch: React.Dispatch<Event>;
   closeMe: () => void;
   template: Template;
 }) {
+  const classes = useStyles();
+
   if (props.field === null) {
     return null;
   }
 
   return (
-    <StyledModal onClose={props.closeMe} open={props.field != null}>
-      {createQuestionForm({
-        question: props.field,
-        onUpdated: (question) => {
-          props.dispatch({
-            type: EventType.QUESTION_UPDATED,
-            payload: question,
-          });
-        },
-        onDeleted: (question) => {
-          props.dispatch({
-            type: EventType.QUESTION_DELETED,
-            payload: question,
-          });
-        },
-        closeMe: props.closeMe,
-      })}
+    <StyledModal open={props.field != null}>
+      <>
+        <IconButton
+          data-cy="close-modal-btn"
+          className={classes.closeButton}
+          onClick={props.closeMe}
+        >
+          <CloseIcon />
+        </IconButton>
+        {createQuestionForm({
+          question: props.field,
+          onUpdated: (question) => {
+            props.dispatch({
+              type: EventType.QUESTION_UPDATED,
+              payload: question,
+            });
+          },
+          onDeleted: (question) => {
+            props.dispatch({
+              type: EventType.QUESTION_DELETED,
+              payload: question,
+            });
+          },
+          closeMe: props.closeMe,
+        })}
+      </>
     </StyledModal>
   );
 }
