0258690ea4460350947933464c49b30972a77c3c
Jekabs Karklins <Jekabs.Karklins@ess.eu>
2023-01-01 17:00:23 +0100

    fix: use multiGraylog

--- apps/user-office-backend/src/config/ess/configureGrayLogLogger.ts
+++ apps/user-office-backend/src/config/ess/configureGrayLogLogger.ts
@@ -1,8 +1,10 @@
 import {
   ConsoleLogger,
   GrayLogLogger,
+  MultiLogg,
   setLogger,
 } from '@user-office-software/duo-logger';
+
 export function configureGraylogLogger() {
   const server = process.env.GRAYLOG_SERVER;
   const port = process.env.GRAYLOG_PORT;
@@ -10,12 +12,14 @@ export function configureGraylogLogger() {
   if (server && port) {
     const env = process.env.NODE_ENV || 'unset';
     setLogger(
-      new GrayLogLogger(
-        server,
-        parseInt(port),
-        { facility: 'DMSC', environment: env, service: 'duo-backend' },
-        ['QueryName', 'UserID']
-      )
+      new MultiLogg([
+        new GrayLogLogger(
+          server,
+          parseInt(port),
+          { facility: 'DMSC', environment: env, service: 'duo-backend' },
+          ['QueryName', 'UserID']
+        ),
+      ])
     );
   } else {
     setLogger(new ConsoleLogger());
