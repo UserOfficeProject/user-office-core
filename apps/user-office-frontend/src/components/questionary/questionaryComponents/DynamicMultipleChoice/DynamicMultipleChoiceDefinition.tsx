4da92501d9ce96f98d6a3b9eb562d08d3ea6ef54
Jay Quan <a331998513@gmail.com>
2023-02-13 16:05:22 +0100

    feat: new questionary component that consume external api

---
+++ apps/user-office-frontend/src/components/questionary/questionaryComponents/DynamicMultipleChoice/DynamicMultipleChoiceDefinition.tsx
@@ -0,0 +1,31 @@
+import RadioButtonCheckedIcon from '@mui/icons-material/RadioButtonChecked';
+import React from 'react';
+
+import defaultRenderer from 'components/questionary/DefaultQuestionRenderer';
+import DynamicMultipleChoiceSearchCriteriaComponent from 'components/questionary/questionaryComponents/DynamicMultipleChoice/DynamicMultipleChoiceSearchCriteriaComponent';
+import { DataType } from 'generated/sdk';
+
+import { QuestionaryComponentDefinition } from '../../QuestionaryComponentRegistry';
+import DynamicMultipleChoiceAnswerRenderer from './DynamicMultipleChoiceAnswerRenderer';
+import { QuestionaryComponentDynamicMultipleChoice } from './QuestionaryComponentDynamicMultipleChoice';
+import { QuestionDynamicMultipleChoiceForm } from './QuestionDynamicMultipleChoiceForm';
+import { QuestionTemplateRelationDynamicMultipleChoiceForm } from './QuestionTemplateRelationDynamicMultipleChoiceForm';
+
+export const dynamicMultipleChoiceDefinition: QuestionaryComponentDefinition = {
+  dataType: DataType.DYNAMIC_MULTIPLE_CHOICE,
+  name: 'Dynamic Multiple choice',
+  questionaryComponent: QuestionaryComponentDynamicMultipleChoice,
+  questionForm: () => QuestionDynamicMultipleChoiceForm,
+  questionTemplateRelationForm: () =>
+    QuestionTemplateRelationDynamicMultipleChoiceForm,
+  readonly: false,
+  creatable: true,
+  icon: <RadioButtonCheckedIcon />,
+  renderers: {
+    answerRenderer: DynamicMultipleChoiceAnswerRenderer,
+    questionRenderer: defaultRenderer.questionRenderer,
+  },
+  createYupValidationSchema: null, //TODO: Create dynamicMultipleChoiceValidationSchema on @user-office-software/duo-validation and put here.
+  getYupInitialValue: ({ answer }) => answer.value || [],
+  searchCriteriaComponent: DynamicMultipleChoiceSearchCriteriaComponent,
+};
---
+++ apps/user-office-frontend/src/components/questionary/questionaryComponents/DynamicMultipleChoice/QuestionaryComponentDynamicMultipleChoice.tsx
@@ -0,0 +1,155 @@
+import FormControl from '@mui/material/FormControl';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import FormHelperText from '@mui/material/FormHelperText';
+import FormLabel from '@mui/material/FormLabel';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import Radio from '@mui/material/Radio';
+import RadioGroup from '@mui/material/RadioGroup';
+import Select, { SelectChangeEvent } from '@mui/material/Select';
+import makeStyles from '@mui/styles/makeStyles';
+import { getIn } from 'formik';
+import React, { useEffect, useState } from 'react';
+
+import MultiMenuItem from 'components/common/MultiMenuItem';
+import { BasicComponentProps } from 'components/proposal/IBasicComponentProps';
+import { DynamicMultipleChoiceConfig } from 'generated/sdk';
+import { useExternalApi } from 'hooks/common/useExternalApi';
+
+const toArray = (input: string | string[]): string[] => {
+  if (typeof input === 'string') {
+    return [input];
+  }
+
+  return input;
+};
+
+const useStyles = makeStyles(() => ({
+  horizontalLayout: {
+    flexDirection: 'row',
+  },
+  verticalLayout: {
+    flexDirection: 'column',
+  },
+}));
+
+export function QuestionaryComponentDynamicMultipleChoice(
+  props: BasicComponentProps
+) {
+  const classes = useStyles();
+
+  const {
+    answer,
+    onComplete,
+    formikProps: { errors, touched },
+  } = props;
+  const {
+    question: { id, question, naturalKey },
+  } = answer;
+
+  const config = answer.config as DynamicMultipleChoiceConfig;
+  const fieldError = getIn(errors, id);
+  const isError = getIn(touched, id) && !!fieldError;
+  const { content } = useExternalApi(config.url);
+  const [stateValue, setStateValue] = useState<Array<string>>(content);
+
+  useEffect(() => {
+    setStateValue(answer.value);
+  }, [answer]);
+
+  const handleOnChange = (event: SelectChangeEvent<string | string[]>) => {
+    const newValue = toArray(event.target.value);
+
+    onComplete(newValue);
+  };
+
+  const label = (
+    <>
+      {question}
+      {config.small_label && (
+        <>
+          <br />
+          <small>{config.small_label}</small>
+        </>
+      )}
+    </>
+  );
+
+  const SelectMenuItem = config.isMultipleSelect ? MultiMenuItem : MenuItem;
+
+  switch (config.variant) {
+    case 'dropdown':
+      return (
+        <FormControl
+          fullWidth
+          required={config.required}
+          error={isError}
+          margin="dense"
+        >
+          <InputLabel id={`questionary-${id}`}>{label}</InputLabel>
+          <Select
+            id={id}
+            value={
+              config.isMultipleSelect
+                ? stateValue
+                : stateValue.length > 0
+                ? stateValue[0]
+                : ''
+            }
+            onChange={handleOnChange}
+            multiple={config.isMultipleSelect}
+            labelId={`questionary-${id}`}
+            required={config.required}
+            renderValue={(item) =>
+              config.isMultipleSelect
+                ? (item as string[]).join(', ')
+                : (item as string)
+            }
+            MenuProps={{
+              variant: 'menu',
+            }}
+            data-natural-key={naturalKey}
+          >
+            {content.map((option) => {
+              return (
+                <SelectMenuItem value={option} key={option}>
+                  {option}
+                </SelectMenuItem>
+              );
+            })}
+          </Select>
+          {isError && <FormHelperText>{fieldError}</FormHelperText>}
+        </FormControl>
+      );
+
+    default:
+      return (
+        <FormControl required={config.required} error={isError} margin="dense">
+          <FormLabel>{label}</FormLabel>
+          <RadioGroup
+            id={id}
+            name={id}
+            value={stateValue[0] || ''}
+            onChange={handleOnChange}
+            className={
+              content.length < 3
+                ? classes.horizontalLayout
+                : classes.verticalLayout
+            }
+          >
+            {content.map((option) => {
+              return (
+                <FormControlLabel
+                  value={option}
+                  key={option}
+                  control={<Radio />}
+                  label={option}
+                />
+              );
+            })}
+          </RadioGroup>
+          {isError && <FormHelperText>{fieldError}</FormHelperText>}
+        </FormControl>
+      );
+  }
+}
