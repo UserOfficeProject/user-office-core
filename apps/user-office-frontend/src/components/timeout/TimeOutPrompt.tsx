bf0e5a83ab1610b550e8d64c7d89874d580a8d16
Thomas Cottee Meldrum <thomas.cottee-meldrum@stfc.ac.uk>
2023-02-22 17:15:23 +0000

    feat: Idle timeout warning user-office-project-issue-tracker#791

---
+++ apps/user-office-frontend/src/components/timeout/TimeOutPrompt.tsx
@@ -0,0 +1,58 @@
+import {
+  Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
+  // useTheme,
+} from '@mui/material';
+import makeStyles from '@mui/styles/makeStyles';
+import React from 'react';
+
+type IdleTimeoutPromptProps = {
+  isIdle: boolean;
+  buttonMessage: string;
+  onConfirm: () => void;
+};
+
+const useStyles = makeStyles((theme) => ({
+  title: {
+    marginTop: '12px',
+    color: theme.palette.error.main,
+  },
+  body: {
+    display: 'grid',
+    placeItems: 'center',
+  },
+}));
+
+function IdleTimeoutPrompt(props: IdleTimeoutPromptProps): JSX.Element {
+  const classes = useStyles();
+
+  return (
+    <>
+      <Dialog fullWidth data-cy="timeout-dialog" open={props.isIdle}>
+        <DialogTitle className={classes.title}>{'Warning'}</DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+            {
+              'You are at risk of being logged out and losing any unsaved data due to inactivity. Please confirm you are still using the system'
+            }
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button
+            onClick={props.onConfirm}
+            data-cy="confirm-idle"
+            variant="text"
+          >
+            {'Close'}
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </>
+  );
+}
+
+export default IdleTimeoutPrompt;
---
+++ apps/user-office-frontend/src/context/IdleContextProvider.tsx
@@ -0,0 +1,138 @@
+import jwtDecode from 'jwt-decode';
+import PropTypes from 'prop-types';
+import React, { useContext, useEffect, useRef } from 'react';
+
+import IdleTimeoutPrompt from 'components/timeout/TimeOutPrompt';
+import { FeatureId, SettingsId } from 'generated/sdk';
+import { useUnauthorizedApi } from 'hooks/common/useDataApi';
+
+import { FeatureContext } from './FeatureContextProvider';
+import { SettingsContext } from './SettingsContextProvider';
+import { UserContext } from './UserContextProvider';
+
+interface IdleContextData {
+  sessionTimer: NodeJS.Timeout | undefined;
+  logoutTimer: NodeJS.Timeout | undefined;
+  isIdle: boolean;
+  handleUserActive: () => void;
+}
+
+const initIdleData: IdleContextData = {
+  sessionTimer: undefined,
+  logoutTimer: undefined,
+  isIdle: false,
+  handleUserActive: () => {},
+};
+
+export const IdleContext = React.createContext<IdleContextData>(initIdleData);
+
+export const EmptyIdleContextProvider: React.FC = (props) => {
+  const [state] = React.useState(initIdleData);
+
+  return (
+    <IdleContext.Provider
+      value={{
+        ...state,
+      }}
+    >
+      {props.children}
+    </IdleContext.Provider>
+  );
+};
+
+export const IdleContextProvider: React.FC = (props) => {
+  const [state, setState] = React.useState(initIdleData);
+  // Separate state to avoid weirdness  s timeouts use the state of when they are started
+
+  const unauthorizedApi = useUnauthorizedApi();
+  const { token } = useContext(UserContext);
+  const { settingsMap } = useContext(SettingsContext);
+
+  const externalAuthLoginUrl = settingsMap.get(
+    SettingsId.EXTERNAL_AUTH_LOGIN_URL
+  )?.settingsValue;
+  const idleTimeoutLength = settingsMap.get(
+    SettingsId.IDLE_TIMEOUT
+  )?.settingsValue;
+
+  const idleTimeout = useRef<NodeJS.Timeout>();
+
+  const userIdleTimer = function () {
+    if (idleTimeout.current) {
+      clearTimeout(idleTimeout.current);
+    }
+
+    return setTimeout(
+      () => {
+        setState({
+          ...state,
+          isIdle: true,
+        });
+      },
+      idleTimeoutLength ? parseInt(idleTimeoutLength) : 3600000
+    );
+  };
+
+  function userActive() {
+    idleTimeout.current = userIdleTimer();
+
+    if (state.isIdle)
+      setState({
+        ...state,
+        isIdle: false,
+      });
+  }
+
+  function userActiveButton() {
+    console.log((jwtDecode(token) as any).externalToken);
+    unauthorizedApi()
+      .checkExternalToken({ token: (jwtDecode(token) as any).externalToken })
+      .then((res) => {
+        if (!res.checkExternalToken.isValid && externalAuthLoginUrl) {
+          const url = new URL(externalAuthLoginUrl);
+          url.searchParams.set('redirect_uri', encodeURI(window.location.href));
+          window.location.href = url.toString();
+        }
+      });
+
+    userActive();
+  }
+
+  useEffect(() => {
+    idleTimeout.current = userIdleTimer();
+
+    return () => {
+      if (idleTimeout.current) clearTimeout(idleTimeout.current);
+    };
+  }, []);
+
+  return (
+    <IdleContext.Provider
+      value={{
+        ...state,
+        handleUserActive: userActive,
+      }}
+    >
+      {props.children}
+      <IdleTimeoutPrompt isIdle={state.isIdle} onConfirm={userActiveButton} />
+    </IdleContext.Provider>
+  );
+};
+
+IdleContextProvider.propTypes = { children: PropTypes.node.isRequired };
+
+export const IdleContextPicker: React.FC = ({ children }) => {
+  const featureContext = useContext(FeatureContext);
+
+  const isIdleContextEnabled = featureContext.featuresMap.get(
+    FeatureId.STFC_IDLE_TIMER
+  )?.isEnabled;
+
+  return isIdleContextEnabled ? (
+    <IdleContextProvider>{children}</IdleContextProvider>
+  ) : (
+    <EmptyIdleContextProvider>{children}</EmptyIdleContextProvider>
+  );
+};
+
+IdleContextPicker.propTypes = { children: PropTypes.node.isRequired };
