f675bac70d30aad93becbf9fbb8dea8c029d9f66
Farai Mutambara <farai.mutambara@stfc.ac.uk>
2023-02-28 13:06:23 +0000

    bump up timeout for Idle to patch 125

--- apps/user-office-backend/db_patches/0125_AddEndSessionUrl.sql
+++ apps/user-office-backend/db_patches/0125_AddEndSessionUrl.sql
@@ -7,7 +7,9 @@ BEGIN
         settings(settings_id, settings_value, description)
       VALUES
         ('EXTERNAL_AUTH_LOGOUT_URL', '', 'URL which terminates the external auth session');
-
+    INSERT INTO 
+        settings(settings_id, description) 
+      VALUES ('IDLE_TIMEOUT', 'Timeout for Idle timer in milliseconds');
 
     DELETE FROM features WHERE feature_id = 'EXTERNAL_AUTH';
 
