8f0af5bbbeb85b647d2798f18546d92fc425846c
Farai Mutambara <farai.mutambara@stfc.ac.uk>
2023-02-28 14:03:15 +0000

    fix: revert changes

--- apps/user-office-backend/db_patches/0125_AddEndSessionUrl.sql
+++ apps/user-office-backend/db_patches/0125_AddEndSessionUrl.sql
@@ -7,10 +7,7 @@ BEGIN
         settings(settings_id, settings_value, description)
       VALUES
         ('EXTERNAL_AUTH_LOGOUT_URL', '', 'URL which terminates the external auth session');
-    INSERT INTO 
-        settings(settings_id, description) 
-      VALUES 
-        ('IDLE_TIMEOUT', 'Timeout for Idle timer in milliseconds');
+
 
     DELETE FROM features WHERE feature_id = 'EXTERNAL_AUTH';
 
--- apps/user-office-backend/db_patches/0130_AddTimedLogoutWarningFeature.sql
+++ apps/user-office-backend/db_patches/0130_AddTimedLogoutWarningFeature.sql
@@ -3,10 +3,8 @@ $$
 BEGIN
     IF register_patch('AddTimedLogoutWarningfeature.sql', 'Thomas Cottee Meldrum', 'Create STFC idle warning feature', '2023-02-14') THEN
 
-    INSERT INTO 
-        features(feature_id, description) 
-       VALUES 
-        ('STFC_IDLE_TIMER', 'STFC idle warning popup');
+    INSERT INTO features(feature_id, description) VALUES ('STFC_IDLE_TIMER', 'STFC idle warning popup');
+    INSERT INTO settings(settings_id, description) VALUES ('IDLE_TIMEOUT', 'Timeout for Idle timer in milliseconds');
 
     END IF;
 END;
