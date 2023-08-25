e845e1bec3dd336354f7c9c967e622f027a59088
Martin Trajanovski <martin.trajanovski@gmail.com>
2023-08-25 09:41:40 +0200

    add description and insert some status actions

--- apps/user-office-backend/db_patches/0134_AddWorkflowStatusActions.sql
+++ apps/user-office-backend/db_patches/0134_AddWorkflowStatusActions.sql
@@ -6,9 +6,15 @@ BEGIN
 			CREATE TABLE IF NOT EXISTS proposal_status_actions (
 				proposal_status_action_id serial UNIQUE,
 				name VARCHAR(64) NOT NULL,
+				description TEXT NULL,
 				default_config jsonb,
 				type VARCHAR(512) NOT NULL
 			);
+
+			INSERT INTO proposal_status_actions(name, description, default_config, type)
+    	VALUES('Email action', 'This is an action for email sending. It can be configured to use different recipients and email templates.', '{}', 'EMAIL');
+			INSERT INTO proposal_status_actions(name, description, default_config, type)
+    	VALUES('RabbitMQ action', 'This is an action for sending messages to a specific exchange. It can be configured to use different exchange name recipients and data.', '{}', 'RABBITMQ');
     END;
 	END IF;
 END;
