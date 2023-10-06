37a9068e986fd44d7022fadbb809a8da5c42cef8
Yoganandan <yoganandan.pandiyan@ess.eu>
2023-10-06 13:43:14 +0200

    Make sepId nullable.

--- apps/backend/src/models/CallHasInstrument.ts
+++ apps/backend/src/models/CallHasInstrument.ts
@@ -4,6 +4,6 @@ export class CallHasInstrument {
     public instrumentId: number,
     public availabilityTime: number,
     public submitted: boolean,
-    public sepId?: number
+    public sepId: number | null
   ) {}
 }
