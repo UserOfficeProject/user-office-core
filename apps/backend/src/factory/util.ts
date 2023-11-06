a70a28b8ca2b8d324c42d9555619c64100c4e2ce
Martin Trajanovski <martin.trajanovski@gmail.com>
2023-11-06 14:42:22 +0100

    build(packages): try to remove deprecated request package and use node fetch api instead

--- apps/backend/src/factory/util.ts
+++ apps/backend/src/factory/util.ts
@@ -1,6 +1,5 @@
 import { logger } from '@user-office-software/duo-logger';
 import { DateTime } from 'luxon';
-import request from 'request';
 
 import { Answer } from '../models/Questionary';
 import { DataType } from '../models/Template';
@@ -23,17 +22,6 @@ export const getFileAttachments = (answer: Answer): Attachment[] => {
 
   return [];
 };
-
-export const bufferRequestBody = (req: request.Request) =>
-  new Promise((resolve) => {
-    const buffer: Buffer[] = [];
-
-    req.on('data', (chunk) =>
-      buffer.push(typeof chunk === 'string' ? Buffer.from(chunk) : chunk)
-    );
-
-    req.on('complete', () => resolve(Buffer.concat(buffer).toString()));
-  });
 
 const DATE_TIMESTAMP_FORMAT = 'yyyy-MM-dd_HHmmss';
 
