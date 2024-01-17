53f618e7065a662e2c24c461ce942a896d2c349b
Ellen Wright <ellen.wright@stfc.ac.uk>
2024-01-17 12:59:18 +0000

    Remove changes to getFullUserName

--- apps/frontend/src/utils/user.ts
+++ apps/frontend/src/utils/user.ts
@@ -2,16 +2,8 @@ import { BasicUserDetails } from 'generated/sdk';
 import { BasicUserData } from 'hooks/user/useUserData';
 
 export const getFullUserName = (
-  user?: Pick<
-    BasicUserDetails,
-    'firstname' | 'lastname' | 'preferredname'
-  > | null
-): string =>
-  user
-    ? `${user.preferredname}`
-      ? `${user.preferredname} ${user.lastname}`
-      : `${user.firstname} ${user.lastname}`
-    : 'None';
+  user?: Pick<BasicUserDetails, 'firstname' | 'lastname'> | null
+): string => (user ? `${user.firstname} ${user.lastname}` : 'None');
 
 export const getFullUserNameWithEmail = (
   user?: Pick<
