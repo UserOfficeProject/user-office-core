ab1ae268577f828c45c44931bf0324bb12df9a36
martintrajanovski <martin.trajanovski@gmail.com>
2024-02-26 10:42:22 +0100

    fix db seeds

--- apps/backend/db_patches/db_seeds/0001_ProposalForScheduling.sql
+++ apps/backend/db_patches/db_seeds/0001_ProposalForScheduling.sql
@@ -6,8 +6,8 @@ BEGIN
   INSERT INTO instruments (instrument_id, name, short_code, description, manager_user_id) VALUES (2, 'Instrument 2', 'INSTR2', 'Test instrument 2', 0);
   INSERT INTO instruments (instrument_id, name, short_code, description, manager_user_id) VALUES (3, 'Instrument 3', 'INSTR3', 'Test instrument 3', 0);
   
-  INSERT INTO call_has_instruments (call_id, instrument_id, availability_time, submitted) VALUES (1, 1, NULL, false);
-  INSERT INTO call_has_instruments (call_id, instrument_id, availability_time, submitted) VALUES (1, 3, NULL, false);
+  INSERT INTO call_has_instruments (call_id, instrument_id, availability_time) VALUES (1, 1, NULL);
+  INSERT INTO call_has_instruments (call_id, instrument_id, availability_time) VALUES (1, 3, NULL);
 
   INSERT INTO questionaries(template_id, created_at, creator_id) VALUES (1, NOW(), 1);
 
