0fdcf058324607bc0e6191a4d666df48a1638675
deepak jaison <deepak.jaison@stfc.ac.uk>
2024-03-28 16:19:15 +0000

    e2e tests

--- apps/e2e/cypress/e2e/proposalAdministration.cy.ts
+++ apps/e2e/cypress/e2e/proposalAdministration.cy.ts
@@ -1,6 +1,11 @@
 import { faker } from '@faker-js/faker';
-import { FeatureId } from '@user-office-software-libs/shared-types';
+import {
+  DataType,
+  FeatureId,
+  TemplateCategoryId,
+} from '@user-office-software-libs/shared-types';
 import { DateTime } from 'luxon';
+import PdfParse from 'pdf-parse';
 
 import featureFlags from '../support/featureFlags';
 import initialDBData from '../support/initialDBData';
@@ -85,6 +90,384 @@ context('Proposal administration tests', () => {
       cy.visit('/');
     });
 
+    it('Should be able to set comment for user/manager and final status', function () {
+      if (!featureFlags.getEnabledFeatures().get(FeatureId.TECHNICAL_REVIEW)) {
+        this.skip();
+      }
+
+      cy.assignProposalsToInstruments({
+        instrumentIds: [initialDBData.instrument1.id],
+        proposalPks: [createdProposalPk],
+      });
+
+      cy.contains('Proposals').click();
+
+      cy.get('[data-cy=view-proposal]').click();
+      cy.finishedLoading();
+      cy.get('[role="dialog"]').contains('Admin').click();
+      cy.get('[data-cy="proposal-final-status"]').should('exist');
+      cy.get('[role="dialog"]').contains('Logs').click();
+      cy.get('[role="dialog"]').contains('Admin').click();
+
+      cy.get('[data-cy="proposal-final-status"]').click();
+
+      cy.get('[data-cy="proposal-final-status-options"] li')
+        .contains('Accepted')
+        .click();
+
+      cy.get(
+        `[data-cy="managementTimeAllocation-${createdInstrumentId}"] label`
+      ).should('include.text', initialDBData.call.allocationTimeUnit);
+
+      cy.get(
+        `[data-cy="managementTimeAllocation-${createdInstrumentId}"] input`
+      )
+        .clear()
+        .type('-123')
+        .blur();
+      cy.get<JQuery<HTMLInputElement>>(
+        `[data-cy="managementTimeAllocation-${createdInstrumentId}"] input`
+      ).then(($input) => {
+        expect($input[0].validity.valid).to.be.false;
+        expect($input[0].validationMessage).to.include(
+          'Value must be greater than or equal to 0'
+        );
+      });
+
+      cy.get(
+        `[data-cy="managementTimeAllocation-${createdInstrumentId}"] input`
+      )
+        .clear()
+        .type('987654321')
+        .blur();
+      cy.get<JQuery<HTMLInputElement>>(
+        `[data-cy="managementTimeAllocation-${createdInstrumentId}"] input`
+      ).then(($input) => {
+        expect($input[0].validity.valid).to.be.false;
+        expect($input[0].validationMessage).to.include(
+          `Value must be less than or equal to ${1e5}`
+        );
+      });
+
+      cy.get(
+        `[data-cy="managementTimeAllocation-${createdInstrumentId}"] input`
+      )
+        .clear()
+        .type('20');
+
+      cy.get('[data-cy="commentForUser"]').clear().type(textUser);
+      cy.get('[data-cy="commentForManagement"]').clear().type(textManager);
+
+      cy.on('window:confirm', (str) => {
+        expect(str).to.equal(
+          'Changes you recently made in this tab will be lost! Are you sure?'
+        );
+
+        return false;
+      });
+
+      cy.contains('Proposal information').click();
+
+      cy.get('[data-cy="is-management-decision-submitted"]').click();
+
+      cy.get('[data-cy="save-admin-decision"]').click();
+
+      cy.notification({ variant: 'success', text: 'Saved' });
+
+      cy.reload();
+
+      cy.get('[data-cy="commentForUser"] textarea').should(
+        'have.value',
+        textUser
+      );
+
+      cy.get('[data-cy="commentForManagement"] textarea').should(
+        'have.value',
+        textManager
+      );
+
+      cy.get(
+        `[data-cy="managementTimeAllocation-${createdInstrumentId}"] input`
+      ).should('have.value', '20');
+
+      cy.get('[data-cy="is-management-decision-submitted"] input').should(
+        'have.value',
+        'true'
+      );
+
+      cy.closeModal();
+
+      cy.contains('Accepted');
+      cy.contains('DRAFT');
+      cy.get("[aria-label='Show Columns']").first().click();
+      cy.get('.MuiPopover-paper').contains('Final time allocation').click();
+      cy.get('body').click();
+      cy.contains(proposalName1)
+        .parent()
+        .should('include.text', initialDBData.call.allocationTimeUnit);
+    });
+
+    it('Should show warning if proposal status is changing to SCHEDULING and proposal has no instrument', () => {
+      cy.contains('Proposals').click();
+
+      if (proposalName1) {
+        cy.contains(proposalName1).parent().find('[type="checkbox"]').check();
+      } else {
+        cy.get('[type="checkbox"]').first().check();
+      }
+
+      cy.get('[data-cy="change-proposal-status"]').click();
+
+      cy.get('[role="presentation"] .MuiDialogContent-root').as('dialog');
+      cy.get('@dialog').contains('Change proposal/s status');
+
+      cy.get('@dialog')
+        .find('#selectedStatusId-input')
+        .should('not.have.class', 'Mui-disabled');
+
+      cy.get('@dialog').find('#selectedStatusId-input').click();
+
+      cy.get('[role="listbox"]').contains('SCHEDULING').click();
+
+      cy.get('[role="alert"] .MuiAlert-message')
+        .should('exist')
+        .and(
+          'contain.text',
+          'Be aware that proposal/s not assigned to an instrument will not be shown in the scheduler after changing status to "SCHEDULING"'
+        );
+    });
+
+    it('Should be able to re-open proposal for submission', () => {
+      cy.contains('Proposals').click();
+
+      if (proposalName1) {
+        cy.contains(proposalName1).parent().find('[type="checkbox"]').check();
+      } else {
+        cy.get('[type="checkbox"]').first().check();
+      }
+
+      cy.get('[data-cy="change-proposal-status"]').click();
+
+      cy.get('[role="presentation"] .MuiDialogContent-root').as('dialog');
+      cy.get('@dialog').contains('Change proposal/s status');
+
+      cy.get('@dialog')
+        .find('#selectedStatusId-input')
+        .should('not.have.class', 'Mui-disabled');
+
+      cy.get('@dialog').find('#selectedStatusId-input').click();
+
+      cy.get('[role="listbox"]').contains('DRAFT').click();
+
+      cy.get('[role="alert"] .MuiAlert-message').contains(
+        'Be aware that changing status to "DRAFT" will reopen proposal for changes and submission.'
+      );
+
+      cy.get('[data-cy="submit-proposal-status-change"]').click();
+
+      cy.notification({
+        variant: 'success',
+        text: 'status changed successfully',
+      });
+
+      cy.contains(proposalName1).parent().contains('No');
+
+      if (featureFlags.getEnabledFeatures().get(FeatureId.SCHEDULER)) {
+        cy.logout();
+      }
+
+      cy.login('user1', initialDBData.roles.user);
+      cy.visit('/');
+
+      cy.contains(proposalName1)
+        .parent()
+        .get('[aria-label="Edit proposal"]')
+        .click();
+
+      cy.finishedLoading();
+      cy.contains(proposalName1);
+
+      cy.contains('Submit').parent().should('not.be.disabled');
+    });
+
+    it('If you select a tab in tabular view and reload the page it should stay on specific selected tab', function () {
+      if (!featureFlags.getEnabledFeatures().get(FeatureId.TECHNICAL_REVIEW)) {
+        this.skip();
+      }
+      cy.contains('Proposals').click();
+
+      cy.get('[data-cy=view-proposal]').click();
+      cy.finishedLoading();
+
+      cy.get('[role="dialog"]').contains('Admin').click();
+
+      cy.reload();
+
+      cy.get('[data-cy="commentForUser"]').should('exist');
+
+      cy.get('[role="dialog"]').contains('Technical review').click();
+
+      cy.reload();
+
+      cy.get('button[role="tab"]')
+        .contains('Technical review')
+        .should('have.attr', 'aria-selected', 'true');
+    });
+
+    it('Download proposal is working with dialog window showing up', () => {
+      cy.createProposal({ callId: initialDBData.call.id }).then((result) => {
+        if (result.createProposal) {
+          cy.updateProposal({
+            proposalPk: result.createProposal.primaryKey,
+            proposerId: existingUserId,
+            title: proposalFixedName,
+            abstract: proposalName2,
+          });
+        }
+      });
+      cy.contains(proposalName1)
+        .parent()
+        .find('input[type="checkbox"]')
+        .check();
+
+      cy.get('[data-cy="download-proposals"]').click();
+
+      cy.contains('Proposal(s)').click();
+
+      cy.get('[data-cy="preparing-download-dialog"]').should('exist');
+      cy.get('[data-cy="preparing-download-dialog-item"]').contains(
+        proposalName1
+      );
+
+      cy.contains(proposalFixedName)
+        .parent()
+        .find('input[type="checkbox"]')
+        .check();
+
+      cy.get('[data-cy="download-proposals"]').click();
+
+      cy.contains('Proposal(s)').click();
+
+      cy.get('[data-cy="preparing-download-dialog"]').should('exist');
+      cy.get('[data-cy="preparing-download-dialog-item"]').contains(
+        '2 selected items'
+      );
+    });
+
+    it('Download proposal attachment(s) working with dialog window showing up', () => {
+      cy.createProposal({ callId: initialDBData.call.id }).then((result) => {
+        if (result.createProposal) {
+          cy.updateProposal({
+            proposalPk: result.createProposal.primaryKey,
+            proposerId: existingUserId,
+            title: proposalFixedName,
+            abstract: proposalName2,
+          });
+        }
+      });
+      cy.createTopic({
+        templateId: initialDBData.template.id,
+        sortOrder: 1,
+      }).then((topicResult) => {
+        if (topicResult.createTopic) {
+          const topicId =
+            topicResult.createTopic.steps[
+              topicResult.createTopic.steps.length - 1
+            ].topic.id;
+          cy.createQuestion({
+            categoryId: TemplateCategoryId.PROPOSAL_QUESTIONARY,
+            dataType: DataType.FILE_UPLOAD,
+          }).then((questionResult) => {
+            const createdQuestion = questionResult.createQuestion;
+            if (createdQuestion) {
+              cy.updateQuestion({
+                id: createdQuestion.id,
+                question: initialDBData.questions.fileUpload.text,
+                config: `{"file_type":[".pdf",".docx","image/*"]}`,
+              });
+              cy.createQuestionTemplateRelation({
+                templateId: initialDBData.template.id,
+                sortOrder: 0,
+                topicId: topicId,
+                questionId: createdQuestion.id,
+              });
+            }
+          });
+        }
+      });
+
+      cy.contains(proposalName1)
+        .parent()
+        .find('input[type="checkbox"]')
+        .check();
+
+      cy.get('[data-cy="download-proposals"]').click();
+
+      cy.contains('Attachment(s)').click();
+
+      cy.get('[data-cy="attachmentQuestionName"]').click();
+      cy.get('[role="presentation"]')
+        .contains(initialDBData.questions.fileUpload.text)
+        .click();
+
+      cy.get('[data-cy="proposalAttachmentDownloadButton"]').click();
+
+      cy.get('[data-cy="preparing-download-dialog"]').should('exist');
+      cy.get('[data-cy="preparing-download-dialog-item"]').contains(
+        'attachment'
+      );
+
+      cy.contains(proposalFixedName)
+        .parent()
+        .find('input[type="checkbox"]')
+        .check();
+
+      cy.get('[data-cy="download-proposals"]').click();
+
+      cy.contains('Attachment(s)').click();
+
+      cy.get('[data-cy="attachmentQuestionName"]').click();
+      cy.get('[role="presentation"]')
+        .contains(initialDBData.questions.fileUpload.text)
+        .click();
+
+      cy.get('[data-cy="proposalAttachmentDownloadButton"]').click();
+
+      cy.get('[data-cy="preparing-download-dialog"]').should('exist');
+      cy.get('[data-cy="preparing-download-dialog-item"]').contains(
+        '2 selected items'
+      );
+    });
+
+    it('Downloaded proposal filename format is RB_SURNAME_YYYY', function () {
+      if (!featureFlags.getEnabledFeatures().get(FeatureId.SCHEDULER)) {
+        //temporarily skipping, until issue is fixed on github actions
+        this.skip();
+      }
+      cy.contains('Proposals').click();
+
+      cy.contains(proposalName1)
+        .parent()
+        .find('input[type="checkbox"]')
+        .check();
+
+      cy.get('[data-cy="download-proposals"]').click();
+
+      cy.contains('Proposal(s)').click();
+
+      cy.get('[data-cy="preparing-download-dialog"]').should('exist');
+      cy.get('[data-cy="preparing-download-dialog-item"]').contains(
+        proposalName1
+      );
+
+      const currentYear = new Date().getFullYear();
+      const downloadedFileName = `${createdProposalId}_${initialDBData.users.user1.lastName}_${currentYear}.pdf`;
+      const downloadsFolder = Cypress.config('downloadsFolder');
+      const downloadFilePath = `${downloadsFolder}/${downloadedFileName}`;
+
+      cy.readFile(downloadFilePath).should('exist');
+    });
+
     it('Should be able to whitelist origin for download proposal pdf endpoint', function () {
       if (featureFlags.getEnabledFeatures().get(FeatureId.OAUTH)) {
         this.skip();
@@ -100,6 +483,103 @@ context('Proposal administration tests', () => {
         headers: {
           authorization: `Bearer ${token}`,
           Origin: whitelistedUrl,
+        },
+      }).then((response) => {
+        expect(response.headers['content-type']).to.be.equal('application/pdf');
+        expect(response.status).to.be.equal(200);
+      });
+    });
+
+    it('Should be able to download proposal PDFs and verify their content', function () {
+      if (!featureFlags.getEnabledFeatures().get(FeatureId.SCHEDULER)) {
+        //temporarily skipping, until issue is fixed on github actions
+        this.skip();
+      }
+
+      cy.createProposal({ callId: initialDBData.call.id }).then((result) => {
+        if (result.createProposal) {
+          const newlyCreatedProposalId = result.createProposal.proposalId;
+          const newlyCreatedProposalPk = result.createProposal.primaryKey;
+          cy.updateProposal({
+            proposalPk: result.createProposal.primaryKey,
+            proposerId: existingUserId,
+            title: proposalFixedName,
+            abstract: proposalFixedAbstract,
+          });
+
+          const token = window.localStorage.getItem('token');
+
+          if (!token) {
+            throw new Error('Token not provided');
+          }
+
+          const currentYear = new Date().getFullYear();
+          const downloadedFileName = `${currentYear}_${initialDBData.users.user1.lastName}_${newlyCreatedProposalId}.pdf`;
+          const downloadsFolder = Cypress.config('downloadsFolder');
+          const downloadFilePath = `${downloadsFolder}/${downloadedFileName}`;
+
+          cy.task('downloadFile', {
+            url: `${Cypress.config(
+              'baseUrl'
+            )}/download/pdf/proposal/${newlyCreatedProposalPk}`,
+            token: token,
+            filename: downloadedFileName,
+            downloadsFolder: downloadsFolder,
+          });
+
+          cy.task('readPdf', downloadFilePath).then((args) => {
+            const { text, numpages } = args as PdfParse.Result;
+
+            expect(text).to.include(newlyCreatedProposalId);
+            expect(text).to.include(proposalFixedName);
+            expect(text).to.include(proposalFixedAbstract);
+
+            expect(numpages).to.equal(1);
+          });
+
+          // NOTE: We can't test the multi file download file size because the title and abstract are random and it can vary between some numbers. That's why we only test the file content.
+          const downloadedMultiFileName = `${currentYear}_proposals_${createdProposalPk}_${newlyCreatedProposalPk}.pdf`;
+          const multiFileDownloadPath = `${downloadsFolder}/${downloadedMultiFileName}`;
+
+          cy.task('downloadFile', {
+            url: `${Cypress.config(
+              'baseUrl'
+            )}/download/pdf/proposal/${createdProposalPk},${newlyCreatedProposalPk}`,
+            token,
+            filename: downloadedMultiFileName,
+            downloadsFolder: downloadsFolder,
+          });
+
+          cy.task('readPdf', multiFileDownloadPath).then((args) => {
+            const { text, numpages } = args as PdfParse.Result;
+
+            expect(text).to.include(createdProposalId);
+            expect(text).to.include(proposalName1);
+            expect(text).to.include(proposalAbstract1);
+            expect(text).to.include(newlyCreatedProposalId);
+            expect(text).to.include(proposalFixedName);
+            expect(text).to.include(proposalFixedAbstract);
+
+            expect(numpages).to.equal(2);
+          });
+        }
+      });
+    });
+
+    it('Should be able to download proposal pdf', function () {
+      if (!featureFlags.getEnabledFeatures().get(FeatureId.SCHEDULER)) {
+        //temporarily skipping, until issue is fixed on github actions
+        this.skip();
+      }
+      cy.contains('Proposals').click();
+
+      const token = window.localStorage.getItem('token');
+
+      cy.request({
+        url: '/download/pdf/proposal/1',
+        method: 'GET',
+        headers: {
+          authorization: `Bearer ${token}`,
         },
       }).then((response) => {
         expect(response.headers['content-type']).to.be.equal('application/pdf');
