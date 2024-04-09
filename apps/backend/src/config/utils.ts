2980be6ffbb7b5922ee130aeaba82a82d691f7b1
junjiequan <a331998513@gmail.com>
2024-04-09 12:13:05 +0200

    test: test push - do not merge!

--- apps/backend/src/config/utils.ts
+++ apps/backend/src/config/utils.ts
@@ -19,3 +19,25 @@ export const mapValue = <T>(type: symbol, value: T) => {
     useValue: value,
   });
 };
+
+export async function fetchData(url: string) {
+  try {
+    // Making the GET request to the URL
+    const response = await fetch(url);
+
+    // Checking if the response status is not OK (200-299)
+    if (!response.ok) {
+      // Throwing an error with the response status text if the request was not successful
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+
+    // Parsing the JSON response body
+    const data = await response.json();
+
+    // Returning the parsed data
+    return data;
+  } catch (error) {
+    // Re-throwing the error for further handling if necessary
+    throw error;
+  }
+}
