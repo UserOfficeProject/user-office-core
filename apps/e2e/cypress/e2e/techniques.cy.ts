3721725e6f504f6de6b64014b76ab97bc1542717
deepak jaison <deepak.jaison@stfc.ac.uk>
2024-06-12 10:00:15 +0100

    Basic test cases

--- apps/e2e/cypress/e2e/techniques.cy.ts
+++ apps/e2e/cypress/e2e/techniques.cy.ts
@@ -2,11 +2,28 @@ import { faker } from '@faker-js/faker';
 
 import initialDBData from '../support/initialDBData';
 
-context('Instrument tests', () => {
+context('Technique tests', () => {
   const technique1 = {
     name: faker.random.words(2),
     shortCode: faker.random.alphaNumeric(15),
     description: faker.random.words(5),
+  };
+
+  const scientist1 = initialDBData.users.user1;
+  const scientist2 = initialDBData.users.user2;
+
+  const instrument1 = {
+    name: faker.random.words(2),
+    shortCode: faker.random.alphaNumeric(15),
+    description: faker.random.words(5),
+    managerUserId: scientist1.id,
+  };
+
+  const instrument2 = {
+    name: faker.random.words(2),
+    shortCode: faker.random.alphaNumeric(15),
+    description: faker.random.words(5),
+    managerUserId: scientist2.id,
   };
 
   beforeEach(() => {
@@ -30,15 +47,60 @@ context('Instrument tests', () => {
     });
 
     it('User officer should be able to create technique', function () {
-      this.skip();
+      cy.contains('Techniques').click();
+      cy.contains('Create').click();
+      cy.get('#name').type(technique1.name);
+      cy.get('#shortCode').type(technique1.shortCode);
+      cy.get('#description').type(technique1.description);
+
+      cy.get('[data-cy="submit"]').click();
+
+      cy.notification({ variant: 'success', text: 'created successfully' });
+
+      cy.contains(technique1.name);
+      cy.contains(technique1.shortCode);
+      cy.contains(technique1.description);
     });
 
     it('User officer should be able to update technique', function () {
-      this.skip();
+      const newName = faker.random.words(2);
+      const newShortCode = faker.random.alphaNumeric(15);
+      const newDescription = faker.random.words(5);
+      //cy.createTechnique(technique1);
+
+      cy.contains('Techniques').click();
+      cy.contains(technique1.name).parent().find('[aria-label="Edit"]').click();
+      cy.get('#name').clear();
+      cy.get('#name').type(newName);
+      cy.get('#shortCode').clear();
+      cy.get('#shortCode').type(newShortCode);
+      cy.get('#description').type(newDescription);
+      cy.get('[data-cy="submit"]').click();
+
+      cy.notification({ variant: 'success', text: 'updated successfully' });
+
+      cy.get('[data-cy="techniques-table"]').as('techniquesTable');
+
+      cy.get('@techniquesTable').should('contain', newName);
+      cy.get('@techniquesTable').should('contain', newShortCode);
+      cy.get('@techniquesTable').should('contain', newDescription);
     });
 
     it('User officer should be able to delete technique', function () {
-      this.skip();
+      //cy.createTechnique(technique1);
+
+      cy.contains('Techniques').click();
+
+      cy.contains(technique1.name)
+        .parent()
+        .find('[aria-label="Delete"]')
+        .click();
+
+      cy.get('[aria-label="Save"]').click();
+
+      cy.notification({ variant: 'success', text: 'Technique removed' });
+
+      cy.contains(technique1.name).should('not.exist');
     });
   });
 
@@ -46,10 +108,19 @@ context('Instrument tests', () => {
     beforeEach(() => {
       cy.login('officer');
       cy.visit('/');
+      cy.createInstrument(instrument1);
+      cy.createInstrument(instrument2);
     });
 
     it('User officer should be able to assign and unassign instruments to technique without page refresh', function () {
-      this.skip();
+      cy.createInstrument(instrument1);
+      cy.createInstrument(instrument2);
+      //cy.createTechnique(technique1);
+
+      cy.contains(technique1.name)
+        .parent()
+        .find('[aria-label="Delete"]')
+        .click();
     });
   });
 });
