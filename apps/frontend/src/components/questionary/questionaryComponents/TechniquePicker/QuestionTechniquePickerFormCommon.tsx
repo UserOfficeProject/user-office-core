0187884f106b9c58716789a94b0db16af9fbdb3d
Simon Fernandes <simon.fernandes@stfc.ac.uk>
2024-06-28 16:40:56 +0100

    Add initial frontend tech picker changes
    Part of UserOfficeProject/issue-tracker#1097

---
+++ apps/frontend/src/components/questionary/questionaryComponents/TechniquePicker/QuestionTechniquePickerFormCommon.tsx
@@ -0,0 +1,82 @@
+import {
+  FormControl,
+  FormControlLabel,
+  InputLabel,
+  MenuItem,
+  SelectChangeEvent,
+} from '@mui/material';
+import { Field } from 'formik';
+import { Checkbox, Select } from 'formik-mui';
+import React, { useState } from 'react';
+
+import TitledContainer from 'components/common/TitledContainer';
+import { TechniquePickerConfig } from 'generated/sdk';
+const availableVariantOptions = [
+  { label: 'Radio', value: 'radio' },
+  { label: 'Dropdown', value: 'dropdown' },
+];
+export const QuestionTechniquePickerFormCommon = ({
+  config,
+}: {
+  config: TechniquePickerConfig;
+}) => {
+  const [showIsMultipleSelectCheckbox, setShowIsMultipleSelectCheckbox] =
+    useState(config.variant === 'dropdown');
+
+  return (
+    <>
+      <TitledContainer label="Constraints">
+        <FormControlLabel
+          control={
+            <Field
+              name="config.required"
+              component={Checkbox}
+              type="checkbox"
+              inputProps={{ 'data-cy': 'required' }}
+            />
+          }
+          label="Is required"
+        />
+      </TitledContainer>
+      <TitledContainer label="Options">
+        <FormControl fullWidth>
+          <InputLabel htmlFor="config.variant" shrink>
+            Variant
+          </InputLabel>
+          <Field
+            id="config.variant"
+            name="config.variant"
+            type="text"
+            component={Select}
+            data-cy="variant"
+            onChange={(e: SelectChangeEvent) => {
+              setShowIsMultipleSelectCheckbox(e.target.value === 'dropdown');
+            }}
+          >
+            {availableVariantOptions.map(({ value, label }) => {
+              return (
+                <MenuItem value={value} key={value}>
+                  {label}
+                </MenuItem>
+              );
+            })}
+          </Field>
+        </FormControl>
+
+        {showIsMultipleSelectCheckbox && (
+          <FormControlLabel
+            control={
+              <Field
+                name="config.isMultipleSelect"
+                component={Checkbox}
+                type="checkbox"
+                inputProps={{ 'data-cy': 'is-multiple-select' }}
+              />
+            }
+            label="Is multiple select"
+          />
+        )}
+      </TitledContainer>
+    </>
+  );
+};
