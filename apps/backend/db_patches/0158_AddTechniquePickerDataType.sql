602a96316aca1d9e877383efdebc36da24b16cd7
deepak jaison <deepak.jaison@stfc.ac.uk>
2024-06-28 16:01:47 +0100

    path query fix

--- apps/backend/db_patches/0158_AddTechniquePickerDataType.sql
+++ apps/backend/db_patches/0158_AddTechniquePickerDataType.sql
@@ -8,7 +8,7 @@ BEGIN
 
 	CREATE TABLE IF NOT EXISTS technique_has_proposals (
         technique_id integer NOT NULL REFERENCES techniques (technique_id) ON UPDATE CASCADE ON DELETE CASCADE,
-        proposal_id integer NOT NULL REFERENCES  proposals (proposal_id) ON UPDATE CASCADE ON DELETE CASCADE,
+        proposal_id integer NOT NULL REFERENCES  proposals (proposal_pk) ON UPDATE CASCADE ON DELETE CASCADE,
         PRIMARY KEY (technique_id, proposal_id)
     );
 
