b412c2e1cc0d9e898ccdf8f902bd1653d65fe0ca
deepaksftc <149392207+deepaksftc@users.noreply.github.com>
2024-07-31 08:36:58 +0100

    feat: add an option to have bcc recipient for emails sent through proposal status actions (#667)
    * Adding option to include bcc recipient in mails

--- apps/backend/src/eventHandlers/MailService/SMTPMailService.spec.ts
+++ apps/backend/src/eventHandlers/MailService/SMTPMailService.spec.ts
@@ -1,11 +1,24 @@
 import * as path from 'path';
 
+import EmailTemplates from 'email-templates';
+import { container } from 'tsyringe';
+
+import { Tokens } from '../../config/Tokens';
+import { AdminDataSource } from '../../datasources/AdminDataSource';
+import { SettingsId } from '../../models/Settings';
 import EmailSettings from './EmailSettings';
 import { SMTPMailService } from './SMTPMailService';
 
 jest.mock('email-templates');
+const mockAdminDataSource = container.resolve(
+  Tokens.AdminDataSource
+) as AdminDataSource;
+
+const mockGetSetting = jest.spyOn(mockAdminDataSource, 'getSetting');
 
 test('Return result should indicate all emails were successfully sent', async () => {
+  mockGetSetting.mockResolvedValue(null);
+
   const options: EmailSettings = {
     content: {
       template_id: path.resolve('src', 'eventHandlers', 'emails', 'submit'),
@@ -27,6 +40,62 @@ test('Return result should indicate all emails were successfully sent', async ()
 
   const smtpMailService: SMTPMailService = new SMTPMailService();
   const result = await smtpMailService.sendMail(options);
+
+  return expect(result).toStrictEqual({
+    results: {
+      id: 'test',
+      total_accepted_recipients: 1,
+      total_rejected_recipients: 0,
+    },
+  });
+});
+
+test('All emails with bcc were successfully sent', async () => {
+  mockGetSetting.mockClear();
+  const emailInfo = jest.spyOn(EmailTemplates.prototype, 'send');
+
+  const bccEmail = 'testmail@test.co';
+
+  const substitutionData = {
+    piPreferredname: 'John',
+    piLastname: 'Doe',
+    proposalNumber: '1',
+    proposalTitle: 'Title',
+    coProposers: ['Jane Doe'],
+    call: '',
+  };
+
+  mockGetSetting.mockImplementation(() =>
+    Promise.resolve({
+      settingsValue: bccEmail,
+      description: 'bcc mail',
+      id: SettingsId.SMTP_BCC_EMAIL,
+    })
+  );
+
+  const options: EmailSettings = {
+    content: {
+      template_id: path.resolve('src', 'eventHandlers', 'emails', 'submit'),
+    },
+    substitution_data: substitutionData,
+    recipients: [
+      {
+        address: 'john.doe@email.com',
+      },
+    ],
+  };
+
+  const smtpMailService: SMTPMailService = new SMTPMailService();
+  const result = await smtpMailService.sendMail(options);
+
+  expect(emailInfo).toHaveBeenCalledWith({
+    template: expect.any(String),
+    message: {
+      to: process.env.SINK_EMAIL,
+      bcc: bccEmail,
+    },
+    locals: substitutionData,
+  });
 
   return expect(result).toStrictEqual({
     results: {
