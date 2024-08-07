d42ab540d82eee96676c41dd8f03989fd36dbf87
Thomas Cottee Meldrum <81559866+TCMeldrum@users.noreply.github.com>
2024-08-07 14:19:22 +0100

    feat: on proposal assignment page show all proposals in table (#681)
    * feat: on proposal assignment page show all proposals in table
    
    * put max page length to var
    
    * move sorting out of html
    
    * fix sort function

--- apps/frontend/src/components/fap/Proposals/FapProposalsAndAssignmentsTable.tsx
+++ apps/frontend/src/components/fap/Proposals/FapProposalsAndAssignmentsTable.tsx
@@ -716,6 +716,12 @@ const FapProposalsAndAssignmentsTable = ({
     })
   );
 
+  const maxPageLength = FapProposalsWitIdAndFormattedDate.length;
+
+  const pageSizeOptions = [5, 10, 20, maxPageLength]
+    .sort((a, b) => a - b)
+    .filter((n) => n <= maxPageLength);
+
   return (
     <>
       <ProposalReviewModal
@@ -765,6 +771,8 @@ const FapProposalsAndAssignmentsTable = ({
           options={{
             search: true,
             selection: true,
+            pageSize: Math.min(10, maxPageLength),
+            pageSizeOptions: pageSizeOptions,
             headerSelectionProps: {
               inputProps: {
                 'aria-label': 'Select all rows',
