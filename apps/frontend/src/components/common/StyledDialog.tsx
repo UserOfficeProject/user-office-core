4324be37f93d22da0f859c23253e2bbbec9412fa
Yoganandan Pandiyan <p.yoganandan@gmail.com>
2024-08-08 16:21:21 +0200

    Added documentation to the react component

--- apps/frontend/src/components/common/StyledDialog.tsx
+++ apps/frontend/src/components/common/StyledDialog.tsx
@@ -1,3 +1,32 @@
+/**
+ * StyledDialog Component
+ *
+ * This is a customizable dialog component built on top of MUI's Dialog.
+ * It adds optional title and close button functionality.
+ *
+ * Props:
+ * - `title?: string`: Optional. The title displayed at the top of the dialog.
+ * - `onClose?`: Optional. A callback function triggered when the close button
+ *   is clicked or the dialog is closed with the 'Escape' key.
+ *   Passes the event and a 'reason' string ('escapeKeyDown').
+ * - `children`: The content of the dialog. Any components that can be
+ *   placed inside MUI's Dialog can also be used inside StyledDialog.
+ * - Additional props (`DialogProps`) are passed down to the underlying MUI Dialog component.
+ *
+ * Usage:
+ * ```
+ * <StyledDialog
+ *   open={true}
+ *   onClose={handleClose}
+ *   title="Dialog Title"
+ * >
+ *   <DialogContent>
+ *     Your content here.
+ *   </DialogContent>
+ * </StyledDialog>
+ * ```
+ */
+
 import CloseIcon from '@mui/icons-material/Close';
 import { DialogTitle, IconButton } from '@mui/material';
 import Dialog, { DialogProps } from '@mui/material/Dialog';
