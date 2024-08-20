1f52306e4924cab0d6a61f1c3aaac21e4d357a48
Farai Mutambara <farai.mutambara@stfc.ac.uk>
2024-08-20 10:00:36 +0100

    fix failing general faps tests

--- apps/e2e/cypress/e2e/generalFaps.cy.ts
+++ apps/e2e/cypress/e2e/generalFaps.cy.ts
@@ -296,7 +296,7 @@ context('General facility access panel tests', () => {
         text: 'Fap secretary assigned successfully',
       });
 
-      cy.get('[role="dialog"] [role="tab"]').contains('Logs').click();
+      cy.get('button[role="tab"]').contains('Logs').click();
 
       cy.contains(Event.FAP_MEMBERS_ASSIGNED);
 
@@ -430,7 +430,7 @@ context('General facility access panel tests', () => {
 
       cy.contains(fapMembers.reviewer.lastName);
 
-      cy.get('[role="dialog"] [role="tab"]').contains('Logs').click();
+      cy.get('button[role="tab"]').contains('Logs').click();
 
       cy.contains(Event.FAP_MEMBERS_ASSIGNED);
     });
@@ -458,7 +458,7 @@ context('General facility access panel tests', () => {
         text: 'Fap member removed successfully',
       });
 
-      cy.get('[role="dialog"] [role="tab"]').contains('Logs').click();
+      cy.get('button[role="tab"]').contains('Logs').click();
 
       cy.finishedLoading();
 
