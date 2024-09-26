bef81934975363fd9e78bd83922c8e59a79f3cb0
deepak jaison <deepak.jaison@stfc.ac.uk>
2024-09-26 12:03:20 +0100

    Revert last commit changes

--- apps/e2e/cypress/e2e/instruments.cy.ts
+++ apps/e2e/cypress/e2e/instruments.cy.ts
@@ -536,13 +536,12 @@ context('Instrument tests', () => {
       // TODO: Extend here when technical reviewer is added to the table.
     });
 
-    it.only('User Officer should be able to remove assigned scientist from instrument', () => {
+    it('User Officer should be able to remove assigned scientist from instrument', () => {
       cy.assignScientistsToInstrument({
         instrumentId: createdInstrumentId,
         scientistIds: [scientist2.id],
       });
       cy.contains('Instruments').click();
-      cy.finishedLoading();
 
       cy.contains(instrument1.name)
         .parent()
