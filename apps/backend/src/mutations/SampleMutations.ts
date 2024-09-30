ec1acd004f61c2e85865524716c91712b4b6b8bb
Yoganandan Pandiyan <p.yoganandan@gmail.com>
2024-09-30 14:49:52 +0200

    fix: added sample safety reviewers permission to submit review on sample safety

--- apps/backend/src/mutations/SampleMutations.ts
+++ apps/backend/src/mutations/SampleMutations.ts
@@ -9,7 +9,6 @@ import { TemplateDataSource } from '../datasources/TemplateDataSource';
 import { Authorized, EventBus } from '../decorators';
 import { Event } from '../events/event.enum';
 import { rejection } from '../models/Rejection';
-import { Roles } from '../models/Role';
 import { TemplateGroupId } from '../models/Template';
 import { UserWithRole } from '../models/User';
 import { CreateSampleInput } from '../resolvers/mutations/CreateSampleMutation';
@@ -103,7 +102,7 @@ export default class SampleMutations {
   }
 
   @EventBus(Event.PROPOSAL_SAMPLE_REVIEW_SUBMITTED)
-  @Authorized([Roles.USER_OFFICER, Roles.SAMPLE_SAFETY_REVIEWER])
+  @Authorized()
   async updateSample(agent: UserWithRole | null, args: UpdateSampleArgs) {
     const sample = await this.sampleDataSource.getSample(args.sampleId);
     if (sample === null) {
