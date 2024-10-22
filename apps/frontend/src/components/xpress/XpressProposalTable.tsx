9c20314ef9d65e7526dba371df63b55b6bf6ae05
deepak jaison <deepak.jaison@stfc.ac.uk>
2024-10-22 08:54:11 +0100

    Initial commit

--- apps/frontend/src/components/xpress/XpressProposalTable.tsx
+++ apps/frontend/src/components/xpress/XpressProposalTable.tsx
@@ -4,6 +4,7 @@ import MaterialTableCore, {
   Query,
   QueryResult,
 } from '@material-table/core';
+import GridOnIcon from '@mui/icons-material/GridOn';
 import {
   getTranslation,
   ResourceId,
@@ -16,6 +17,7 @@ import { UserContext } from 'context/UserContextProvider';
 import { ProposalsFilter, UserRole } from 'generated/sdk';
 import { CallsDataQuantity, useCallsData } from 'hooks/call/useCallsData';
 import { useDataApi } from 'hooks/common/useDataApi';
+import { useDownloadXLSXProposal } from 'hooks/proposal/useDownloadXLSXProposal';
 import { ProposalViewData } from 'hooks/proposal/useProposalsCoreData';
 import { useProposalStatusesData } from 'hooks/settings/useProposalStatusesData';
 import { useTechniquesData } from 'hooks/technique/useTechniquesData';
@@ -311,6 +313,9 @@ const XpressProposalTable = () => {
     });
   };
 
+  const ExportIcon = (): JSX.Element => <GridOnIcon />;
+  const downloadXLSXProposal = useDownloadXLSXProposal();
+
   return (
     <>
       <StyledContainer maxWidth={false}>
@@ -352,6 +357,22 @@ const XpressProposalTable = () => {
               pageSize: pageSize ? +pageSize : 5,
               initialPage: page ? +page : 0,
             }}
+            actions={[
+              {
+                icon: ExportIcon,
+                tooltip: 'Export proposals in Excel',
+                onClick: (): void => {
+                  downloadXLSXProposal(
+                    searchParams
+                      .getAll('selection')
+                      .filter((item): item is string => !!item)
+                      .map((item) => +item),
+                    'title'
+                  );
+                },
+                position: 'toolbarOnSelect',
+              },
+            ]}
             onSelectionChange={(selectedItems) => {
               setSearchParams((searchParam) => {
                 searchParam.delete('selection');
