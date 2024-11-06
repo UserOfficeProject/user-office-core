83ddcdfd621472ac7a7457af89113b538abce9d5
Gergely Nyiri <gergely.nyiri@eli-alps.hu>
2024-11-06 10:29:12 +0100

    graphql updates for technical review templates

---
+++ apps/frontend/src/models/questionary/technicalReview/TechnicalReviewWithQuestionary.ts
@@ -0,0 +1,16 @@
+/*import { TechnicalReview } from 'generated/sdk';
+
+export type FapReviewWithQuestionary = Pick<
+  TechnicalReview,
+  | 'id'
+  | 'grade'
+  | 'comment'
+  | 'fapID'
+  | 'reviewer'
+  | 'status'
+  | 'questionary'
+  | 'questionaryID'
+  | 'questionary'
+  | 'proposal'
+>;*/
+export {};
