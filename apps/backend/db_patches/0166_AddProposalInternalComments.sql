aeb529e3d000f4ffa27cbc1c31e3cd2084ed7705
Farai Mutambara <farai.mutambara@stfc.ac.uk>
2024-11-20 17:43:30 +0000

    add compound primary key

--- apps/backend/db_patches/0166_AddProposalInternalComments.sql
+++ apps/backend/db_patches/0166_AddProposalInternalComments.sql
@@ -4,9 +4,10 @@ BEGIN
 	IF register_patch('0166_AddProposalInternalCommentTable.sql', 'Farai Mutambara', 'Adding proposal internal comment table', '2024-11-19') THEN
 		BEGIN
 			CREATE TABLE IF NOT EXISTS proposal_internal_comments (
-				comment_id SERIAL PRIMARY KEY,
+				comment_id SERIAL,
 				proposal_pk INT REFERENCES proposals(proposal_pk) ON DELETE CASCADE,
-				comment TEXT NOT NULL
+				comment TEXT NOT NULL,
+				PRIMARY KEY (comment_id, proposal_pk)
 			);
         END;
 	END IF;
