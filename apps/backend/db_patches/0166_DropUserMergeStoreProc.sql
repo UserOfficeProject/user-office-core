57f3e66414584983bf68b53802763cbdc1bfefe3
Chi Kai Lam <chi-kai.lam@stfc.ac.uk>
2024-11-21 16:28:23 +0000

    #1155: Move stored procedure script of merge user tool of the new submission system

--- apps/backend/db_patches/0166_DropUserMergeStoreProc.sql
+++ apps/backend/db_patches/0166_DropUserMergeStoreProc.sql
@@ -1,7 +1,7 @@
 DO
 $$
 BEGIN
-  IF register_patch('0166_DropUserMergeStoreProc.sql', 'Chi Kai Lam', 'Drop stored procedure of merge user tool', '2024-11-21') THEN
+  IF register_patch('0166_DropUserMergeStoredProc.sql', 'Chi Kai Lam', 'Drop stored procedure of merge user tool', '2024-11-21') THEN
     BEGIN
       DROP TABLE MERGING_TABLE_REGISTRY;
       DROP FUNCTION REPLACE_OLD_USER_ID_WITH_NEW;
