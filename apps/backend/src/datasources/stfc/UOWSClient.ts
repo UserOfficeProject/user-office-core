a1df6b4530253804b4f81c3743baf61e34a78d7a
William Edwards <william.edwards@stfc.ac.uk>
2025-01-10 13:50:47 +0000

    Change BASE_URL to a different env variable name

--- apps/backend/src/datasources/stfc/UOWSClient.ts
+++ apps/backend/src/datasources/stfc/UOWSClient.ts
@@ -6,7 +6,7 @@ dotenv.config({ path: process.env.CONFIG_PATH });
 
 export function createUOWSClient() {
   return new UOWS({
-    BASE: process.env.BASE_URL,
+    BASE: process.env.EXTERNAL_UOWS_API_URL,
     HEADERS: {
       Authorization: `Api-key ${process.env.API_KEY}`,
     },
