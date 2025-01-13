4993aebb8e9bdbdd62d3751a9245d70ff94efaef
Jekabs Karklins <jekabskarklins@ess.eu>
2025-01-13 17:06:02 +0100

    feat: add co-proposer claims table and update indices

--- apps/backend/db_patches/0168_AddCoProposerClaims.sql
+++ apps/backend/db_patches/0168_AddCoProposerClaims.sql
@@ -4,11 +4,11 @@ BEGIN
   IF register_patch('0168_CreateCoProposerInvite', 'Jekabs Karklins', 'Adding co-proposer claims', '2025-01-10') THEN
     BEGIN
       CREATE TABLE co_proposer_invites (
-        role_invite_id SERIAL PRIMARY KEY,
+        co_proposer_invite_id SERIAL PRIMARY KEY,
         invite_code_id INT NOT NULL REFERENCES invite_codes(invite_code_id) ON DELETE CASCADE,
-        role_id INT NOT NULL REFERENCES roles(role_id) ON DELETE CASCADE
+        proposal_pk INT NOT NULL REFERENCES proposals(proposal_pk) ON DELETE CASCADE
       );
-      CREATE INDEX co_proposer_invites_invite_code_id_idx ON co_proposer_invites(invite_code_id);
+      CREATE INDEX co_proposer_co_proposer_invite_id_idx ON co_proposer_invites(co_proposer_invite_id);
 
     END;
   END IF;
