ebc2f9333a57b2a3c19f5184ea9c170ee8077372
William-Edwards-STFC <william.edwards@stfc.ac.uk>
2025-01-14 09:32:57 +0000

    remove generated files

--- apps/backend/src/generated/models/IdeiioMeta.ts
+++
@@ -1,9 +0,0 @@
-/* generated using openapi-typescript-codegen -- do not edit */
-/* istanbul ignore file */
-/* tslint:disable */
-/* eslint-disable */
-export type IdeiioMeta = {
-    created?: string;
-    lastModified?: string;
-};
-
