62684cd8340930b46a64a8cde236d68d2d2610ce
Jekabs Karklins <jekabskarklins@ess.eu>
2025-01-20 11:21:47 +0100

    feat: add co_proposer_invites table and index with existence checks

--- apps/backend/db_patches/0168_AddCoProposerClaims.sql
+++ apps/backend/db_patches/0168_AddCoProposerClaims.sql
@@ -3,12 +3,12 @@ $$
 BEGIN
   IF register_patch('0168_CreateCoProposerInvite', 'Jekabs Karklins', 'Adding co-proposer claims', '2025-01-10') THEN
     BEGIN
-      CREATE TABLE co_proposer_invites (
+      CREATE TABLE IF NOT EXISTS co_proposer_invites (
         co_proposer_invite_id SERIAL PRIMARY KEY,
         invite_code_id INT NOT NULL REFERENCES invite_codes(invite_code_id) ON DELETE CASCADE,
         proposal_pk INT NOT NULL REFERENCES proposals(proposal_pk) ON DELETE CASCADE
       );
-      CREATE INDEX co_proposer_co_proposer_invite_id_idx ON co_proposer_invites(co_proposer_invite_id);
+      CREATE INDEX IF NOT EXISTS co_proposer_co_proposer_invite_id_idx ON co_proposer_invites(co_proposer_invite_id);
 
     END;
   END IF;
