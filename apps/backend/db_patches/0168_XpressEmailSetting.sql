2447e690a04200a3239f27306eaa557d519894a6
Chi Kai Lam <chi-kai.lam@stfc.ac.uk>
2025-01-28 14:25:15 +0000

    Update setting description

--- apps/backend/db_patches/0168_XpressEmailSetting.sql
+++ apps/backend/db_patches/0168_XpressEmailSetting.sql
@@ -3,7 +3,7 @@ $$
 BEGIN
   IF register_patch('0168_XpressEmailSetting.sql', 'Chi Kai Lam', 'Xpress email setting for recipients SAMPLE_SAFETY_EMAIL', '2025-01-24') THEN
     BEGIN
-      INSERT INTO settings (settings_id, settings_value, description) VALUES ('SAMPLE_SAFETY_EMAIL', '', 'Email address for the sample safety.'); 
+      INSERT INTO settings (settings_id, settings_value, description) VALUES ('SAMPLE_SAFETY_EMAIL', '', 'Email address for the sample safety team.'); 
     END;
   END IF;
 END;
