02d77154150eefd8d8ed56ba34c0853d35d30818
Jekabs Karklins <jekabskarklins@ess.eu>
2025-01-29 10:15:05 +0100

    fix: enable button only when available

--- apps/frontend/src/components/proposal/InviteUser.tsx
+++ apps/frontend/src/components/proposal/InviteUser.tsx
@@ -261,10 +261,11 @@ export default function InviteUser({
         />
 
         <Button
-          variant="outlined"
+          variant="contained"
           onClick={handleSubmit}
           sx={{ margin: '16px 0 8px 0' }}
           startIcon={<AddIcon />}
+          disabled={!selectedItems.length}
         >
           Add
         </Button>
