363b8d4c4f9a8a1f024e157ade7413101284f649
Jekabs Karklins <jekabskarklins@ess.eu>
2025-01-30 13:58:09 +0100

    feat: implement authorization checks for co-proposer invites based on user role and proposal membership

--- apps/backend/src/queries/InviteQueries.ts
+++ apps/backend/src/queries/InviteQueries.ts
@@ -1,11 +1,13 @@
 import { inject, injectable } from 'tsyringe';
 
+import { ProposalAuthorization } from '../auth/ProposalAuthorization';
 import { UserAuthorization } from '../auth/UserAuthorization';
 import { Tokens } from '../config/Tokens';
 import { CoProposerClaimDataSource } from '../datasources/CoProposerClaimDataSource';
 import { InviteDataSource } from '../datasources/InviteDataSource';
 import { Authorized } from '../decorators';
 import { Invite } from '../models/Invite';
+import { rejection } from '../models/Rejection';
 import { UserWithRole } from '../models/User';
 
 @injectable()
@@ -16,12 +18,23 @@ export default class InviteQueries {
     @inject(Tokens.InviteDataSource)
     public inviteDataSource: InviteDataSource,
     @inject(Tokens.UserAuthorization)
-    private userAuth: UserAuthorization
+    private userAuth: UserAuthorization,
+    @inject(Tokens.ProposalAuthorization)
+    private proposalAuth: ProposalAuthorization
   ) {}
 
   @Authorized()
   async getCoProposerInvites(agent: UserWithRole | null, proposalPk: number) {
-    // TODO implement authorization
+    const isUserOfficer = await this.userAuth.isUserOfficer(agent);
+    const isMemberOfProposal = await this.proposalAuth.isMemberOfProposal(
+      agent,
+      proposalPk
+    );
+
+    if (!isUserOfficer && !isMemberOfProposal) {
+      return rejection('User is not authorized to view these invites');
+    }
+
     const coProposerClaims =
       await this.coProposerClaimDataSource.getByProposalPk(proposalPk);
     const invites = await Promise.all(
