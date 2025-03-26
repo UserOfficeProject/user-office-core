837e3a22a3db9f9a97b471c22460d44010fc34e8
Jekabs Karklins <jekabskarklins@ess.eu>
2025-03-26 10:42:45 +0100

    fix: correct TODAY_MIDNIGHT initialization in visit validation schema

--- apps/frontend/src/components/questionary/questionaryComponents/VisitBasis/createVisitBasisValidationSchema.ts
+++ apps/frontend/src/components/questionary/questionaryComponents/VisitBasis/createVisitBasisValidationSchema.ts
@@ -3,7 +3,7 @@ import * as Yup from 'yup';
 import { CreateYupValidation } from 'components/questionary/QuestionaryComponentRegistry';
 
 export const createVisitBasisValidationSchema: CreateYupValidation = () => {
-  const TODAY_MIDNIGT = new Date().setHours(0, 0, 0, 0);
+  const TODAY_MIDNIGT = new Date(new Date().setHours(0, 0, 0, 0));
   const schema = Yup.object().shape({
     proposalPk: Yup.number().min(1, 'Proposal is required'),
 
