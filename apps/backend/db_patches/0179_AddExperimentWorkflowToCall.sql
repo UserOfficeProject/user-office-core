a2d590e2e61668ee2b072a17994e95c4b3c3d841
Yoganandan Pandiyan <yoganandan.pandiyan@ess.eu>
2025-04-22 01:23:47 +0200

    Experiment Safety Workflow Integration done

---
+++ apps/backend/db_patches/0179_AddExperimentWorkflowToCall.sql
@@ -0,0 +1,13 @@
+DO
+$$
+BEGIN
+	-- Change AddExperimentWorkflowToCall to 0179_AddExperimentWorkflowToCall
+	IF register_patch('AddExperimentWorkflowToCall.sql', 'Yoganandan Pandiyan', 'Add experiment_workflow_id to call table', '2025-04-21') THEN
+	  BEGIN
+      ALTER table call
+	      ADD COLUMN experiment_workflow_id INTEGER REFERENCES workflows(workflow_id) DEFAULT null;
+    END;
+	END IF;
+END;
+$$
+LANGUAGE plpgsql;
