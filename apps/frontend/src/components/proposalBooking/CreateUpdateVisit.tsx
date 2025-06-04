85755b8c068abe8b5b744e9122eb90d7870a2f94
Jekabs Karklins <jekabskarklins@ess.eu>
2025-06-04 15:53:24 +0200

    fix: show chips

--- apps/frontend/src/components/proposalBooking/CreateUpdateVisit.tsx
+++ apps/frontend/src/components/proposalBooking/CreateUpdateVisit.tsx
@@ -1,13 +1,13 @@
 import { Button, Typography } from '@mui/material';
 import { Form, Formik } from 'formik';
-import React from 'react';
+import React, { useState } from 'react';
 import * as Yup from 'yup';
 
 import { ActionButtonContainer } from 'components/common/ActionButtonContainer';
 import ErrorMessage from 'components/common/ErrorMessage';
 import FormikUIAutocomplete from 'components/common/FormikUIAutocomplete';
 import Participants from 'components/proposal/Participants';
-import { BasicUserDetails } from 'generated/sdk';
+import { BasicUserDetails, Invite } from 'generated/sdk';
 import { UserExperiment } from 'hooks/experiment/useUserExperiments';
 import useDataApiWithFeedback from 'utils/useDataApiWithFeedback';
 import { getFullUserName } from 'utils/user';
@@ -18,6 +18,7 @@ interface CreateUpdateVisitProps {
 }
 function CreateUpdateVisit({ event, close }: CreateUpdateVisitProps) {
   const { api } = useDataApiWithFeedback();
+  const [visitInvites, setVisitInvites] = useState<Invite[]>([]);
 
   const visit = event.visit;
 
@@ -81,8 +82,8 @@ function CreateUpdateVisit({ event, close }: CreateUpdateVisitProps) {
           </Typography>
           <Participants
             title="Visitors"
-            setInvites={() => {}}
-            invites={[]}
+            setInvites={setVisitInvites}
+            invites={visitInvites}
             setUsers={(team: BasicUserDetails[]) => {
               setFieldValue('team', team);
             }}
