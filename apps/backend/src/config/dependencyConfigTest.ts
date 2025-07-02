4b30486fc0234eb26ef6b16ee33f9efcb3b81910
Jekabs Karklins <jekabskarklins@ess.eu>
2025-07-02 09:03:16 +0200

    feat: add tests for co-proposer invite email and logging functionality in InviteMutations

--- apps/backend/src/config/dependencyConfigTest.ts
+++ apps/backend/src/config/dependencyConfigTest.ts
@@ -1,5 +1,5 @@
 /* eslint-disable @typescript-eslint/no-empty-function */
-import { ConsoleLogger, setLogger } from '@user-office-software/duo-logger';
+import { setLogger } from '@user-office-software/duo-logger';
 
 import 'reflect-metadata';
 import { UserAuthorizationMock } from '../auth/mockups/UserAuthorization';
@@ -37,12 +37,13 @@ import { UserDataSourceMock } from '../datasources/mockups/UserDataSource';
 import { WorkflowDataSourceMock } from '../datasources/mockups/WorkflowDataSource';
 import PostgresPredefinedMessageDataSource from '../datasources/postgres/PredefinedMessageDataSource';
 import { essEmailHandler } from '../eventHandlers/email/essEmailHandler';
+import createLoggingHandler from '../eventHandlers/logging';
 import { SkipSendMailService } from '../eventHandlers/MailService/SkipSendMailService';
 import {
   createSkipListeningHandler,
   createSkipPostingHandler,
 } from '../eventHandlers/messageBroker';
-import { EventBus } from '../events/eventBus';
+import { createApplicationEventBus } from '../events';
 import { DefaultDownloadService } from '../factory/DefaultDownloadService';
 import BasicUserDetailsLoader from '../loaders/BasicUserDetailsLoader';
 import { SkipAssetRegistrar } from '../services/assetRegistrar/skip/SkipAssetRegistrar';
@@ -96,8 +97,7 @@ mapClass(Tokens.ProposalAuthorization, ProposalAuthorization);
 mapClass(Tokens.AssetRegistrar, SkipAssetRegistrar);
 
 mapValue(Tokens.PostToMessageQueue, createSkipPostingHandler());
-mapValue(Tokens.LoggingHandler, jest.mocked(new EventBus()));
-mapValue(Tokens.EventBus, jest.mocked(new EventBus()));
+mapValue(Tokens.LoggingHandler, createLoggingHandler());
 mapValue(Tokens.ListenToMessageQueue, createSkipListeningHandler());
 
 mapClass(Tokens.MailService, SkipSendMailService);
@@ -105,14 +105,11 @@ mapClass(Tokens.MailService, SkipSendMailService);
 mapValue(Tokens.EmailEventHandler, essEmailHandler);
 
 mapValue(Tokens.ConfigureEnvironment, () => {});
-mapValue(Tokens.ConfigureLogger, () =>
-  setLogger(new ConsoleLogger({ colorize: true }))
-);
+mapValue(Tokens.ConfigureLogger, () => {
+  setLogger([]);
+});
 
 mapClass(Tokens.DownloadService, DefaultDownloadService);
 
 mapClass(Tokens.BasicUserDetailsLoader, BasicUserDetailsLoader);
-
-jest.mock('../decorators/EventBus', () => {
-  return () => jest.fn();
-});
+mapValue(Tokens.EventBus, createApplicationEventBus());
