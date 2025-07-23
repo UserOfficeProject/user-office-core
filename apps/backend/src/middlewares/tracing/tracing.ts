8ca4292b45d0b5f334ca0684bd9130aa6fd24388
deepak jaison <deepak.jaison@stfc.ac.uk>
2025-07-23 12:09:17 +0100

    Block senstive data from trace

--- apps/backend/src/middlewares/tracing/tracing.ts
+++ apps/backend/src/middlewares/tracing/tracing.ts
@@ -9,7 +9,11 @@ import { containerDetector } from '@opentelemetry/resource-detector-container';
 import { resourceFromAttributes } from '@opentelemetry/resources';
 import { envDetector, processDetector } from '@opentelemetry/resources';
 import { NodeSDK } from '@opentelemetry/sdk-node';
-import { BatchSpanProcessor } from '@opentelemetry/sdk-trace-node';
+import {
+  BatchSpanProcessor,
+  ReadableSpan,
+  SpanProcessor,
+} from '@opentelemetry/sdk-trace-node';
 
 const collectorOptions = {
   url: process.env.OTEL_EXPORTER_OTLP_ENDPOINT,
@@ -17,12 +21,59 @@ const collectorOptions = {
 
 const collectorTraceExporter = new OTLPTraceExporter(collectorOptions);
 
+class AttributeFilterProcessor implements SpanProcessor {
+  private blocklist: string[];
+  private masklist: string[];
+
+  constructor(blocklist: string[], masklist: string[]) {
+    this.blocklist = blocklist;
+    this.masklist = masklist;
+  }
+
+  onStart(): void {}
+
+  onEnd(span: ReadableSpan): void {
+    // Remove blocked attributes
+    for (const key of this.blocklist) {
+      delete span.attributes[key];
+    }
+
+    // Mask specified attributes
+    for (const key of this.masklist) {
+      const value = span.attributes[key];
+      if (typeof value === 'string') {
+        span.attributes[key] = this.maskToken(value);
+      }
+    }
+  }
+
+  private maskToken(token: string): string {
+    const visibleChars = 6;
+    if (token.length <= visibleChars) return '*'.repeat(token.length);
+    const maskedPart = '*'.repeat(token.length - visibleChars);
+
+    return maskedPart + token.slice(-visibleChars);
+  }
+
+  shutdown(): Promise<void> {
+    return Promise.resolve();
+  }
+
+  forceFlush(): Promise<void> {
+    return Promise.resolve();
+  }
+}
+
+const attributeBlocklist = ['graphql.variables.externalToken'];
+const attributeMasklist = ['graphql.variables.token'];
+
 const otelSDK = new NodeSDK({
   traceExporter: collectorTraceExporter,
   resource: resourceFromAttributes({
     ['service.name']: process.env.OTEL_SERVICE_NAME || 'proposal-backend',
   }),
   spanProcessors: [
+    new AttributeFilterProcessor(attributeBlocklist, attributeMasklist),
     new BatchSpanProcessor(collectorTraceExporter, {
       maxQueueSize: 1024,
       maxExportBatchSize: 1024,
