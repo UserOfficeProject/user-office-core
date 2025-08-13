94746913a8e6b381c247a71a99c12f6646cb8d5a
Farai Mutambara <farai.mutambara@stfc.ac.uk>
2025-08-13 15:36:58 +0100

    drop if check

--- apps/backend/src/factory/pdf/proposal.ts
+++ apps/backend/src/factory/pdf/proposal.ts
@@ -79,18 +79,13 @@ const getTechnicalReviewHumanReadableStatus = (
 };
 
 const getSampleQuestionarySteps = async (
-  questionaryId: number,
-  user: UserWithRole | null
+  questionaryId: number
 ): Promise<QuestionaryStep[]> => {
   const questionaryDataSource = container.resolve<QuestionaryDataSource>(
     Tokens.QuestionaryDataSource
   );
-  const questionarySteps = user
-    ? await baseContext.queries.questionary.getQuestionarySteps(
-        user,
-        questionaryId
-      )
-    : await questionaryDataSource.getQuestionarySteps(questionaryId);
+  const questionarySteps =
+    await questionaryDataSource.getQuestionarySteps(questionaryId);
   if (!questionarySteps) {
     throw new Error(
       `Questionary steps for Questionary ID '${questionaryId}' not found, or the user has insufficient rights`
@@ -581,7 +576,7 @@ export const collectProposalPDFDataTokenAccess = async (
           undefined,
           sample,
           await getQuestionary(sample.questionaryId),
-          await getSampleQuestionarySteps(sample.questionaryId, null)
+          await getSampleQuestionarySteps(sample.questionaryId)
         )
       )
     )
@@ -616,7 +611,7 @@ export const collectProposalPDFDataTokenAccess = async (
           undefined,
           genericTemplate,
           await getQuestionary(genericTemplate.questionaryId),
-          await getSampleQuestionarySteps(genericTemplate.questionaryId, null)
+          await getSampleQuestionarySteps(genericTemplate.questionaryId)
         )
       )
     )
