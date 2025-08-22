266beb66cea79d37a69dbb706d58c089ff16592c
Simon Fernandes <simon.fernandes@stfc.ac.uk>
2025-08-22 15:17:25 +0100

    feat: download pregenerated PDFs (#1134)
    Co-authored-by: Yoganandan Pandiyan <132274772+yoganandaness@users.noreply.github.com>

--- apps/backend/src/config/dependencyConfigESS.ts
+++ apps/backend/src/config/dependencyConfigESS.ts
@@ -46,7 +46,6 @@ import {
   createPostToRabbitMQHandler,
 } from '../eventHandlers/messageBroker';
 import { createApplicationEventBus } from '../events';
-import { DefaultDownloadService } from '../factory/DefaultDownloadService';
 import { FapDataColumns } from '../factory/xlsx/FapDataColumns';
 import {
   callFapPopulateRow,
@@ -134,7 +133,5 @@ mapValue(
   isDevelopment ? configureESSDevelopmentEnvironment : () => {}
 );
 mapValue(Tokens.ConfigureLogger, configureGraylogLogger);
-
-mapClass(Tokens.DownloadService, DefaultDownloadService);
 
 mapClass(Tokens.BasicUserDetailsLoader, BasicUserDetailsLoader);
