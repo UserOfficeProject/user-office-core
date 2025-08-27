532117ac10dee66782ba4dc0a507d7c130d308f1
Ellen Wright <144028998+ellen-wright@users.noreply.github.com>
2025-08-27 13:32:17 +0100

    feat(frontend): add call ordering functionality (#1131)
    Co-authored-by: Farai Mutambara <71100224+mutambaraf@users.noreply.github.com>

--- apps/frontend/src/components/call/CallPage.tsx
+++ apps/frontend/src/components/call/CallPage.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 
+import SimpleTabs from 'components/common/SimpleTabs';
 import { StyledContainer, StyledPaper } from 'styles/StyledComponents';
 
 import CallsTable from './CallsTable';
@@ -8,7 +9,10 @@ const CallPage = () => {
   return (
     <StyledContainer maxWidth={false}>
       <StyledPaper>
-        <CallsTable />
+        <SimpleTabs tabNames={['Current', 'Archived']}>
+          <CallsTable isArchivedTab={true} />
+          <CallsTable isArchivedTab={false} />
+        </SimpleTabs>
       </StyledPaper>
     </StyledContainer>
   );
