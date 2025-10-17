ace92388f6ecba22ca39add4d5553ead5348c364
Thomas Cottee Meldrum <thomas.cottee-meldrum@stfc.ac.uk>
2025-10-17 15:52:57 +0100

    settings test fix

--- apps/e2e/cypress/e2e/settings.cy.ts
+++ apps/e2e/cypress/e2e/settings.cy.ts
@@ -1172,7 +1172,10 @@ context('Settings tests', () => {
       cy.setTinyMceContent('comment', faker.lorem.words(3));
       cy.get(`#comment_ifr`).first().focus().click();
 
-      cy.get('[data-cy="grade-proposal"]').click().type('1');
+      cy.get('[data-cy="grade-proposal"]').click();
+      cy.get('[data-cy="grade-proposal-options"] [role="option"]')
+        .first()
+        .click();
 
       cy.get('[data-cy="save-and-continue-button"]').focus().click();
       cy.get('[data-cy="is-grade-submitted"]').click();
