d1f92a65a7cbc2f4fd896363aa04fbf689c46513
Fredrik Bolmsten <fredrik.bolmsten@ess.eu>
2025-11-14 15:49:47 +0100

    Fix E2E tests for removal of old people search

--- apps/e2e/cypress/e2e/peopleTable.cy.ts
+++ apps/e2e/cypress/e2e/peopleTable.cy.ts
@@ -31,35 +31,23 @@ context('PageTable component tests', () => {
 
       cy.get('[role="presentation"] [role="dialog"]').as('modal');
 
-      cy.get('@modal').contains('No Previous Collaborators');
-
-      cy.finishedLoading();
-
-      cy.get('[data-cy=email]').type(initialDBData.users.user2.email);
-
-      cy.get('[data-cy="findUser"]').click();
-      cy.finishedLoading();
-
-      cy.get('@modal')
-        .find('tr[index="0"]')
-        .contains(initialDBData.users.user2.firstName);
-
-      cy.get('@modal').contains('1 user(s) selected');
-
-      cy.get('[data-cy="email"]').type(initialDBData.users.officer.email);
-
-      cy.get('[data-cy="findUser"]').click();
-      cy.finishedLoading();
-
-      cy.get('[role="presentation"] .MuiAlert-message').contains(
-        'We cannot find that email'
+      cy.get('[data-cy="invite-user-autocomplete"]').type(
+        initialDBData.users.user2.email
       );
 
-      cy.get('@modal').contains('1 User(s) Selected');
+      cy.get('[role=presentation]')
+        .contains(initialDBData.users.user2.lastName)
+        .click();
 
-      cy.get('[data-cy="assign-selected-users"]').click();
+      cy.get('[role=presentation]').contains(
+        initialDBData.users.user2.lastName
+      );
 
-      cy.get('@modal').should('not.exist');
+      cy.get('[data-cy="invite-user-autocomplete"]').type(emails[0]);
+
+      cy.contains(`Invite ${emails[0]} via email`).should('exist');
+
+      cy.get('[data-cy="invite-user-submit-button"]').click();
 
       cy.get('[data-cy=title] input').type(title).should('have.value', title);
 
@@ -85,305 +73,11 @@ context('PageTable component tests', () => {
 
       cy.get('[data-cy=add-participant-button]').click();
 
-      cy.get('[role="presentation"]')
-        .find('tr[index="0"]')
-        .contains(initialDBData.users.user2.firstName);
-    });
-
-    it('Should preserve the selected users', () => {
-      cy.login('user1');
-      cy.visit('/');
-
-      cy.contains('New Proposal').click();
-      cy.get('[data-cy=call-list]').find('li:first-child').click();
-
-      cy.get('[data-cy=add-participant-button]').click();
-
-      cy.get('[role="presentation"] [role="dialog"]').as('modal');
-
-      cy.get('@modal').contains('0 user(s) selected');
-
-      cy.get('[data-cy=email]').type(initialDBData.users.placeholderUser.email);
-
-      cy.get('[data-cy="findUser"]').click();
-      cy.finishedLoading();
-
-      cy.get('@modal').find('tr[index="0"] input').uncheck();
-
-      cy.get('@modal').contains('0 user(s) selected');
-
-      cy.get('[data-cy=email]').type('ben@inbox.com');
-
-      cy.get('[data-cy="findUser"]').click();
-      cy.finishedLoading();
-
-      cy.get('@modal')
-        .find('tr[index="0"]')
-        .contains(initialDBData.users.user2.firstName);
-
-      cy.get('@modal').contains('1 user(s) selected');
-
-      cy.get('@modal').find('[aria-label="Search"]').type('foo bar');
-
-      cy.finishedLoading();
-
-      cy.get('@modal').contains('No Previous Collaborators');
-      cy.get('@modal').contains('1 user(s) selected');
-
-      cy.get('@modal').find('[aria-label="Search"] ~ * > button').click();
-
-      cy.get('@modal').contains('1 user(s) selected');
-
-      cy.get('@modal')
-        .contains(initialDBData.users.user2.firstName)
-        .parent()
-        .find('input')
-        .should('be.checked');
-
-      cy.finishedLoading();
-
-      cy.get('@modal')
-        .find('[aria-label="Search"]')
-        .type(initialDBData.users.placeholderUser.firstName);
-
-      cy.finishedLoading();
-
-      cy.get('@modal').contains('1 user(s) selected');
-      cy.get('@modal')
-        .contains(initialDBData.users.placeholderUser.firstName)
-        .parent()
-        .find('input[type="checkbox"]')
-        .should('not.be.checked');
-
-      cy.get('@modal')
-        .find('[aria-label="Search"]')
-        .clear()
-        .type(initialDBData.users.user2.firstName);
-
-      cy.finishedLoading();
-
-      cy.get('@modal').contains('1 user(s) selected');
-      cy.get('@modal')
-        .contains(initialDBData.users.user2.firstName)
-        .parent()
-        .find('input')
-        .should('be.checked');
-
-      cy.get('[data-cy="assign-selected-users"]').click();
-    });
-
-    it('should preserve the selected users after pagination', () => {
-      cy.login('user1');
-      cy.visit('/');
-
-      cy.finishedLoading();
-      cy.contains('New Proposal').click();
-      cy.get('[data-cy=call-list]').find('li:first-child').click();
-
-      cy.get('[data-cy=add-participant-button]').click();
-
-      cy.get('[role="presentation"] [role="dialog"]').as('modal');
-
-      cy.finishedLoading();
-
-      cy.get('@modal').contains('10 rows').click();
-      cy.get('[data-value=5]').click();
-
-      let index = 0;
-      for (const email of emails) {
-        cy.finishedLoading();
-        cy.get('[data-cy=email]').clear().type(email);
-
-        cy.get('[data-cy="findUser"]').click();
-        cy.finishedLoading();
-
-        if (index >= 5) {
-          cy.get('[role="presentation"] .MuiAlert-message').contains(
-            'We cannot find that email'
-          );
-
-          cy.get('[data-cy=email]').clear();
-        }
-
-        cy.get('@modal').find('tr[index="0"] input').uncheck();
-        index++;
-      }
-
-      cy.finishedLoading();
-      cy.get('[data-cy=email]').clear().type('ben@inbox.com');
-
-      cy.get('[data-cy="findUser"]').click();
-      cy.finishedLoading();
-
-      cy.get('@modal').contains('5 user(s) selected');
-      cy.get('@modal').contains(/1-5 of [0-9]+/);
-
-      cy.get('@modal').find('tr[index="1"] input').uncheck();
-
-      cy.get('@modal').contains('4 user(s) selected');
-
-      cy.finishedLoading();
-      cy.get('@modal').find('button[aria-label="Next Page"]').click();
-
-      cy.finishedLoading();
-
-      cy.get('@modal').find('tr[index="0"] input').check();
-      cy.get('@modal').contains('5 user(s) selected');
-
-      cy.get('@modal').find('button[aria-label="Previous Page"]').click();
-
-      cy.finishedLoading();
-
-      cy.get('@modal').find('tr[index="0"] input:checked');
-      cy.get('@modal').contains('5 user(s) selected');
-
-      cy.get('@modal').find('thead th input').check();
-      cy.get('@modal').contains('6 user(s) selected');
-      cy.get('@modal').find('thead th input').uncheck();
-      cy.get('@modal').contains('1 user(s) selected');
-
-      cy.get('@modal').find('button[aria-label="Next Page"]').click();
-      cy.finishedLoading();
-      cy.get('@modal').find('tr[index="0"] input:checked');
-      cy.get('@modal').contains('1 user(s) selected');
-      cy.get('[data-cy="assign-selected-users"]').click();
-    });
-  });
-
-  describe('PeopleTable component preserves selected users', () => {
-    beforeEach(() => {
-      cy.createProposal({ callId: initialDBData.call.id });
-    });
-
-    it('Should preserve the selected users', () => {
-      cy.login('officer');
-      cy.visit('/');
-
-      cy.get('[data-cy=view-proposal]').click();
-
-      cy.get('[data-cy=toggle-edit-proposal]').click();
-
-      cy.get('[data-cy=questionary-stepper]').contains('New proposal').click();
-
-      cy.get('[data-cy=add-participant-button]').click();
-
-      cy.get('[role="presentation"] [role="dialog"]').as('modal');
-
-      cy.get('@modal').contains('0 user(s) selected');
-
-      cy.finishedLoading();
-
-      cy.get('@modal')
-        .contains(initialDBData.users.user2.firstName)
-        .parent()
-        .find('input')
-        .check();
-
-      cy.get('@modal').contains('1 user(s) selected');
-
-      cy.finishedLoading();
-
-      cy.get('@modal').find('[aria-label="Search"]').type('foo bar');
-
-      cy.finishedLoading();
-
-      cy.get('@modal').contains('No Users');
-
-      cy.get('@modal').contains('1 user(s) selected');
-
-      cy.get('@modal').find('[aria-label="Search"] ~ * > button').click();
-
-      cy.get('@modal').contains('1 user(s) selected');
-
-      cy.get('@modal')
-        .contains(initialDBData.users.user2.firstName)
-        .parent()
-        .find('input')
-        .should('be.checked');
-
-      cy.finishedLoading();
-
-      cy.get('@modal')
-        .find('[aria-label="Search"]')
-        .type(initialDBData.users.placeholderUser.firstName);
-
-      cy.finishedLoading();
-
-      cy.get('@modal').contains('1 user(s) selected');
-      cy.get('@modal')
-        .contains(initialDBData.users.placeholderUser.firstName)
-        .parent()
-        .find('input')
-        .should('not.be.checked');
-
-      cy.get('@modal')
-        .find('[aria-label="Search"]')
-        .clear()
-        .type(initialDBData.users.user2.firstName);
-
-      cy.finishedLoading();
-
-      cy.get('@modal').contains('1 user(s) selected');
-      cy.get('@modal')
-        .contains(initialDBData.users.user2.firstName)
-        .parent()
-        .find('input')
-        .should('be.checked');
-
-      cy.get('[data-cy="assign-selected-users"]').click();
-    });
-
-    it('Should preserve the selected users after pagination', () => {
-      cy.login('officer');
-      cy.visit('/');
-
-      cy.get('[data-cy=view-proposal]').click();
-
-      cy.get('[data-cy=toggle-edit-proposal]').click();
-
-      cy.get('[data-cy=questionary-stepper]').contains('New proposal').click();
-
-      cy.get('[data-cy=add-participant-button]').click();
-
-      cy.get('[role="presentation"] [role="dialog"]').as('modal');
-
-      cy.finishedLoading();
-
-      cy.get('@modal').contains('10 rows').click();
-      cy.get('[data-value=5]').click();
-
-      cy.get('@modal').contains('0 user(s) selected');
-      cy.get('@modal').contains(/1-5 of [0-9]+/);
-
-      cy.get('@modal').find('tr[index="1"] input').check();
-
-      cy.get('@modal').contains('1 user(s) selected');
-
-      cy.get('@modal').find('button[aria-label="Next Page"]').click();
-
-      cy.finishedLoading();
-
-      cy.get('@modal').find('tr[index="0"] input').check();
-      cy.get('@modal').contains('2 user(s) selected');
-
-      cy.get('@modal').find('button[aria-label="Previous Page"]').click();
-
-      cy.finishedLoading();
-
-      cy.get('@modal').find('tr[index="1"] input:checked');
-      cy.get('@modal').contains('2 user(s) selected');
-
-      cy.get('@modal').find('thead th input').check();
-      cy.get('@modal').contains('6 user(s) selected');
-      cy.get('@modal').find('thead th input').uncheck();
-      cy.get('@modal').contains('1 user(s) selected');
-
-      cy.get('@modal').find('button[aria-label="Next Page"]').click();
-
-      cy.finishedLoading();
-
-      cy.get('@modal').find('tr[index="0"] input:checked');
-      cy.get('@modal').contains('1 user(s) selected');
+      cy.get('[data-cy="invite-user-autocomplete"]').click();
+
+      cy.get('[role=presentation]').contains(
+        initialDBData.users.user2.lastName
+      );
     });
   });
 
@@ -461,15 +155,17 @@ context('PageTable component tests', () => {
 
       cy.get('[data-cy=add-participant-button]').click();
 
-      cy.get('[role="presentation"] [role="dialog"]');
+      cy.get('[data-cy="invite-user-autocomplete"]').type(
+        initialDBData.users.user2.email
+      );
 
-      cy.get('[data-cy=email]').type(initialDBData.users.user2.email);
+      cy.get('[role=presentation]')
+        .contains(initialDBData.users.user2.lastName)
+        .click();
 
-      cy.get('[data-cy="findUser"]').click();
-
-      cy.finishedLoading();
-
-      cy.get('[data-cy="assign-selected-users"]').click();
+      cy.get('[data-cy="invite-user-submit-button"]')
+        .should('be.enabled')
+        .click();
 
       cy.finishedLoading();
 
