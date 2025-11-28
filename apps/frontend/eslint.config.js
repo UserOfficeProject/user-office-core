d3b221c250511105b0e82257b400258221a95c77
Thomas Cottee Meldrum <thomas.cottee-meldrum@stfc.ac.uk>
2025-11-28 12:04:36 +0000

    chore: update eslint frontend v9

---
+++ apps/frontend/eslint.config.js
@@ -0,0 +1,132 @@
+import eslint from '@eslint/js';
+import tsParser from '@typescript-eslint/parser';
+import { defineConfig } from 'eslint/config';
+import importPlugin from "eslint-plugin-import";
+import prettier from "eslint-plugin-prettier";
+import react from "eslint-plugin-react";
+import reactHooks from "eslint-plugin-react-hooks";
+import unusedImports from "eslint-plugin-unused-imports";
+import tseslint from "typescript-eslint";
+
+export default defineConfig([
+  {
+    ignores: [
+     "node_modules",
+     ".next",
+     "build",
+     "dist",
+     "public",
+     "src/serviceWorker.js",
+     "src/setupProxy.js",
+     "src/generated/sdk.ts",
+     "server.js",
+    ],
+  },
+  // Base configs
+  // js.configs.recommended,
+  eslint.configs.recommended,
+  tseslint.configs.recommended,
+  react.configs.flat.recommended,
+  reactHooks.configs.flat.recommended,
+  importPlugin.flatConfigs.recommended,
+  importPlugin.flatConfigs.typescript,
+
+  // Custom project configuration
+  {
+    files: ["**/*.{ts,tsx,js,jsx}"],
+    languageOptions: {
+      parser: tsParser,
+      ecmaVersion: 2018,
+      sourceType: "module",
+      globals: {
+        browser: true,
+        commonjs: true,
+        node: true,
+        es6: true,
+      },
+    },
+    settings: {
+      react: {
+        version: "detect",
+      },
+      "import/resolver": {
+        typescript: {
+          project: "./tsconfig.json", 
+        },
+      }
+    },
+    plugins: {
+      "unused-imports": unusedImports,
+      prettier,
+    },
+    rules: {
+      // Import order
+      "import/order": [
+        "error",
+        {
+          groups: [
+            "builtin",
+            "external",
+            "internal",
+            ["parent", "sibling"],
+            "index",
+          ],
+          pathGroups: [
+            {
+              pattern:
+                "+(components|context|generated|graphql|hooks|images|models|styles|utils)/**",
+              group: "internal",
+              position: "after",
+            },
+          ],
+          pathGroupsExcludedImportTypes: ["builtin"],
+          "newlines-between": "always",
+          alphabetize: { order: "asc", caseInsensitive: true },
+        },
+      ],
+      "react-hooks/rules-of-hooks": "error",
+      "react-hooks/exhaustive-deps": "warn",
+      "@typescript-eslint/no-use-before-define": ["error"],
+      "@typescript-eslint/no-empty-function": [
+        "error",
+        { allow: ["arrowFunctions"] },
+      ],
+      "@typescript-eslint/naming-convention": [
+        "error",
+        {
+          selector: "variable",
+          format: ["camelCase", "PascalCase", "UPPER_CASE"],
+          leadingUnderscore: "allow",
+          trailingUnderscore: "allow",
+        },
+        {
+          selector: "function",
+          format: ["PascalCase", "camelCase"],
+        },
+      ],
+      "@typescript-eslint/ban-ts-comment": "warn",
+      "@typescript-eslint/explicit-module-boundary-types": "off",
+      "@typescript-eslint/no-non-null-assertion": "off",
+      "react/display-name": "warn",
+      "react/prop-types": "off",
+      "padding-line-between-statements": [
+        "error",
+        { blankLine: "always", prev: "*", next: "return" },
+      ],
+      "unused-imports/no-unused-imports": "error",
+      '@typescript-eslint/no-unused-expressions': [
+        'error',
+        { allowShortCircuit: true, allowTernary: true },
+      ],
+      //TS already handles undef variables
+      'no-undef': 'off',
+      'no-unused-vars': 'off',
+      'no-unused-expressions': 'off',
+
+      // Maybe we can enable these later
+      'no-extra-boolean-cast': 'off',
+      'no-case-declarations': 'off',
+      'react-hooks/set-state-in-effect': 'off',
+    },
+  }]
+);
