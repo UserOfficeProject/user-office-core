ab6bb338288c073ec333c0995b64081d194d01ea
Gergely Nyiri <gergely.nyiri@eli-alps.hu>
2026-01-12 08:43:57 +0100

    fix: remove migration part of DB script

--- apps/backend/db_patches/0202_CreateEmailTemplatesTable.sql
+++ apps/backend/db_patches/0202_CreateEmailTemplatesTable.sql
@@ -1,14 +1,7 @@
 DO
 $$
-DECLARE 
-      email_template_id_var int;
-      connection_loop_var record;
-      array_size_var int;
-      array_loop_var int;
-      template_name varchar(200);
 BEGIN
 	IF register_patch('CreateEmailTemplatesTable.sql', 'Gergely Nyiri', 'Create email templates table', '2025-07-15') THEN
-
             CREATE TABLE IF NOT EXISTS email_templates (
                   email_template_id serial PRIMARY KEY,
                   created_by INT NOT NULL REFERENCES users(user_id),
@@ -18,82 +11,6 @@ BEGIN
                   subject varchar(160) NOT NULL, 
                   body text
             ); 
-
-            INSERT INTO email_templates (created_by, name, description, subject, body)
-            VALUES
-                  (0, 'CLF PI Co-I Submission Email', 'CLF PI Co-I Submission Email', '= `Proposal submitted`', 'Proposal submitted');
-
-            INSERT INTO email_templates (created_by, name, description, subject, body)
-            VALUES
-                  (0, 'ISIS PI Co-I Submission Email', 'ISIS PI Co-I Submission Email', '= `Proposal submitted`', 'Proposal submitted');
-
-            INSERT INTO email_templates (created_by, name, description, subject, body)
-            VALUES
-                  (0, 'ISIS Rapid PI Co-I Submission Email', 'ISIS Rapid PI Co-I Submission Email', '= `Proposal submitted`', 'Proposal submitted');
-
-            INSERT INTO email_templates (created_by, name, description, subject, body)
-            VALUES
-                  (0, 'ISIS Rapid User Office Submission Email', 'ISIS Rapid User Office Submission Email', '= `Proposal submitted`', 'Proposal submitted');
-
-            INSERT INTO email_templates (created_by, name, description, subject, body)
-            VALUES
-                  (0, 'ISIS Xpress PI Co-I Submission Email', 'ISIS Xpress PI Co-I Submission Email', '= `Proposal submitted`', 'Proposal submitted');
-
-            INSERT INTO email_templates (created_by, name, description, subject, body)
-            VALUES
-                  (0, 'ISIS Xpress Scientist Submission Email', 'ISIS Xpress Scientist Submission Email', '= `Proposal submitted`', 'Proposal submitted');
-
-            INSERT INTO email_templates (created_by, name, description, subject, body)
-            VALUES
-                  (0, 'ISIS Xpress PI Co-I Under Review Email', 'ISIS Xpress PI Co-I Under Review Email', '= `Proposal under review`', 'Proposal under review');
-
-            INSERT INTO email_templates (created_by, name, description, subject, body)
-            VALUES
-                  (0, 'ISIS Xpress PI Co-I Approval Email', 'ISIS Xpress PI Co-I Approval Email', '= `Proposal approved`', 'Proposal approved');
-
-            INSERT INTO email_templates (created_by, name, description, subject, body)
-            VALUES
-                  (0, 'ISIS Xpress SRA Request Email', 'ISIS Xpress SRA Request Email', '= `Proposal SRA request`', 'Proposal SRA request');
-
-            INSERT INTO email_templates (created_by, name, description, subject, body)
-            VALUES
-                  (0, 'ISIS Xpress PI Co-I Reject Email', 'ISIS Xpress PI Co-I Reject Email', '= `Proposal unsuccessful`', 'Proposal unsuccessful');
-
-            INSERT INTO email_templates (created_by, name, description, subject, body)
-            VALUES
-                  (0, 'ISIS Xpress PI Co-I Finish Email', 'ISIS Xpress PI Co-I Finish Email', '= `Proposal finished`', 'Proposal finished');
-
-            FOR connection_loop_var IN
-                  SELECT * FROM workflow_connection_has_actions
-            LOOP
-                  IF connection_loop_var.config IS NOT NULL THEN
-                        SELECT jsonb_array_length(connection_loop_var.config->'recipientsWithEmailTemplate') INTO array_size_var;
-
-                        IF array_size_var IS NULL THEN
-                              array_size_var := 0;
-                        END IF;
-
-                        FOR array_loop_var IN 0..array_size_var-1 LOOP
-                              SELECT connection_loop_var.config->'recipientsWithEmailTemplate'->array_loop_var->'emailTemplate'->>'name' INTO template_name;
-
-                              IF template_name IS NOT NULL THEN
-                                    -- RAISE NOTICE 'Processing connection % % with template % to %', array_loop_var, connection_loop_var.connection_id, template_name,  update_config;
-
-                                    SELECT email_templates.email_template_id INTO email_template_id_var FROM email_templates WHERE name = template_name;
-                                    
-                                    IF email_template_id_var IS NOT NULL THEN
-                                          UPDATE workflow_connection_has_actions 
-                                          SET config = jsonb_set(config::jsonb, ('{recipientsWithEmailTemplate, ' || array_loop_var || ', emailTemplate, id}')::text[], to_jsonb(email_template_id_var), false)
-                                          WHERE connection_id = connection_loop_var.connection_id;
-                                    -- ELSE
-                                    --       RAISE WARNING 'No email template found for name %', template_name;
-                                    END IF;
-                              -- ELSE
-                              --       RAISE WARNING 'No email template found for connection %', template_name;
-                              END IF;
-                        END LOOP;
-                  END IF;
-            END LOOP;
       END IF;
 END;
 $$
