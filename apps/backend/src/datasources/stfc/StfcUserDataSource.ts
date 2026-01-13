cd6f958dd22bd3bc4cbc2f9008074f5b314c7e35
Yoganandan Pandiyan <yoganandan.pandiyan@ess.eu>
2026-01-13 12:38:50 +0100

    chore: cleanup of update user by oidcsub

--- apps/backend/src/datasources/stfc/StfcUserDataSource.ts
+++ apps/backend/src/datasources/stfc/StfcUserDataSource.ts
@@ -9,10 +9,7 @@ import { Role, Roles } from '../../models/Role';
 import { BasicUserDetails, User, UserRole } from '../../models/User';
 import { AddUserRoleArgs } from '../../resolvers/mutations/AddUserRoleMutation';
 import { CreateUserByEmailInviteArgs } from '../../resolvers/mutations/CreateUserByEmailInviteMutation';
-import {
-  UpdateUserByIdArgs,
-  UpdateUserByOidcSubArgs,
-} from '../../resolvers/mutations/UpdateUserMutation';
+import { UpdateUserByIdArgs } from '../../resolvers/mutations/UpdateUserMutation';
 import { UsersArgs } from '../../resolvers/queries/UsersQuery';
 import { Cache } from '../../utils/Cache';
 import PostgresUserDataSource from '../postgres/UserDataSource';
@@ -500,12 +497,6 @@ export class StfcUserDataSource implements UserDataSource {
   }
 
   async update(user: UpdateUserByIdArgs): Promise<User> {
-    throw new Error('Method not implemented.');
-  }
-
-  async updateUserByOidcSub(
-    args: UpdateUserByOidcSubArgs
-  ): Promise<User | null> {
     throw new Error('Method not implemented.');
   }
 
