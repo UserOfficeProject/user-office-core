feb9f348824a446b9df11b2fe8f1148a8c16430f
Chong Jia Chua <Chong.JiaChua@stfc.ac.uk>
2026-04-16 14:08:18 +0100

    feat: add button linking back to external homepage

---
+++ apps/backend/db_patches/0209_AddExternalHomepageSetting.sql
@@ -0,0 +1,13 @@
+DO
+$$
+BEGIN
+	IF register_patch('AddExternalHomepageSetting.sql', 'ChongJiaChua', 'Adding external facility homepage url to settings', '2026-04-16') THEN
+	BEGIN
+        INSERT INTO settings (settings_id, settings_value, description) 
+        VALUES ('EXTERNAL_AUTH_HOMEPAGE_URL', 'https://users.facilities.rl.ac.uk/', 'External Facility Homepage URL') 
+        ON CONFLICT DO NOTHING;
+    END;
+	END IF;
+END;
+$$
+LANGUAGE plpgsql;
