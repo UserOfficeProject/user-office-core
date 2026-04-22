6451a5ec1de87de11be9dad0e9e52fa84f2fee4f
Chong Jia Chua <Chong.JiaChua@stfc.ac.uk>
2026-04-22 12:34:11 +0100

    fix: remove hardcoded URL

--- apps/backend/db_patches/0209_AddExternalHomepageSetting.sql
+++ apps/backend/db_patches/0209_AddExternalHomepageSetting.sql
@@ -4,7 +4,7 @@ BEGIN
 	IF register_patch('AddExternalHomepageSetting.sql', 'ChongJiaChua', 'Adding external facility homepage url to settings', '2026-04-16') THEN
 	BEGIN
         INSERT INTO settings (settings_id, settings_value, description) 
-        VALUES ('EXTERNAL_AUTH_HOMEPAGE_URL', 'https://users.facilities.rl.ac.uk/', 'External Facility Homepage URL') 
+        VALUES ('EXTERNAL_AUTH_HOMEPAGE_URL', '', 'External Facility Homepage URL') 
         ON CONFLICT DO NOTHING;
     END;
 	END IF;
