f70ca9458f02f3621b4001e75105bf0a4b804dd4
zachary <zachary.hankin@diamond.ac.uk>
2026-05-13 08:43:00 +0100

    simplify + fix: syntax

--- apps/backend/src/datasources/postgres/UserDataSource.ts
+++ apps/backend/src/datasources/postgres/UserDataSource.ts
@@ -37,7 +37,7 @@ const fieldMap: { [key: string]: string } = {
   lastname: 'lastname',
   institution: 'i.institution',
   email: 'email',
-  oidcSub: 'oidc_sub'
+  oidcSub: 'oidc_sub',
 };
 
 export default class PostgresUserDataSource implements UserDataSource {
@@ -435,10 +435,7 @@ export default class PostgresUserDataSource implements UserDataSource {
             throw new GraphQLError(`Bad sort field given: ${sortField}`);
           }
           sortField = fieldMap[sortField];
-          query.orderByRaw(
-            `LOWER(??) ${sortDirection}, ?? ${sortDirection}`,
-            [sortField, sortField]
-          );
+          query.orderByRaw(`LOWER(??) ${sortDirection}`, [sortField]);
         }
       })
       .then(
