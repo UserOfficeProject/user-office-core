0d004051c4d79ac853ed11c645f3177b62739485
zachary <zachary.hankin@diamond.ac.uk>
2026-06-02 09:14:49 +0100

    fix syntax

--- apps/backend/db_patches/0209_addBaseulrtodynamiccs.sql
+++ apps/backend/db_patches/0209_addBaseulrtodynamiccs.sql
@@ -2,7 +2,7 @@ DO
 $$
 BEGIN
     IF register_patch('addBaseulrtodynamiccs.sql', 'Zachary Hankin', 'Adds an option to the dynamic HTTP question type allowing use of base domain', '2026-5-21') THEN
-        UPDATE duo.public.questions
+        UPDATE questions
         SET default_config = jsonb_set(
             default_config::jsonb,
             '{useBaseDomain}',
