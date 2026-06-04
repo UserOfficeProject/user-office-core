a84f26910cc0e169d5255629c733f8d1cf1394ab
William-Edwards-STFC <william.edwards@stfc.ac.uk>
2026-06-04 15:21:16 +0100

    #1499 | Fix the tests

--- apps/e2e/cypress/e2e/instruments.cy.ts
+++ apps/e2e/cypress/e2e/instruments.cy.ts
@@ -536,7 +536,7 @@ context('Instrument tests', () => {
 
       cy.get('[data-cy="user-list"] input').should(
         'have.value',
-        `${scientist1.firstName} ${scientist1.lastName}`
+        `${scientist1.preferredName} ${scientist1.lastName}`
       );
 
       cy.get('[data-cy="user-list"]').click();
@@ -546,7 +546,7 @@ context('Instrument tests', () => {
         numberOfScientistsAndManagerAssignedToCreatedInstrument
       );
       cy.get('[title="user-list-options"]')
-        .contains(scientist2.firstName)
+        .contains(scientist2.preferredName)
         .click();
 
       cy.get('[data-cy="re-assign-submit"]').click();
@@ -554,7 +554,7 @@ context('Instrument tests', () => {
 
       cy.notification({
         variant: 'success',
-        text: `Assigned to ${scientist2.firstName} ${scientist2.lastName}`,
+        text: `Assigned to ${scientist2.preferredName} ${scientist2.lastName}`,
       });
 
       cy.closeModal();
@@ -962,10 +962,7 @@ context('Instrument tests', () => {
         instrumentId: createdInstrumentId,
         questionaryId: initialDBData.technicalReview.questionaryId,
       });
-      let updatedContact = `${scientist2.firstName} ${scientist2.lastName} (${scientist2.email})`;
-      if (featureFlags.getEnabledFeatures().get(FeatureId.USER_SEARCH_FILTER)) {
-        updatedContact = `${scientist2.firstName.slice(0, 3)} ${scientist2.lastName} (${scientist2.email})`;
-      }
+      const updatedContact = `${scientist2.preferredName} ${scientist2.lastName} (${scientist2.email})`;
       cy.login('officer', initialDBData.roles.userOfficer);
       cy.visit('/');
 
@@ -982,7 +979,7 @@ context('Instrument tests', () => {
 
       cy.get('[role="dialog"]').contains('Technical review').click();
 
-      cy.contains(`${scientist2.firstName} ${scientist2.lastName}`).should(
+      cy.contains(`${scientist2.preferredName} ${scientist2.lastName}`).should(
         'not.exist'
       );
 
@@ -1009,7 +1006,7 @@ context('Instrument tests', () => {
       cy.get('[data-cy="confirm-ok"]').click();
 
       cy.get('[aria-label="Detail panel visibility toggle"]').eq(0).click();
-      cy.contains(`${scientist2.firstName} ${scientist2.lastName}`).should(
+      cy.contains(`${scientist2.preferredName} ${scientist2.lastName}`).should(
         'be.visible'
       );
 
@@ -1026,7 +1023,7 @@ context('Instrument tests', () => {
 
       cy.get('[role="dialog"]').contains('Technical review').click();
 
-      cy.contains(`${scientist2.firstName} ${scientist2.lastName}`).should(
+      cy.contains(`${scientist2.preferredName} ${scientist2.lastName}`).should(
         'not.exist'
       );
     });
