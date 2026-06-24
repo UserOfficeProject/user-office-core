b866d14b816d23750afac0b13623b40d3d1e63f9
zachary <zachary.hankin@diamond.ac.uk>
2026-06-24 15:24:05 +0100

    undo

--- apps/backend/src/datasources/postgres/FapDataSource.ts
+++ apps/backend/src/datasources/postgres/FapDataSource.ts
@@ -1248,8 +1248,6 @@ export default class PostgresFapDataSource implements FapDataSource {
       ...chairs.map((r) => r.user_id),
       ...secretaries.map((r) => r.user_id),
       ...reviewers.map((r) => r.user_id),
-      ...chairs.map((r) => r.fap_chair_user_id),
-      ...secretaries.map((r) => r.fap_secretary_user_id),
     ];
 
     return relatedUsers;
