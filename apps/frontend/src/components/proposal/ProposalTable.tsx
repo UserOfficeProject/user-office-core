4ef9f25db666a9b3b5c0327231493ea49e520411
Shivam K <292110273+shivoomiess@users.noreply.github.com>
2026-07-28 16:51:58 +0200

    add: updated E2E tests for DAU

--- apps/frontend/src/components/proposal/ProposalTable.tsx
+++ apps/frontend/src/components/proposal/ProposalTable.tsx
@@ -274,7 +274,7 @@ const ProposalTable = ({
             const isPI = rowData.proposerId === userContext.user.id;
 
             return {
-              icon: PeopleIcon,
+              icon: () => <PeopleIcon data-cy="view-data-access-users-icon" />,
               tooltip: 'View data access users',
               hidden:
                 isDataAccessUsersEnabled === false ||
