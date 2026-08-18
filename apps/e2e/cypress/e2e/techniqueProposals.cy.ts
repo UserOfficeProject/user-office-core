f82b8872d3247edc81b27a2eb24c50dadf56d8c0
Ellen Wright <ellen.wright@stfc.ac.uk>
2026-08-18 11:03:04 +0100

    Add User Officer tests

--- apps/e2e/cypress/e2e/techniqueProposals.cy.ts
+++ apps/e2e/cypress/e2e/techniqueProposals.cy.ts
@@ -1746,6 +1746,71 @@ context('Technique Proposal tests', () => {
         .should('not.exist');
     });
 
+    it('User Officer should be able to add a rejection comment when changing status to unsuccessful', function () {
+      cy.changeProposalsStatus({
+        proposalPks: createdProposalPk1,
+        workflowStatusId: underReviewStatus.workflowStatusId as number,
+      });
+      cy.login('officer');
+      cy.visit('/');
+      cy.finishedLoading();
+
+      cy.contains('Technique Proposals').click();
+      cy.finishedLoading();
+
+      cy.contains(proposal1.title)
+        .parent()
+        .find('[data-cy="status-dropdown"]')
+        .click();
+
+      cy.get('[role="listbox"]')
+        .should('contain', unsuccessfulStatus.name)
+        .click();
+
+      cy.setTinyMceContent(
+        `${createdProposalPk1}-rejection-comment`,
+        faker.lorem.words(10)
+      );
+
+      cy.get('[data-cy="submit-proposal-rejection-comment"]').click();
+      cy.get('[data-cy="confirm-ok"]').click();
+      cy.contains(proposal1.title)
+        .parent()
+        .should('contain', unsuccessfulStatus.name)
+        .find('[data-cy="status-dropdown"]')
+        .should('not.exist');
+    });
+
+    it('User Officer should be able to not add a rejection comment when changing status to unsuccessful', function () {
+      cy.changeProposalsStatus({
+        proposalPks: createdProposalPk1,
+        workflowStatusId: underReviewStatus.workflowStatusId as number,
+      });
+      cy.login('officer');
+      cy.visit('/');
+      cy.finishedLoading();
+
+      cy.contains('Technique Proposals').click();
+      cy.finishedLoading();
+
+      cy.contains(proposal1.title)
+        .parent()
+        .find('[data-cy="status-dropdown"]')
+        .click();
+
+      cy.get('[role="listbox"]')
+        .should('contain', unsuccessfulStatus.name)
+        .click();
+      cy.get('[data-cy="submit-no-proposal-rejection-comment"]').click();
+      cy.get('[data-cy="confirm-ok"]').click();
+
+      cy.contains(proposal1.title)
+        .parent()
+        .should('contain', unsuccessfulStatus.name)
+        .find('[data-cy="status-dropdown"]')
+        .should('not.exist');
+    });
+
     it('Scientist can only change to specific statuses and cannot assign an instrument when the current status is submitted', function () {
       cy.changeProposalsStatus({
         proposalPks: createdProposalPk1,
