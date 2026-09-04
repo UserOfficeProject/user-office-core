e12dfa996b3d4f6723ebbdf6201c323ee55ef622
Thomas Cottee Meldrum <thomas.cottee-meldrum@stfc.ac.uk>
2026-09-04 11:43:50 +0100

    missed sql change

--- apps/backend/db_patches/0213_TopicLastEdited.sql
+++ apps/backend/db_patches/0213_TopicLastEdited.sql
@@ -6,6 +6,9 @@ BEGIN
         ALTER TABLE IF EXISTS answers ADD COLUMN last_edited TIMESTAMP WITH TIME ZONE;
         update answers set last_edited = created_at;
 
+        ALTER TABLE IF EXISTS proposal ADD COLUMN last_edited TIMESTAMP WITH TIME ZONE;
+        update proposal set last_edited = created_at;
+
     END IF;
 END;
 $$
